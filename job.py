d354d4e26dc19eba0ce15a237e58f39e5fa91aa1
elaird <elaird>
2011-01-28 09:28:21 +0000

    pass bins rather than values

--- job.py
+++ job.py
@@ -12,10 +12,10 @@ def funcName() :
     return fileName().split("/")[-1].replace(".C","")
 
 def points() :
-    return [(float(sys.argv[i]), float(sys.argv[i+1])) for i in range(3, len(sys.argv), 2)]
+    return [(int(sys.argv[i]), int(sys.argv[i+1])) for i in range(3, len(sys.argv), 2)]
 
 r.gROOT.LoadMacro("%s+"%fileName())
 
 for point in points() :
-    h = getattr(r, funcName())(*point)
+    h = getattr(r, funcName())(point[0], point[1])
     h.Print()
