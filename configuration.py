63087fea93955746ef6d5c1bae19ccdc97ed020d
elaird <elaird>
2011-01-28 11:22:42 +0000

    more options

--- configuration.py
+++ configuration.py
@@ -1,22 +1,36 @@
 #!/usr/bin/env python
 
-def sourceFile() :
-    return "Lepton.C"
+def testPoints() : return True
 
-def tag(m0, m12) :
-    return "m0_%d_m12_%d"%(m0, m12)
+def sourceFile() : return "Lepton.C"
 
-def plotStem() :
-    return "Significance_35pb"
+#name for m0-m12 plane for event yields in signal-like region
+def mSuGra_FileSignal() : return "AK5Calo_PhysicsProcesses_mSUGRA_tanbeta3Fall10v1_nlo.root"
 
-def plotFileName(m0, m12) :
-    return "%s_%s.root"%(plotStem(), tag(m0, m12))
+#name for m0-m12 plane for event yields in the muon control region
+def mSuGra_FileMuonControl() : return "AK5Calo_PhysicsProcesses_mSUGRA_tanbeta3Fall10v1_muon.root"
+def mSuGra_DirMuonControl() : return "mSuGraScan_350"
+def mSuGra_HistMuonControl() : return "m0_m12_0"
 
-def workspaceStem() :
-    return "Combine"
+#name for m0-m12 plane for event yields with modified factorization and renormalization scale 0.5 down
+def mSuGra_FileSys05() : return "AK5Calo_PhysicsProcesses_mSUGRA_tanbeta3Fall10v1_sys05.root"
 
-def workspaceFileName(m0, m12) :
-    return "%s_%s.root"%(workspaceStem(), tag(m0, m12))
+#name for m0-m12 plane for event yields with modified factorization and renormalization scale 2 up
+def mSuGra_FileSys2() : return "AK5Calo_PhysicsProcesses_mSUGRA_tanbeta3Fall10v1_sys2.root"
 
-def logFileName(iSlice) :
-    return "Significance_job_%d.log"%iSlice
+#/pb
+def lumi() : return 35
+
+#output name options
+def outputDir() : return "output"
+def logDir() : return "log"
+def tag(m0, m12) : return "m0_%d_m12_%d"%(m0, m12)
+
+def plotStem() : return "%s/Significance_%spb"%(outputDir(), lumi())
+def plotFileName(m0, m12) : return "%s_%s.root"%(plotStem(), tag(m0, m12))
+
+def workspaceStem() : return "Combine"
+def workspaceFileName(m0, m12) : return "%s_%s.root"%(workspaceStem(), tag(m0, m12))
+
+def logStem() : return "%s/job"%logDir()
+def logFileName(iSlice) : return "%s_%d.log"%(logStem(), iSlice)
