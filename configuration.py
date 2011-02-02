57d6033248215034f7407e225c5139db0dc123d5
elaird <elaird>
2011-02-02 19:07:42 +0000

    nlo switch

--- configuration.py
+++ configuration.py
@@ -6,6 +6,7 @@ def switches() :
     d["doFeldmanCousins"] = False
     d["doMCMC"] = False
 
+    d["nlo"] = True
     d["testPointsOnly"] = True
     d["printCovarianceMatrix"] = False
     d["writeWorkspaceFile"] = False
@@ -14,19 +15,28 @@ def switches() :
 
 def strings(m0, m12) :
     d = {}
-    d["sourceFile"] = "Lepton2.C"
+    d["sourceFile"] = "Lepton.C"
+    #d["sourceFile"] = "SplitSignal.C"
+
+    d["signalFile"]    = "AK5Calo_PhysicsProcesses_mSUGRA_tanbeta3Fall10v1_nlo.root"
+    d["signalDir1"]    = "mSuGraScan_beforeAll"
+    d["signalDir2"]    = "mSuGraScan_350"
+    d["signalLoYield"] = "m0_m12_0"
+
+    d["muonControlFile"]    = "AK5Calo_PhysicsProcesses_mSUGRA_tanbeta3Fall10v1_muon.root"
+    d["muonControlDir1"]    = "mSuGraScan_beforeAll"
+    d["muonControlDir2"]    = "mSuGraScan_350"
+    d["muonControlLoYield"] = "m0_m12_0"
+
+    d["sys05File"]    = "AK5Calo_PhysicsProcesses_mSUGRA_tanbeta3Fall10v1_sys05.root"
+    d["sys05Dir1"]    = "mSuGraScan_beforeAll"
+    d["sys05Dir2"]    = "mSuGraScan_350"
+    d["sys05LoYield"] = "m0_m12_0"
     
-    d["signalFile"] = "AK5Calo_PhysicsProcesses_mSUGRA_tanbeta3Fall10v1_nlo.root"
-    d["signalDir1"] = "mSuGraScan_beforeAll"
-    d["signalDir2"] = "mSuGraScan_350"
-    d["signalHistExample"] = "m0_m12_0"
-
-    d["muonControlFile"] = "AK5Calo_PhysicsProcesses_mSUGRA_tanbeta3Fall10v1_muon.root"
-    d["muonControlDir"]  = "mSuGraScan_350"
-    d["muonControlHist"] = "m0_m12_0"
-
-    d["sys05File"]       = "AK5Calo_PhysicsProcesses_mSUGRA_tanbeta3Fall10v1_sys05.root"
-    d["sys2File"]        = "AK5Calo_PhysicsProcesses_mSUGRA_tanbeta3Fall10v1_sys2.root"
+    d["sys2File"]    = "AK5Calo_PhysicsProcesses_mSUGRA_tanbeta3Fall10v1_sys2.root"
+    d["sys2Dir1"]    = "mSuGraScan_beforeAll"
+    d["sys2Dir2"]    = "mSuGraScan_350"
+    d["sys2LoYield"] = "m0_m12_0"
 
     #output name options
     d["outputDir"]         = "output"
