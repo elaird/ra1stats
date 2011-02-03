5e743b420bfb2926e527120d03310958a8e6e0ae
elaird <elaird>
2011-02-03 16:33:06 +0000

    load libraries

--- job.py
+++ job.py
@@ -13,6 +13,9 @@ def fileName() :
 def funcName() :
     return fileName().split("/")[-1].replace(".C+","").replace(".C","")
 
+def libName() :
+    return funcName()+"_C.so"
+
 def points() :
     return [(int(sys.argv[i]), int(sys.argv[i+1])) for i in range(3, len(sys.argv), 2)]
 
@@ -22,7 +25,8 @@ def m0(point) :
 def m12(point) :
     return point[1]
 
-r.gROOT.LoadMacro("%s"%fileName())
+for lib in ["AutoDict_pair_std__string_std__string__cxx.so", "AutoDict_std__map_std__string_std__string__cxx.so", libName()] :
+    assert not r.gSystem.Load(lib), "Could not load library %s"%lib
 
 for point in points() :
     getattr(r, funcName())(utils.stdMap(conf.switches(), "string", "int"),
