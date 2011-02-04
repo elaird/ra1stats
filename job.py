e1c1465b01678f8b938afd22897fb1c2dc221a81
elaird <elaird>
2011-02-04 11:35:08 +0000

    3d

--- job.py
+++ job.py
@@ -17,22 +17,14 @@ def libName() :
     return funcName()+"_C.so"
 
 def points() :
-    return [(int(sys.argv[i]), int(sys.argv[i+1])) for i in range(3, len(sys.argv), 2)]
-
-def m0(point) :
-    return point[0]
-
-def m12(point) :
-    return point[1]
+    return [(int(sys.argv[i]), int(sys.argv[i+1]), int(sys.argv[i+2])) for i in range(3, len(sys.argv), 3)]
 
 for lib in ["AutoDict_pair_std__string_std__string__cxx.so", "AutoDict_std__map_std__string_std__string__cxx.so", libName()] :
     assert not r.gSystem.Load(lib), "Could not load library %s"%lib
 
 for point in points() :
     getattr(r, funcName())(utils.stdMap(conf.switches(), "string", "int"),
-                           utils.stdMap(conf.strings(m0(point), m12(point)), "string", "string"),
+                           utils.stdMap(conf.strings(*point), "string", "string"),
                            utils.stdMap(data.numbers(), "string", "double"),
-                           
-                           m0(point),
-                           m12(point),
+                           *point
                            )
