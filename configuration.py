82fee284e5e48e764c6d1304b90a679f468020ba
elaird <elaird>
2011-02-04 19:29:36 +0000

    overhaul histo specs

--- configuration.py
+++ configuration.py
@@ -19,34 +19,48 @@ def switches() :
     
     return d
 
-def stringsNoArgs() :
+def histoSpecs() :
     dir = "/afs/cern.ch/user/e/elaird/public/20_yieldHistograms"
 
     d = {}
-    d["sourceFile"] = "Lepton.C"
-    #d["sourceFile"] = "SplitSignal.C"
 
     ##v1
-    #d["signalFile"]      = "%s/v1/AK5Calo_PhysicsProcesses_mSUGRA_tanbeta3Fall10v1_nlo.root"%dir
-    #d["muonControlFile"] = "%s/v1/AK5Calo_PhysicsProcesses_mSUGRA_tanbeta3Fall10v1_muon.root"%dir
-    #d["sys05File"]       = "%s/v1/AK5Calo_PhysicsProcesses_mSUGRA_tanbeta3Fall10v1_sys05.root"%dir
-    #d["sys2File"]        = "%s/v1/AK5Calo_PhysicsProcesses_mSUGRA_tanbeta3Fall10v1_sys2.root"%dir
-    #
-    #for item in ["muonControl", "signal", "sys05", "sys2"] :
-    #    d["%sDir1"%item]    = "mSuGraScan_beforeAll"
-    #    d["%sDir2"%item]    = "mSuGraScan_350"
-    #    d["%sLoYield"%item] = "m0_m12_0"
+    #d["signal"] = {"file": "%s/v1/AK5Calo_PhysicsProcesses_mSUGRA_tanbeta3Fall10v1_nlo.root"%dir}
+    #d["muon"]   = {"file": "%s/v1/AK5Calo_PhysicsProcesses_mSUGRA_tanbeta3Fall10v1_muon.root"%dir}
+    #d["sys05"]  = {"file": "%s/v1/AK5Calo_PhysicsProcesses_mSUGRA_tanbeta3Fall10v1_sys05.root"%dir}
+    #d["sys2"]   = {"file": "%s/v1/AK5Calo_PhysicsProcesses_mSUGRA_tanbeta3Fall10v1_sys2.root"%dir}
 
     #v2
-    d["signalFile"]      = "%s/v2/Signal/AK5Calo_tanBeta3.root"%dir
-    d["muonControlFile"] = "%s/v2/Muon/AK5Calo_mSugra_TanBeta3.root"%dir
-    d["sys05File"]       = "%s/v2/AK5Calo_PhysicsProcesses_mSUGRA_tanbeta3Fall10v1_sys05.root"%dir
-    d["sys2File"]        = "%s/v2/AK5Calo_PhysicsProcesses_mSUGRA_tanbeta3Fall10v1_sys2.root"%dir
+    sig = "AK5Calo_PhysicsProcesses_mSUGRA_TanBeta3.root"
+    d["sig10"]  = {"file": "%s/v2/Signal/%s"%(dir, sig)}
+    d["muon"]   = {"file": "%s/v2/Muon/AK5Calo_mSugra_TanBeta3.root"%dir}
+    d["sig05"]  = {"file": "%s/v2/Signal/%s"%(dir, sig)}
+    d["sig20"]  = {"file": "%s/v2/Signal/%s"%(dir, sig)}
+    d["ht"]     = {"file": "%s/v2/QCDBkgd/QcdBkgdEst_tanbeta3.root"%dir}
 
-    for item in ["muonControl", "signal", "sys05", "sys2"] :
-        d["%sDir1"%item]    = "mSuGraScan_beforeAll"
-        d["%sDir2"%item]    = "mSuGraScan_350"
-        d["%sLoYield"%item] = "m0_m12_mChi_0"
+    for key in d :
+        d[key]["beforeDir"] = "mSuGraScan_beforeAll"
+        d[key]["250Dirs"  ] = []
+        d[key]["300Dirs"  ] = []
+        d[key]["350Dirs"  ] = ["mSuGraScan_350_%s"%key[-2:]]
+        d[key]["450Dirs"  ] = ["mSuGraScan_450_%s"%key[-2:]]
+        d[key]["loYield"  ] = "m0_m12_mChi_0"
+
+    d["muon"]["350Dirs"] = ["mSuGraScan_350"]
+    d["muon"]["450Dirs"] = ["mSuGraScan_450"]
+
+    d["ht"]["beforeDir"] = None
+    d["ht"]["250Dirs"]   = ["Reco_Bin_250_HT_300"]
+    d["ht"]["300Dirs"]   = ["Reco_Bin_300_HT_350"]
+    d["ht"]["350Dirs"]   = ["Reco_Bin_350_HT_400", "Reco_Bin_400_HT_450"]
+    d["ht"]["450Dirs"]   = ["Reco_Bin_450_HT_500", "Reco_Bin_500_HT_Inf"]
+    
+    return d
+
+def stringsNoArgs() :
+    d = {}
+    d["sourceFile"] = "Lepton.C"
+    #d["sourceFile"] = "SplitSignal.C"
 
     #internal names
     if switches()["twoHtBins"] :
@@ -61,9 +75,11 @@ def stringsNoArgs() :
     #output name options
     d["outputDir"]         = "output"
     d["logDir"]            = "log"
+    d["configDir"]         = "config"
     d["plotStem"]          = "%s/Significance"%d["outputDir"]
     d["workspaceStem"]     = "%s/Combine"%d["outputDir"]
     d["logStem"]           = "%s/job"%d["logDir"]
+    d["configStem"]        = "%s/job"%d["configDir"]
     return d
 
 def strings(xBin, yBin, zBin) :
@@ -71,6 +87,7 @@ def strings(xBin, yBin, zBin) :
     #output name options
     d["tag"]               = "m0_%d_m12_%d_mZ_%d"%(xBin, yBin, zBin)
     d["plotFileName"]      = "%s_%s.root"%(d["plotStem"], d["tag"])
+    d["configFileName"]    = "%s_%s.pickled"%(d["configStem"], d["tag"])
     d["workspaceFileName"] = "%s_%s.root"%(d["workspaceStem"], d["tag"])
     return d
 
