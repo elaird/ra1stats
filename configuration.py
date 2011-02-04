ce07ef8e0e7d45c9c1e3d5f79025cd120063a264
elaird <elaird>
2011-02-04 16:18:57 +0000

    progress toward two ht bins

--- configuration.py
+++ configuration.py
@@ -6,7 +6,7 @@ def switches() :
     d["doFeldmanCousins"] = False
     d["doMCMC"] = False
 
-    d["nlo"] = True
+    d["nlo"] = False
     d["fixQcdToZero"] = True
     d["constrainParameters"] = False
 
@@ -15,6 +15,8 @@ def switches() :
     d["printByHandValues"] = False
     d["writeWorkspaceFile"] = False
 
+    d["twoHtBins"] = False
+    
     return d
 
 def stringsNoArgs() :
@@ -24,7 +26,7 @@ def stringsNoArgs() :
     d["sourceFile"] = "Lepton.C"
     #d["sourceFile"] = "SplitSignal.C"
 
-    #v1
+    ##v1
     #d["signalFile"]      = "%s/v1/AK5Calo_PhysicsProcesses_mSUGRA_tanbeta3Fall10v1_nlo.root"%dir
     #d["muonControlFile"] = "%s/v1/AK5Calo_PhysicsProcesses_mSUGRA_tanbeta3Fall10v1_muon.root"%dir
     #d["sys05File"]       = "%s/v1/AK5Calo_PhysicsProcesses_mSUGRA_tanbeta3Fall10v1_sys05.root"%dir
@@ -38,13 +40,23 @@ def stringsNoArgs() :
     #v2
     d["signalFile"]      = "%s/v2/Signal/AK5Calo_tanBeta3.root"%dir
     d["muonControlFile"] = "%s/v2/Muon/AK5Calo_mSugra_TanBeta3.root"%dir
-    d["sys05File"]       = "%s/v1/AK5Calo_PhysicsProcesses_mSUGRA_tanbeta3Fall10v1_sys05.root"%dir
-    d["sys2File"]        = "%s/v1/AK5Calo_PhysicsProcesses_mSUGRA_tanbeta3Fall10v1_sys2.root"%dir
+    d["sys05File"]       = "%s/v2/AK5Calo_PhysicsProcesses_mSUGRA_tanbeta3Fall10v1_sys05.root"%dir
+    d["sys2File"]        = "%s/v2/AK5Calo_PhysicsProcesses_mSUGRA_tanbeta3Fall10v1_sys2.root"%dir
 
     for item in ["muonControl", "signal", "sys05", "sys2"] :
         d["%sDir1"%item]    = "mSuGraScan_beforeAll"
         d["%sDir2"%item]    = "mSuGraScan_350"
         d["%sLoYield"%item] = "m0_m12_mChi_0"
+
+    #internal names
+    if switches()["twoHtBins"] :
+        d["pdfName"] = "TopLevelPdf"
+        d["dataName"] = "ObservedNumberCountingDataWithSideband"
+        d["signalVar"] = "masterSignal"
+    else :
+        d["pdfName"] = "total_model"
+        d["dataName"] = "obsDataSet"
+        d["signalVar"] = "s"
 
     #output name options
     d["outputDir"]         = "output"
