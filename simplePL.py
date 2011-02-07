3fee006536db8ea058b41cdf0b0d25e7ec4dcf07
bbetchar <bbetchar>
2011-02-07 18:22:40 +0000

    tweak

--- simplePL.py
+++ simplePL.py
@@ -14,12 +14,12 @@ class simplePL(object) :
         self.interest = "susy"
         self.constituents = ["zinv","ttw"]
 
-        self.values = { 'n_selected'    : sum(iD['n_signal']),
-                        'n_muon'        : sum(iD['n_muoncontrol']),
-                        'n_photon'      : sum(iD['n_photoncontrol']),
-                        'mc_tau_mu'     : sum(iD['mc_muoncontrol'])   / sum(iD['mc_ttW']),
-                        'mc_tau_ph'     : sum(iD['mc_photoncontrol']) / sum(iD['mc_Zinv']),
-                        'sigma_susy_eff': iD['sigma_SigEff']}
+        self.values = { 'n_selected'      : sum(iD['n_signal']),
+                        'n_muon'          : sum(iD['n_muoncontrol']),
+                        'n_photon'        : sum(iD['n_photoncontrol']),
+                        'mc_tau_mu'       : sum(iD['mc_muoncontrol'])   / sum(iD['mc_ttW']),
+                        'mc_tau_ph'       : sum(iD['mc_photoncontrol']) / sum(iD['mc_Zinv']),
+                        'sigma_R_susy_eff': iD['sigma_SigEff']}
         self.values['sigma_tau_ph'] = self.values['mc_tau_ph'] * iD['sigma_Zinv']
         self.values['sigma_tau_mu'] = self.values['mc_tau_mu'] * iD['sigma_ttW']
 
@@ -44,11 +44,6 @@ class simplePL(object) :
         for name,val in self.params.iteritems() : self.wimport(name, val, 0.01*val, 100*val)
         for pair in self.values.iteritems():      self.wimport(*pair)
 
-        self.wspace.defineSet("interest",self.interest)
-        self.wspace.defineSet("nuisance",','.join(self.params.keys()+self.constituents))
-        self.wspace.defineSet("observe","")
-        emptyData = r.RooDataSet("data", "empty", self.wspace.set("observe"))
-
         # Construct the likelihood constraints
         self.wspace.factory("Poisson::select_constraint(n_selected,sum::constituents(%s))" % ','.join(["prod::susy_yeild(susy,R_susy_eff)"]+
                                                                                                       self.constituents))
@@ -56,19 +51,21 @@ class simplePL(object) :
         self.wspace.factory("Poisson::muon_constraint(n_muon,prod::muons(ttw,tau_mu))")
         self.wspace.factory("Gaussian::tau_mu_constraint(mc_tau_mu,tau_mu,sigma_tau_mu)")
         self.wspace.factory("Gaussian::tau_ph_constraint(mc_tau_ph,tau_ph,sigma_tau_ph)")
-        self.wspace.factory("Gaussian::R_susy_eff_constraint(1.,R_susy_eff,sigma_susy_eff)")
+        self.wspace.factory("Gaussian::R_susy_eff_constraint(1.,R_susy_eff,sigma_R_susy_eff)")
         
         self.wspace.factory("PROD::total_model(%s)" % ','.join(["%s_constraint"%s for s in ["select","photon","muon",
                                                                                             "tau_mu","tau_ph","R_susy_eff"]]))
+        self.wspace.defineSet("interest",self.interest)
+        self.wspace.defineSet("nuisance",','.join(self.params.keys()+self.constituents))
+        self.wspace.defineSet("observe","")
+        emptyData = r.RooDataSet("data", "empty", self.wspace.set("observe"))
         return emptyData
     
     def modelConfiguration(self)  :
-        model = r.RooStats.ModelConfig("Model")
-        model.SetWorkspace(self.wspace)
+        model = r.RooStats.ModelConfig("Model",self.wspace)
         model.SetPdf(self.wspace.pdf("total_model"))
         model.SetParametersOfInterest(self.wspace.set("interest"))
         model.SetNuisanceParameters(self.wspace.set("nuisance"))
-        getattr(self.wspace,"import")(model)
         return model
 
     def upperLimit(self, confidenceLevel) :
