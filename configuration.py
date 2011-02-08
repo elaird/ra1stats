0bc7072481942e247f570f4aa7f9665f7f445ab2
elaird <elaird>
2011-02-08 17:05:14 +0000

    clean up

--- configuration.py
+++ configuration.py
@@ -16,7 +16,7 @@ def switches() :
     d["exponentialBkg"] = False
 
     d["ignoreSignalContamination"] = False
-    d["fixQcdToZero"] = True
+    d["assumeUncorrelatedLowHtSystematics"] = True
     d["constrainParameters"] = False
 
     d["printCovarianceMatrix"] = False
@@ -27,34 +27,6 @@ def switches() :
 
 def histoSpecs() :
     dir = "/afs/cern.ch/user/e/elaird/public/20_yieldHistograms"
-
-    ##v4
-    #d = {}
-    #f = "AK5Calo_PhysicsProcesses_mSUGRA_tanbeta%dFall10v1.root"%switches()["tanBeta"]
-    #d["sig10"]  = {"file": "%s/v4/had/%s"%(dir,f)}
-    #d["muon"]   = {"file": "%s/v4/muon/%s"%(dir,f)}
-    #d["sig05"]  = {"file": "%s/v4/had/%s"%(dir,f)}
-    #d["sig20"]  = {"file": "%s/v4/had/%s"%(dir,f)}
-    #d["ht"]     = {"file": "%s/v4/ht/QcdBkgdEst_tanbeta%d.root"%(dir,switches()["tanBeta"])}
-    #
-    #for key in d :
-    #    tag = key[-2:]
-    #    d[key]["beforeDir"] = "mSuGraScan_beforeAll_%s"%tag
-    #    d[key]["250Dirs"  ] = []
-    #    d[key]["300Dirs"  ] = []
-    #    d[key]["350Dirs"  ] = ["mSuGraScan_350_%s"%tag]
-    #    d[key]["450Dirs"  ] = ["mSuGraScan_450_%s"%tag]
-    #    d[key]["loYield"  ] = "m0_m12_mChi_0"
-    #
-    #d["muon"]["beforeDir"] = "mSuGraScan_beforeAll_10"
-    #d["muon"]["350Dirs"] = ["mSuGraScan_350_10"]
-    #d["muon"]["450Dirs"] = ["mSuGraScan_450_10"]
-    #
-    #d["ht"]["beforeDir"] = None
-    #d["ht"]["250Dirs"]   = ["Reco_Bin_250_HT_300"]
-    #d["ht"]["300Dirs"]   = ["Reco_Bin_300_HT_350"]
-    #d["ht"]["350Dirs"]   = ["Reco_Bin_350_HT_400", "Reco_Bin_400_HT_450"]
-    #d["ht"]["450Dirs"]   = ["Reco_Bin_450_HT_500", "Reco_Bin_500_HT_Inf"]
 
     d = {}
     for model in ["tanBeta3", "tanBeta10", "tanBeta50"] :
@@ -133,12 +105,13 @@ def stringsNoArgs() :
     d["plotStem"]       = "%s/Significance"%d["outputDir"]
     d["workspaceStem"]  = "%s/Combine"%d["outputDir"]
     d["logStem"]        = "%s/job"%d["logDir"]
-    d["mergedFile"]     = "%s/Significance_%s_%s_%s_%s.root"%(d["outputDir"],
-                                                                     switches()["method"],
-                                                                     switches()["signalModel"],
-                                                                     "nlo" if switches()["nlo"] else "lo",
-                                                                     "2HtBins" if switches()["twoHtBins"] else "1HtBin",
-                                                                     )
+    d["mergedFile"]     = "%s/%s_%s_%s_%s_%s.root"%(d["outputDir"],
+                                                    switches()["method"],
+                                                    switches()["signalModel"],
+                                                    "nlo" if switches()["nlo"] else "lo",
+                                                    "2HtBins" if switches()["twoHtBins"] else "1HtBin",
+                                                    "expR" if switches()["exponentialBkg"] else "constantR",
+                                                    )
     return d
 
 def strings(xBin, yBin, zBin) :
