932a9cd8336102dea69b7332c41278abcae19cee
elaird <elaird>
2011-02-08 15:32:26 +0000

    one-bin likelihood comes from SlimPdfFactory

--- lepton.py
+++ lepton.py
@@ -3,136 +3,6 @@ import ROOT as r
 import math,array,cPickle
 import histogramProcessing as hp
 
-def setupLikelihoodOneBin(wspace, switches) :
-    
-    def makeSignalModel() :
-        splusb = "sum::splusb(%s,%s)" % ("prod::SigUnc(s,ratioSigEff[1,0.3,1.9])",
-                                         "sum::b(%s)" % (','.join(["prod::ttW(ratioBkgdEff_1[1.0,0.,5.],TTplusW)",
-                                                                   "prod::Zinv(ratioBkgdEff_2[1.0,0.,5.],ZINV)",
-                                                                   "QCD"][:-1 if switches["fixQcdToZero"] else None])))
-        wspace.factory("Poisson::signal(%s,%s)" % ("n_signal",splusb))
-    
-        wspace.factory("Gaussian::sigConstraint(1.,ratioSigEff,sigma_SigEff)")  # Systematic: signal acceptance*efficiency*luminosity
-        wspace.factory("Gaussian::mcCons_ttW(1.,ratioBkgdEff_1,sigma_ttW)")     # Systematic:  ttw background estimate
-        wspace.factory("Gaussian::mcCons_Zinv(1.,ratioBkgdEff_2,sigma_Zinv)")   # Systematic: Zinv background estimate
-
-        wspace.factory("PROD::signal_model(%s,%s,%s,%s)" % ("signal","sigConstraint","mcCons_ttW","mcCons_Zinv"))
-
-    def makePhotonMuonControls() :
-        wspace.factory("Poisson::photoncontrol(%s,%s)" % ("n_photoncontrol",
-                                                          "prod::sideband_photon(tau_photon,ZINV)"))
-
-        wspace.factory("Poisson::muoncontrol(%s,%s)" % ("n_muoncontrol",
-                                                        "sum::MuSPlusB(%s,%s)"%("prod::TTWside(tau_mu,TTplusW)",
-                                                                                "prod::smu(tau_s_mu[0.001],s)") ))
-    def makeQcdModel() :
-        if switches["fixQcdToZero"] : return
-        wspace.factory("Gaussian::xConstraint(1.,f,sigma_x)") # Systematic: uncertainty on assumption that rhoprime = rho*rho
-        #likelihood for the low HT and/or low alphaT auxiliary measurements
-        wspace.factory("Poisson::bar_signal(n_bar_signal,bbar)") #alphaT < 0.55 && HT > 350 GeV
-        wspace.factory("Poisson::control_1(n_control_1,prod::taub(tau,b,rho))") #alphaT > 0.55 && 300 < HT < 350 GeV
-        wspace.factory("Poisson::bar_control_1(n_bar_control_1,prod::taubar(tau,bbar))") #alphaT < 0.55 && 300 < HT << 350 GeV
-        wspace.factory("Poisson::control_2(n_control_2,prod::taub_2(tauprime,b,prod::rhoprime(f[1.,0.,2.],rho,rho)))") #alphaT > 0.55 && 250 < HT < 300 GeV
-        wspace.factory("Poisson::bar_control_2(n_bar_control_2,prod::taubar_2(tauprime,bbar))") #alphaT < 0.55 && 250 < HT < 300 GeV
-
-        wspace.factory("PROD::QCD_model(%s)" % (','.join(["xConstraint","bar_signal",
-                                                          "control_1",  "bar_control_1",
-                                                          "control_2",  "bar_control_2"]) ) )
-    makeSignalModel()
-    makePhotonMuonControls()
-    makeQcdModel()
-
-    wspace.factory("PROD::total_model(%s)" % (','.join(["muoncontrol", "signal_model",
-                                                        "photoncontrol",  "QCD_model"][:-1 if switches["fixQcdToZero"] else None])))
-
-
-    observe = ["n_signal","n_muoncontrol","n_photoncontrol",
-               "n_bar","n_bar_signal","n_control_1","n_bar_control_1","n_control_2","n_bar_control_2"
-               ][:3 if switches["fixQcdToZero"] else None]
-
-    nuisance = ["ratioBkgdEff_1","ratioBkgdEff_2","ratioSigEff","TTplusW","ZINV",
-                "QCD"][:-1 if switches["fixQcdToZero"] else None]
-
-    ## for bayesian methods
-    #wspace.factory("Uniform::prior_nuis({%s})"%(','.join(nuisance)))
-    #wspace.factory("Uniform::prior_poi({s})")
-    #wspace.factory("PROD::prior(prior_poi,prior_nuis)")
-
-    #for later use (plots)
-    wspace.defineSet( "poi","s")
-    wspace.defineSet( "obs",','.join(observe))
-    wspace.defineSet("nuis",','.join(nuisance))
-    return
-
-def importVarsOneBin(wspace, inputData, switches, dataName) :
-    for item in ["sigma_Zinv","sigma_ttW","sigma_x","sigma_SigEff"] :
-        globals()[item] = inputData[item]  # maybe use a class?
-        
-    for item in ["n_signal",
-                 "n_muoncontrol",  "mc_muoncontrol",  "mc_ttW",
-                 "n_photoncontrol","mc_photoncontrol","mc_Zinv"] :
-        globals()[item] = sum(inputData[item][:2]) # maybe use a class?
-
-    tau_mu     = mc_muoncontrol   / mc_ttW    
-    tau_photon = mc_photoncontrol / mc_Zinv
-    
-    n_control_2,     n_control_1     = inputData["n_htcontrol"][:2]
-    n_bar_control_2, n_bar_control_1 = inputData["n_bar_htcontrol"][:2]
-    n_bar_signal = sum(inputData["n_bar_htcontrol"][2:4])
-
-    def wimport(name = None, value = None, lower = None, upper = None) :
-        assert name and value!=None
-        assert (lower==None==upper) or (lower!=None!=upper)
-        if lower==upper==None : getattr(wspace, "import")(r.RooRealVar(name, name, value))
-        else :                  getattr(wspace, "import")(r.RooRealVar(name, name, value, lower, upper))
-
-    wimport("n_signal", n_signal, n_signal/10, n_signal*10)
-    wimport("n_muoncontrol", n_muoncontrol, 0.001, n_muoncontrol*10)
-    wimport("n_photoncontrol", n_photoncontrol, 0.001, n_photoncontrol*10)
-
-    wimport("n_bar_signal", n_bar_signal, n_bar_signal/10, r.RooNumber.infinity())
-    wimport("n_control_1", n_control_1, 0.001, r.RooNumber.infinity())
-    wimport("n_bar_control_1", n_bar_control_1, n_bar_control_1/10, r.RooNumber.infinity())
-    wimport("n_control_2", n_control_2, n_control_2/10, r.RooNumber.infinity())
-    wimport("n_bar_control_2", n_bar_control_2, n_bar_control_2/10,r.RooNumber.infinity())
-
-    #Parameter of interest; the number of (SUSY) signal events above the Standard Model background
-    wimport("s", 2.5, 0.0001, n_signal*3) #expected numer of (SUSY) signal events above background
-
-    #Nuisance parameters
-    wimport("TTplusW", n_signal/2, 0.01, n_signal*10)#expected tt+W background in signal-like region
-    wimport("ZINV", n_signal/2, 0.001, n_signal*10)#expected Zinv background in signal-like region
-    wimport("QCD", 0.001, 0, n_signal*10)#expected QCD background in signal-like region
-
-    #Nuisance parameter for low HT inclusive background estimation method
-    wimport("bbar", n_bar_signal, n_bar_signal/10, n_bar_signal*10)#expected total background in alphaT<0.55 and HT>350 GeV
-    wimport("tau", 1.0, 0.001, 4.)#factor which relates expected background in alphaT > 0.55 for HT > 350 GeV and 300 < HT < 350 GeV
-    wimport("tauprime", 2.515, 0, 5.)#factor which relates expected background in alphaT > 0.55 for HT > 350 GeV and 250 < HT < 300 GeV
-    wimport("rho", 1.18, 0., 2.)#factor rho which takes into account differences between alphaT > 0.55 and alphaT < 0.55 in the signal yield development with HT    
-    
-    for item in ["sigma_x",                # uncertainty on Monte Carlo estimation of X
-                 "tau_mu","sigma_ttW",     # Nuisance par tt+w
-                 "tau_photon","sigma_Zinv",# Nuisance par Zinv
-                 "sigma_SigEff"            # Systematic : uncertainty on signal*acceptance*efficiency*lumi
-                 ] : wimport(item, eval(item))
-    
-    setupLikelihoodOneBin(wspace, switches)
-
-    #set values of observables
-    lolArgSet = wspace.set("obs")
-    newSet = r.RooArgSet(wspace.set("obs"))
-    for item in ["n_signal","n_muoncontrol","n_photoncontrol",
-                 "n_control_1","n_bar_control_1",
-                 "n_control_2","n_bar_control_2","n_bar_signal"
-                 ][:3 if switches["fixQcdToZero"] else None] :
-        newSet.setRealValue(item, eval(item))
-    
-    data = r.RooDataSet(dataName, "title", lolArgSet)
-    data.Print()
-    data.add(lolArgSet)
-    getattr(wspace, "import")(data)
-    return data
-
 def modelConfiguration(wspace, pdfName)  :
     modelConfig = r.RooStats.ModelConfig("Combine")
     modelConfig.SetWorkspace(wspace)
@@ -237,7 +107,7 @@ def yields(specs, nlo, twoHtBins, lumi, m0, m12, mChi) :
                     histo = func(specs[item], specs[item][dir], lumi)
                     if histo : out["%s_%s"%(item,tag)] = histo.GetBinContent(m0, m12, mChi)
             else :
-                histo = func(specs[item], specs[item]["350Dirs"] + specs[item]["450Dirs"], lumi)
+                histo = func(specs[item+"_2"], specs[item]["350Dirs"] + specs[item]["450Dirs"], lumi)
                 if histo : out["%s"%item] = histo.GetBinContent(m0, m12, mChi)
 
         else :
@@ -246,61 +116,37 @@ def yields(specs, nlo, twoHtBins, lumi, m0, m12, mChi) :
                 if histo : out["%s_%s"%(item,tag)] = histo.GetBinContent(m0, m12, mChi)
     return out
 
-def setSignalVars(y, switches, specs, strings, wspace, sigma_SigEff_, pdfUncertainty) :
-    def signalSys(init, y) :
-        d_s       = y["sig10_1"]+y["sig10_2"]
-        d_s_sys05 = y["sig05_1"]+y["sig05_2"]
-        d_s_sys2  = y["sig20_1"]+y["sig20_2"]
-        masterPlus =  abs(max((max((d_s_sys2 - d_s),(d_s_sys05 - d_s))),0.))
-        masterMinus = abs(max((max((d_s - d_s_sys2),(d_s - d_s_sys05))),0.))
-        return math.sqrt( math.pow( max(masterMinus,masterPlus)/d_s,2) + pow(init,2) + pow(pdfUncertainty, 2) )
-
-    def d_s(y) :
-        return y["sig10_1"] + y["sig10_2"]
-
-    def frac(y, tag, index) :
-        return y["%s_%d"%(tag, index)] / d_s(y)
-
-    ds = d_s(y)
-    if ds>0.0 :
-        wspace.var("BR1").setVal(frac(y, "sig10", 1))
-        wspace.var("BR2").setVal(frac(y, "sig10", 2))
-        
-        wspace.var("muon_cont_1").setVal(frac(y, "muon", 1))
-        wspace.var("muon_cont_2").setVal(frac(y, "muon", 2))
-        wspace.var("lowHT_cont_1").setVal(frac(y, "ht", 1))
-        wspace.var("lowHT_cont_2").setVal(frac(y, "ht", 2))
-        wspace.var("signal_sys").setVal(sigma_SigEff_ if not switches["nlo"] else signalSys(sigma_SigEff_, y))
-    return ds
-
-def setSignalVarsOneBin(y, switches, specs, strings, wspace, sigma_SigEff_) :
-    tau_s_muon = 1
-    d_s        = y["sig10"]
-    d_muon     = y["muon"]
-    signal_sys = sigma_SigEff_
+def setSFrac(wspace, sFrac) :
+    wspace.var("BR_1").setVal(sFrac[0])
+    wspace.var("BR_2").setVal(sFrac[1])
     
-    if d_s>0.0 : tau_s_muon = d_muon / d_s
+def setSignalVars(y, switches, specs, strings, wspace, sigma_SigEff, pdfUncertainty) :
+    def d_s(y, tag, twoHtBins) :
+        if twoHtBins : return y["%s_1"%tag] + y["%s_2"%tag]
+        else :         return y["%s_2"%tag]
 
-    if switches["nlo"] and d_s>0.0 :
-        d_s_sys05 = y["sig05"]
-        d_s_sys2  = y["sig20"]
-        masterPlus =  abs(max((max((d_s_sys2 - d_s),(d_s_sys05 - d_s))),0.))
-        masterMinus = abs(max((max((d_s - d_s_sys2),(d_s - d_s_sys05))),0.))
-        signal_sys = math.sqrt( math.pow( max(masterMinus,masterPlus)/d_s,2) + pow(sigma_SigEff_,2)  )
+    def signalSys(init, y, twoHtBins) :
+        ds       = d_s(y, "sig10", twoHtBins)
+        ds_sys05 = d_s(y, "sig05", twoHtBins)
+        ds_sys2  = d_s(y, "sig20", twoHtBins)
+        masterPlus =  abs(max((max((ds_sys2 - ds),(ds_sys05 - ds))),0.))
+        masterMinus = abs(max((max((ds - ds_sys2),(ds - ds_sys05))),0.))
+        return math.sqrt( math.pow( max(masterMinus,masterPlus)/ds,2) + pow(init,2) + pow(pdfUncertainty, 2) )
 
-    #set background contamination
-    wspace.var("tau_s_mu").setVal(tau_s_muon);
-    wspace.var("sigma_SigEff").setVal(signal_sys);
-    return d_s
+    ds = d_s(y, "sig10", switches["twoHtBins"])
 
-def prepareCanvas(doBayesian, doMCMC) :
-    c1 = r.TCanvas("c1")
-    if doBayesian and doMCMC :
-        c1.Divide(3)
-        c1.cd(1)
-    elif doBayesian or doMCMC :
-        c1.Divide(2)
-        c1.cd(1)
+    if ds>0.0 :
+        wspace.var("signal_sys").setVal(sigma_SigEff if not switches["nlo"] else signalSys(sigma_SigEff, y, switches["twoHtBins"]))
+        wspace.var("muon_cont_2").setVal(y["muon_2"]/ds)
+        wspace.var("lowHT_cont_1").setVal(y["ht_1"]/ds)
+        wspace.var("lowHT_cont_2").setVal(y["ht_2"]/ds)
+        
+        if switches["twoHtBins"] :
+            wspace.var("BR_1").setVal(y["sig10_1"]/ds)
+            wspace.var("BR_2").setVal(y["sig10_2"]/ds)
+            wspace.var("muon_cont_1").setVal(y["muon_1"]/ds)
+
+    return ds
 
 def writeNumbers(fileName = None, m0 = None, m12 = None, mChi = None, upperLimit = None, ds = None, y = None) :
     def insert(name, value) :
@@ -344,70 +190,78 @@ def lumi(switches, inputData) :
     else :
         return 1.0
 
-def Lepton(switches, specs, strings, inputData,
-           m0, m12, mChi) :
+def summed(inputData, twoHtBins) :
+    if twoHtBins : return inputData
+    out = {}
+    for key,value in inputData.iteritems() :
+        if type(value) is tuple :
+            out[key] = tuple(list(value[:-2])+[value[-2]+value[-1]])
+        else :
+            out[key] = value
+    return out
+
+def Lepton(switches, specs, strings, inputData, m0, m12, mChi) :
 
     r.RooRandom.randomGenerator().SetSeed(inputData["seed"]) #set RooFit random seed for reproducible results
     wspace = r.RooWorkspace("Combine")
     
-    #import variables and set up total likelihood function
-    if not switches["twoHtBins"] :
-        importVarsOneBin( wspace, inputData, switches, strings["dataName"])
-    else :
-        r.gSystem.Load("SlimPdfFactory_C.so")
-        r.AddModel_Lin_Combi(array.array('d',inputData["sFrac"]),
-                             lumi(switches, inputData),
-                             inputData["_lumi_sys"],
-                             accXeff(switches, specs, inputData, m0, m12, mChi),
-                             inputData["_accXeff_sys"],
-
-                             inputData["sigma_ttW"],
-                             inputData["sigma_Zinv"],
-
-                             inputData["_lowHT_sys_1"],
-                             inputData["_lowHT_sys_2"],
-
-                             inputData["_muon_cont_1"],
-                             inputData["_muon_cont_2"],
-
-                             inputData["_lowHT_cont_1"],
-                             inputData["_lowHT_cont_2"],
-
-                             wspace,
-                             strings["pdfName"],
-                             strings["signalVar"],
-                             )
-
-        r.AddDataSideband_Combi( array.array('d', inputData["n_htcontrol"]),
-                                 array.array('d', inputData["n_bar_htcontrol"]),
-                                 len(inputData["n_bar_htcontrol"]),
-
-                                 array.array('d', inputData["n_muoncontrol"]),
-                                 array.array('d', inputData["n_photoncontrol"]),
-
-                                 taus(inputData["mc_muoncontrol"], inputData["mc_ttW"]),
-                                 taus(inputData["mc_photoncontrol"], inputData["mc_Zinv"]),
-                                 
-                                 len(inputData["n_muoncontrol"]),
-                                 wspace,
-                                 strings["dataName"]
-                                 )
-
+    r.gSystem.Load("SlimPdfFactory_C.so")
+    r.AddModel(lumi(switches, inputData),
+               inputData["_lumi_sys"],
+               accXeff(switches, specs, inputData, m0, m12, mChi),
+               inputData["_accXeff_sys"],
+               
+               inputData["sigma_ttW"],
+               inputData["sigma_Zinv"],
+               
+               inputData["_lowHT_sys_1"],
+               inputData["_lowHT_sys_2"],
+               
+               inputData["_muon_cont_1"],
+               inputData["_muon_cont_2"],
+               
+               inputData["_lowHT_cont_1"],
+               inputData["_lowHT_cont_2"],
+               
+               switches["exponentialBkg"],
+               switches["twoHtBins"],
+               
+               wspace,
+               strings["pdfName"],
+               strings["signalVar"],
+               )
+    
+    summedData = summed(inputData, switches["twoHtBins"])
+    
+    r.AddDataSideband_Combi(array.array('d', summedData["n_htcontrol"]),
+                            array.array('d', summedData["n_bar_htcontrol"]),
+                            len(summedData["n_bar_htcontrol"]),
+                            
+                            array.array('d', summedData["n_muoncontrol"]),
+                            array.array('d', summedData["n_photoncontrol"]),
+                            
+                            taus(summedData["mc_muoncontrol"], summedData["mc_ttW"]),
+                            taus(summedData["mc_photoncontrol"], summedData["mc_Zinv"]),
+                            
+                            len(summedData["n_muoncontrol"]),
+                            switches["twoHtBins"],
+                            wspace,
+                            strings["dataName"]
+                            )
+    
     modelConfig = modelConfiguration(wspace, strings["pdfName"])
 
     if switches["writeWorkspaceFile"] : wspace.writeToFile(strings["outputWorkspaceFileName"])
     if switches["printCovarianceMatrix"] : printCovMat(wspace, dataName)
     if switches["constrainParameters"] : constrainParams(wspace)
     
-    prepareCanvas(switches["method"]=="doBayesian", switches["method"]=="doMCMC")
-
-    if not switches["signalFreeMode"] :
-        y = yields(specs, switches["nlo"], switches["twoHtBins"], inputData["lumi"], m0, m12, mChi)
-        if switches["twoHtBins"] : ds = setSignalVars(      y, switches, specs, strings, wspace, inputData["sigma_SigEff"], inputData["pdfUncertainty"])
-        else :                     ds = setSignalVarsOneBin(y, switches, specs, strings, wspace, inputData["sigma_SigEff"])
-    else :
+    if switches["ignoreSignalContamination"] :
         y = {}
         ds = None
+        setSFrac(wspace, inputData["sFrac"])
+    else :
+        y = yields(specs, switches["nlo"], switches["twoHtBins"], inputData["lumi"], m0, m12, mChi)
+        ds = setSignalVars(y, switches, specs, strings, wspace, inputData["sigma_SigEff"], inputData["pdfUncertainty"])
 
     func = eval(switches["method"])
     upperLimit = func(modelConfig, wspace, strings["dataName"], strings["signalVar"])
