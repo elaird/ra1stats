2307f6e360b103523ae2e022b1122781cf8e3743
elaird <elaird>
2011-02-08 15:31:50 +0000

    consolidate

--- data.py
+++ data.py
@@ -5,16 +5,21 @@ def numbers() :
     d = {}
     d["seed"]             = 4357 #seed for RooRandom::randomGenerator()
     d["lumi"]             = 35.0 #/pb
+    d["_lumi"]            = 1.0
+    d["_lumi_sys"]        = 0.11
                           
-    d["sFrac"]            = (   0.25,   0.75) #assumed fraction of signal in each bin (in case of no model)
-                          
-    d["n_signal"]         = (      8,      5) #number of events measured at HT > 350 GeV and alphaT > 0.55
+    d["n_signal"]         = (8, 5) #number of events measured at HT > 350 GeV and alphaT > 0.55
     d["sigma_SigEff"]     = 0.12    #systematic uncertainty on signal acceptance*efficiency*luminosity //added single uncertainties quadratically
-                          
-    d["n_htcontrol"]      = (33, 11,  8,  5)
+    d["_accXeff"]         = 1.0
+    d["_accXeff_sys"]     = 0.1
+    d["pdfUncertainty"]   = 0.1
+    
+    d["n_htcontrol"]      = (33, 11, 8, 5)
     d["n_bar_htcontrol"]  = (844459, 331948, 225649, 110034)
     d["sigma_x"]          = 0.11 #systematic uncertainty on inclusive background estimation (uncertainty on the assumpotion that rhoprime = rho*rho)
-                          
+    d["_lowHT_sys_1"]     = 0.08
+    d["_lowHT_sys_2"]     = 0.14
+
     d["n_muoncontrol"]    = (      5,      2) #number of events measured in muon control sample
     d["mc_muoncontrol"]   = (    4.1,    1.9) #MC expectation in muon control sample
     d["mc_ttW"]           = (  3.415,  1.692) #MC expectation in hadronic sample
@@ -25,44 +30,11 @@ def numbers() :
     d["mc_Zinv"]          = (  2.586,  1.492) #MC expectation in photon control sample
     d["sigma_Zinv"]       = 0.4     #systematic uncertainty on Zinv background estimation
 
-    #new from Tanja
-    d["pdfUncertainty"] = 0.1
-    
-    d["_lumi"] = 1.0
-    d["_lumi_sys"] = 0.11
-    d["_accXeff"] = 1.0
-    d["_accXeff_sys"] = 0.1
-
-    d["_muon_cont_1"] = 0.2
-    d["_muon_cont_2"] = 0.2
+    #place-holder values; used only when switches["ignoreSignalContamination"]=True; otherwise overridden
+    d["sFrac"]         = (0.25, 0.75) #assumed fraction of signal in each bin (in case of no model)
+    d["_muon_cont_1"]  = 0.2
+    d["_muon_cont_2"]  = 0.2
     d["_lowHT_cont_1"] = 0.2
     d["_lowHT_cont_2"] = 0.2
-
-    d["_lowHT_sys_1"] = 0.08
-    d["_lowHT_sys_2"] = 0.14
-
-    return d
-
-#the numbers below are out-dated
-def numbersOneBin() :
-    d = {}
-    d["lumi"]            =   35.0  #/pb
-    d["n_signal"]        =     13  #number of events measured at HT > 350 GeV and alphaT > 0.55
-    d["n_bar_signal"]    = 336044  #number of events measured at HT > 350 GeV and alphaT < 0.55
-    d["n_control_1"]     =     11  #number of events measured at 300 < HT < 350 GeV and alphaT > 0.55
-    d["n_bar_control_1"] = 332265  #number of events measured at 300 < HT < 350 GeV and alphaT < 0.55
-    d["n_control_2"]     =     33  #number of events measured at 250 < HT < 300 GeV and alphaT > 0.55
-    d["n_bar_control_2"] = 845157  #number of events measured at 250 < HT < 300 GeV and alphaT < 0.55
-    
-    d["sigma_x"]         = 0.11    #systematic uncertainty on inclusive background estimation (uncertainty on the assumpotion that rhoprime = rho*rho)
-    d["n_muoncontrol"]   = 7       #number of events measured in muon control sample
-    d["tau_mu"]          = 5.9/5.1 #Monte Carlo estimation of the factor tau which relates expected events in muon control sample to expected tt+W background in signal-like region
-    d["sigma_ttW"]       = 0.3     #systematic uncertainty on tt+W background estimation
-    
-    d["n_photoncontrol"] = 7       #number of events measured in photon control sample
-    d["tau_photon"]      = 6.5/4.1 #MC estimate of the factor tau which relates expected events in photon control sample to expected Zinv background in signal-like region
-    d["sigma_Zinv"]      = 0.4     #systematic uncertainty on Zinv background estimation
-    
-    d["sigma_SigEff"]    = 0.12    #systematic uncertainty on signal acceptance*efficiency*luminosity //added single uncertainties quadratically
 
     return d
