e8a9a9d7794a5fcdeb2badce3d426a1d253aaea5
elaird <elaird>
2011-02-16 14:17:31 +0000

    410_pre2

--- envC.sh
+++ envC.sh
@@ -1,4 +1,4 @@
 export SCRAM_ARCH=slc5_amd64_gcc434
-cd /afs/cern.ch/cms/slc5_amd64_gcc434/cms/cmssw/CMSSW_4_2_0_pre4/src
+cd /afs/cern.ch/cms/slc5_amd64_gcc434/cms/cmssw/CMSSW_4_1_0_pre2/src
 eval `scram runtime -sh`
 cd - > /dev/null
