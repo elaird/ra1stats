2aa9e6a9df699ea33970ec1eb62d875ede529aa2
elaird <elaird>
2011-03-08 17:49:49 +0000

    restrict lumi range and gauss for accXeff_Cons

--- SlimPdfFactory.C
+++ SlimPdfFactory.C
@@ -74,14 +74,16 @@ void AddModel(Double_t _lumi, Double_t _lumi_sigma, Double_t _accXeff, Double_t 
 
   //define common variables
   RooRealVar* masterSignal = new RooRealVar(muName,"masterSignal",10.,0.,_masterSignalMax);//POI
-  RooRealVar* lumi = new RooRealVar("lumi","lumi", _lumi, _lumi/10.0, _lumi*10.0);
+  //RooRealVar* lumi = new RooRealVar("lumi","lumi", _lumi, _lumi/10.0, _lumi*10.0);
+  RooRealVar* lumi = new RooRealVar("lumi","lumi", _lumi, _lumi/2.0, _lumi*2.0);
   RooRealVar* lumi_nom = new RooRealVar("lumi_nom","lumi_nom",_lumi);
   RooRealVar* lumi_sigma = new RooRealVar("lumi_sigma","lumi_sigma",_lumi*_lumi_sigma);
   
   RooRealVar* accXeff = new RooRealVar("accXeff","accXeff",_accXeff, _accXeff/10.0, _accXeff*10.0);
   RooRealVar* accXeff_nom = new RooRealVar("accXeff_nom","accXeff_nom",_accXeff);
-  RooRealVar* accXeff_sigma = new RooRealVar("accXeff_sigma","accXeff_sigma",_accXeff*_accXeff_sigma);
-  RooFormulaVar* accXeff_sigma_exp = new RooFormulaVar("accXeff_sigma_exp","accXeff_sigma_exp","exp(@0)",RooArgList(*accXeff_sigma));
+  RooRealVar* accXeff_sigma = new RooRealVar("accXeff_sigma","accXeff_sigma",_accXeff_sigma);
+  //RooFormulaVar* accXeff_sigma_exp = new RooFormulaVar("accXeff_sigma_exp","accXeff_sigma_exp","exp(@0*@1)",RooArgList(*accXeff_sigma, *accXeff_nom));
+  RooFormulaVar* accXeff_sigma_prod = new RooFormulaVar("accXeff_sigma_prod","accXeff_sigma_prod","(@0*@1)",RooArgList(*accXeff_sigma, *accXeff_nom));
 
   RooRealVar* BR1 = new RooRealVar("BR_1","BR_1",1.);
   RooRealVar* BR2 = new RooRealVar("BR_2","BR_2",1.);
@@ -247,7 +249,8 @@ void AddModel(Double_t _lumi, Double_t _lumi_sigma, Double_t _accXeff, Double_t 
   RooLognormal *sys_ttW_Cons = new RooLognormal("sys_ttW_Cons","sys_ttW_Cons",*sys_ttW_nom,*sys_ttW,*sys_ttW_sigma);
   RooLognormal *sys_Zinv_Cons = new RooLognormal("sys_Zinv_Cons","sys_Zinv_Cons",*sys_Zinv_nom,*sys_Zinv,*sys_Zinv_sigma);
  
-  RooLognormal *accXeff_Cons = new RooLognormal("accXeff_Cons","accXeff_Cons",*accXeff_nom,*accXeff,*accXeff_sigma_exp);
+  //RooLognormal *accXeff_Cons = new RooLognormal("accXeff_Cons","accXeff_Cons",*accXeff_nom,*accXeff,*accXeff_sigma_exp);
+  RooGaussian *accXeff_Cons = new RooGaussian("accXeff_Cons","accXeff_Cons",*accXeff_nom,*accXeff,*accXeff_sigma_prod);
   RooGaussian *lumi_Cons = new RooGaussian("lumi_Cons","lumi_Cons",*lumi_nom,*lumi,*lumi_sigma);
 
   RooLognormal *sys_lowHT_Cons_1 = new RooLognormal("sys_lowHT_Cons_1","sys_lowHT_Cons_1",*lowHT_sys1_nom,*lowHT_sys1,*lowHT_sys1_sigma);
