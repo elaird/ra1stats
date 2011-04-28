354070d5ca28fc862a3998f3dd47bce72a1ce573
elaird <elaird>
2011-04-28 17:09:28 +0000

    low ht ewk

--- data2.py
+++ data2.py
@@ -11,14 +11,14 @@ def observations() :
     return {"htMean":( 265.0,  315.0,  375.0,  475.0),#place-holder values
             "nBulk": (844459, 331948, 225649, 110034),
             "nSel":  (    33,     11,      8,      5),
-            "nPhot": (    -1,     -1,      6,      1),
-            "nMuon": (    -1,     -1,      5,      2),
+            "nPhot": (    24,      4,      6,      1),
+            "nMuon": (    13,      5,      5,      2),
             }
 
 def mcExpectations() : #events / lumi
-    return {"mcMuon":(    -1,     -1,    4.1,    1.9  ),
-            "mcTtw": (    -1,     -1,    3.415,  1.692),
-            "mcPhot":(    -1,     -1,    4.4,    2.1  ),
+    return {"mcMuon":(  12.2,    5.2,    4.1,    1.9  ),
+            "mcTtw": (  10.5,    4.47,   3.415,  1.692),
+            "mcPhot":(  22.4,    7.0,    4.4,    2.1  ),
             "mcZinv":(    -1,     -1,    2.586,  1.492),
             }
 
