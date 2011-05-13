cd9a2fc8d2f51f5072083820a3bc949689901e02
elaird <elaird>
2011-05-13 11:35:20 +0000

    call fresh

--- job.py
+++ job.py
@@ -1,19 +1,13 @@
 #!/usr/bin/env python
 import sys
-import configuration as conf
-import data,lepton
 
 def points() :
     return [(int(sys.argv[i]), int(sys.argv[i+1]), int(sys.argv[i+2])) for i in range(4, len(sys.argv), 3)]
 
 def go() :
     for point in points() :
-        lepton.Lepton(conf.switches(),
-                      conf.histoSpecs(),
-                      conf.strings(*point),
-                      data.numbers(),
-                      *point
-                      )
+        import fresh
+        fresh.go()
 
 profile = False
 if profile :
