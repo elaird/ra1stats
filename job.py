8fc5fabb3e7d1cc12c1f8969e3b2b61b5815ee56
elaird <elaird>
2011-05-14 17:41:37 +0000

    fresh interface

--- job.py
+++ job.py
@@ -1,13 +1,27 @@
 #!/usr/bin/env python
-import sys
+import sys,cPickle,fresh
+import configuration as conf
 
 def points() :
     return [(int(sys.argv[i]), int(sys.argv[i+1]), int(sys.argv[i+2])) for i in range(4, len(sys.argv), 3)]
 
+def writeNumbers(fileName = None, d = None) :
+    outFile = open(fileName, "w")
+    cPickle.dump(d, outFile)
+    outFile.close()
+
 def go() :
+    print "points:",points()
     for point in points() :
-        import fresh
-        fresh.go()
+        f = fresh.foo(REwk = ["", "FallingExp", "Constant"][0],
+                      RQcd = ["FallingExp", "Zero"][0],
+                      signalXs = 4.9, #pb (LM1); 0 or None means SM only
+                      signalEff = {"had": (0.0,    0.0,    0.02,   0.10),
+                                   "muon":(0.0,    0.0,    0.002,  0.01),
+                                   },
+                      )
+        ul = f.upperLimit()
+        writeNumbers(conf.strings(*point)["pickledFileName"], {"upperLimit":ul})
 
 profile = False
 if profile :
