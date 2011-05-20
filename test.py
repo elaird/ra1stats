42136e8e278fc4bb072c3698ef25e3b50b47775f
elaird <elaird>
2011-05-20 16:25:47 +0000

    test eff in more bins

--- test.py
+++ test.py
@@ -3,12 +3,14 @@
 import fresh
 import data2011 as data
 f = fresh.foo(inputData = data.data(),
-              REwk = ["", "FallingExp", "Constant"][0],
-              RQcd = ["FallingExp", "Zero"][0],
+              REwk = ["", "FallingExp", "Constant"][1],
+              RQcd = ["FallingExp", "Zero"][1],
               signalXs = 4.9, #pb (LM1); 0 or None means SM only
-              signalEff = {"had": (0.0,    0.0,    0.02,   0.10),
-                           "muon":(0.0,    0.0,    0.002,  0.01),
+              signalEff = {"had": (0.006,    0.01,    0.02,   0.029,  0.034,  0.023,  0.011,  0.005, 0.005),
+                           "muon":(0.0,     0.001,   0.002,   0.003,  0.003,  0.002,  0.001,  0.0,   0.0  ),
                            },
+              #signalEff = {"had": (0.0,    0.0,    0.02,   0.10), #2010 binning
+              #             "muon":(0.0,    0.0,    0.002,  0.01),
               )
 
 #ul = f.upperLimit(makePlot = True); print "Upper limit:",ul
