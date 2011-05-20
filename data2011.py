18f58561ee9789cc10d68548068b1460cacae085
elaird <elaird>
2011-05-20 12:58:42 +0000

    update photon and z numbers

--- data2011.py
+++ data2011.py
@@ -11,23 +11,27 @@ class data(object) :
                 "mcMuon":   35.,
                 "mcTtw":    35.,
                 
-                "phot":    101.,
-                "mcPhot":  101.,
-                "mcZinv":  101.,
+                "phot":    164.6,
+                "mcPhot":  164.6,
+                "mcZinv":  164.6,
                 }
     def observations(self) :
-        return {"htMean": (     297.51,      347.25,      415.57,   516.2 , 617.17, 717.72, 818.33, 919.08),
-                "nBulk":  (9.04799e+06, 3.79382e+06, 2.62383e+06,   823968, 289401, 112899,  48118,  65098),
-                "nHad":   (        130,          68,          47,       12,      5,      1,      0,      0),
-                "nPhot":       (    94,     45,     21,      9),
-                "nMuon":       (    13,      5,      5,      2), #2010
+        return {"htMean": (    297.51,      347.25,      415.57,  516.2 , 617.17, 717.72, 818.33, 919.08),
+                "nBulk":  ( 9.047e+06,   3.793e+06,   2.623e+06,  823968, 289401, 112899,  48118,  65098),
+                "nHad":   (       130,          68,          47,      12,      5,      1,      0,      0),
+                "nPhot":  (       142,          57,          43,      12,      2,      2,      2,      0),
+                "nMuon":  (   13,   5,     5,    2), #2010
                 }
+
     def mcExpectations(self) :
-        return {"mcMuon":scaled((  12.2,    5.2,    4.1,    1.9  ), self.lumi()["muon"]/self.lumi()["mcMuon"]), #2010
-                "mcTtw": scaled((  10.5,    4.47,   3.415,  1.692), self.lumi()["had" ]/self.lumi()["mcTtw"] ), #2010
-                "mcPhot":scaled(( 100.,    30.,    15.,     8.   ), self.lumi()["phot"]/self.lumi()["mcPhot"]),
-                "mcZinv":scaled((  33.,    14.,     7.,     7.   ), self.lumi()["had"] /self.lumi()["mcZinv"]),
+        return {"mcMuon":   scaled((  12.2,     5.2,     4.1,    1.9), self.lumi()["muon"]/self.lumi()["mcMuon"]), #2010
+                "mcTtw":    scaled((  10.5,    4.47,   3.415,  1.692), self.lumi()["had" ]/self.lumi()["mcTtw"] ), #2010
+                "mcPhot":   scaled((   110,      37,      28,      9,    4,    0.8,    0.5,    0), self.lumi()["phot"]/self.lumi()["mcPhot"]),
+                "mcPhotErr":scaled((    10,       3,       3,      1,    1,    0.5,    0.4,    1), self.lumi()["phot"]/self.lumi()["mcPhot"]),
+                "mcZinv":   scaled((    39,      16,      18,      9,  1.7,    0.4,    0.4,    0), self.lumi()["had"] /self.lumi()["mcZinv"]),
+                "mcZinvErr":scaled((     4,       3,       3,      2,  0.9,    0.4,    0.4,    1), self.lumi()["had"] /self.lumi()["mcZinv"]),
                 }
+    
     def fixedParameters(self) :
         return {"sigmaLumi":  0.04,
                 "sigmaPhotZ": 0.40,
