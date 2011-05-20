2291e0756d60ff151aa74d19409dd9de59781989
elaird <elaird>
2011-05-20 13:15:17 +0000

    make photons and zs exclusive

--- data2011.py
+++ data2011.py
@@ -1,6 +1,14 @@
-def scaled(t, factor) : return tuple([factor*a for a in t])
+def scaled(t, factor) :
+    return tuple([factor*a for a in t])
+
+def excl(counts, isExclusive) :
+    out = []
+    for i,count,isExcl in zip(range(len(counts)), counts, isExclusive) :
+        out.append(count if isExcl else (count-counts[i+1]))
+    return tuple(out)
 
 class data(object) :
+    def photIsExcl(self) :      return (    1,     1,     0,     0,     0,     0,     0,     1)
     def htBinLowerEdges(self) : return (275.0, 325.0, 375.0, 475.0, 575.0, 675.0, 775.0, 875.0)
     def htMaxForPlot(self) :    return 975.0
     def lumi(self) :
@@ -16,22 +24,20 @@ class data(object) :
                 "mcZinv":  164.6,
                 }
     def observations(self) :
-        return {"htMean": (    297.51,      347.25,      415.57,  516.2 , 617.17, 717.72, 818.33, 919.08),
-                "nBulk":  ( 9.047e+06,   3.793e+06,   2.623e+06,  823968, 289401, 112899,  48118,  65098),
-                "nHad":   (       130,          68,          47,      12,      5,      1,      0,      0),
-                "nPhot":  (       142,          57,          43,      12,      2,      2,      2,      0),
-                "nMuon":  (   13,   5,     5,    2), #2010
+        return {"htMean":    (    297.51,      347.25,      415.57,  516.2 , 617.17, 717.72, 818.33, 919.08 ),
+                "nBulk":     ( 9.047e+06,   3.793e+06,   2.623e+06,  823968, 289401, 112899,  48118,  65098 ),
+                "nHad":      (       130,          68,          47,      12,      5,      1,      0,      0 ),
+                "nPhot":excl((       142,          57,          43,      12,      2,      2,      2,      0), self.photIsExcl()),
+                "nMuon":     (   13,   5,     5,    2), #2010
                 }
-
     def mcExpectations(self) :
-        return {"mcMuon":   scaled((  12.2,     5.2,     4.1,    1.9), self.lumi()["muon"]/self.lumi()["mcMuon"]), #2010
-                "mcTtw":    scaled((  10.5,    4.47,   3.415,  1.692), self.lumi()["had" ]/self.lumi()["mcTtw"] ), #2010
-                "mcPhot":   scaled((   110,      37,      28,      9,    4,    0.8,    0.5,    0), self.lumi()["phot"]/self.lumi()["mcPhot"]),
-                "mcPhotErr":scaled((    10,       3,       3,      1,    1,    0.5,    0.4,    1), self.lumi()["phot"]/self.lumi()["mcPhot"]),
-                "mcZinv":   scaled((    39,      16,      18,      9,  1.7,    0.4,    0.4,    0), self.lumi()["had"] /self.lumi()["mcZinv"]),
-                "mcZinvErr":scaled((     4,       3,       3,      2,  0.9,    0.4,    0.4,    1), self.lumi()["had"] /self.lumi()["mcZinv"]),
+        return {"mcMuon":         scaled((  12.2,     5.2,     4.1,    1.9), self.lumi()["muon"]/self.lumi()["mcMuon"]), #2010
+                "mcTtw":          scaled((  10.5,    4.47,   3.415,  1.692), self.lumi()["had" ]/self.lumi()["mcTtw"] ), #2010
+                "mcPhot":    excl(scaled((   110,      37,      28,      9,    4,    0.8,    0.5,    0), self.lumi()["phot"]/self.lumi()["mcPhot"]), self.photIsExcl()),
+                "mcPhotErr":      scaled((    10,       3,       3,      1,    1,    0.5,    0.4,    1), self.lumi()["phot"]/self.lumi()["mcPhot"]),
+                "mcZinv":    excl(scaled((    39,      16,      18,      9,  1.7,    0.4,    0.4,    0), self.lumi()["had"] /self.lumi()["mcZinv"]), self.photIsExcl()),
+                "mcZinvErr":      scaled((     4,       3,       3,      2,  0.9,    0.4,    0.4,    1), self.lumi()["had"] /self.lumi()["mcZinv"]),
                 }
-    
     def fixedParameters(self) :
         return {"sigmaLumi":  0.04,
                 "sigmaPhotZ": 0.40,
