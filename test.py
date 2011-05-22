e1c29b6373b6977a2a17edbb2f146717cd8b1acd
elaird <elaird>
2011-05-22 11:14:52 +0000

    comments

--- test.py
+++ test.py
@@ -6,14 +6,17 @@ f = fresh.foo(inputData = data.data(),
               REwk = ["", "FallingExp", "Constant"][2],
               RQcd = ["FallingExp", "Zero"][1],
 
-              ##LM1-like:
+              ##LM1-like (2011)
               #signalXs = 4.9, #pb
               #signalEff = {"had": (0.006,    0.01,    0.02,   0.029,  0.034,  0.023,  0.011,  0.005),
               #             "muon":(0.0,     0.001,   0.002,   0.003,  0.003,  0.002,  0.001,  0.0, )}
-              ##signalEff = {"had": (0.0,    0.0,    0.02,   0.10), #2010 binning
-              ##             "muon":(0.0,    0.0,    0.002,  0.01)}
 
-              #LM6-like:
+              ##LM1-like (2010)
+              #signalXs = 4.9, #pb              
+              #signalEff = {"had": (0.0,    0.0,    0.02,   0.10),
+              #             "muon":(0.0,    0.0,    0.002,  0.01)}
+
+              #LM6-like (2011)
               signalXs = 0.3104, #pb
               signalEff = {"had": (0.0,     0.0,     0.005,   0.012,  0.019,  0.022,  0.018,  0.029),
                            "muon":(0.0,     0.0,    0.0005,  0.0012, 0.0019, 0.0022, 0.0018, 0.0029)}
