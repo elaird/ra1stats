5cfce750d1ffdd4f3abaad8cec28a50c5a5e3350
elaird <elaird>
2011-05-22 11:52:00 +0000

    use new functions

--- job.py
+++ job.py
@@ -12,52 +12,37 @@ def writeNumbers(fileName = None, d = None) :
     cPickle.dump(d, outFile)
     outFile.close()
 
-def xs(point) :
-    return hp.loXsHisto().GetBinContent(*point)
-
-def xsFail(point) :
-    spec = hs.histoSpecs()["sig10"]
-    lumi = 1.0
-    h = hp.loYieldHisto(spec, [spec["beforeDir"]], lumi = lumi)
-    eventYield = h.GetBinContent(*point)
-    return eventYield/lumi
-
-def eff(point, label) :
-    out = [0.0, 0.0]
-    spec = hs.histoSpecs()[label]
-
-    for dir in [spec["350Dirs"], spec["450Dirs"]] :
-        num = hp.loYieldHisto(spec, dir, lumi = 1.0)
-        den = hp.loYieldHisto(spec, [spec["beforeDir"]], lumi = 1.0)
-        num.Divide(den)
-        out.append(num.GetBinContent(*point))
-    return tuple(out)
-    
 def go() :
     s = conf.switches()
+    data = conf.data()
+    bins = data.htBinLowerEdges()
+
     for point in points() :
-        x = xs(point)
+        x = hp.loXsHisto().GetBinContent(*point)
         signalEff = {}
-        signalEff["had" ] = eff(point, "sig10")
-        signalEff["muon"] = eff(point, "muon")
+        signalEff["had" ] = [hp.loEffHisto(box = "had", scale = "1", htLower = htLower, htUpper = htUpper).GetBinContent(*point)\
+                             for htLower, htUpper in zip(bins, list(bins[1:])+[None])]
+        signalEff["muon"] = [0.0]*len(bins)
+        #print x,signalEff
         
-        f = fresh.foo(REwk = s["REwk"],
+        f = fresh.foo(inputData = data,
+                      REwk = s["REwk"],
                       RQcd = s["RQcd"],
                       signalXs = x, signalEff = signalEff,
                       )
         ul = f.upperLimit()
-
+        
         out = {}
         out["upperLimit"] = (ul, "95% C.L. upper limit on XS factor")
-        out["xs"] = (x, "#sigma (pb)")
         out["excluded"] = (2.0*(ul<1.0) - 1.0, "is (upper limit on XS factor)<1?")
-        for i,bin in enumerate([250, 300, 350, 450]) :
+
+        out["xs"] = (x, "#sigma (pb)")
+        for i,bin in enumerate(bins) :
             for sel in ["had", "muon"] :
                 out["eff%s%d"%(sel, bin)] = (signalEff[sel][i], "#epsilon of %s %d selection"%(sel, bin))
         writeNumbers(conf.strings(*point)["pickledFileName"], out)
 
-profile = False
-if profile :
+if False :
     import cProfile
     cProfile.run("go()", "resultProfile.out")
 else :
