b6f475815984f392304bb5dd49e407739076a509
elaird <elaird>
2011-05-29 17:54:27 +0000

    accommodate move to inputData

--- test.py
+++ test.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 
 import fresh
-import data2011 as data
-f = fresh.foo(inputData = data.data(),
+from inputData import data2010,data2011
+f = fresh.foo(inputData = data2011(),
               REwk = ["", "FallingExp", "Constant"][2],
               RQcd = ["FallingExp", "Zero"][1],
 
@@ -16,10 +16,10 @@ f = fresh.foo(inputData = data.data(),
               #signalEff = {"had": (0.0,    0.0,    0.02,   0.10),
               #             "muon":(0.0,    0.0,    0.002,  0.01)}
 
-              #LM6-like (2011)
-              signalXs = 0.3104, #pb
-              signalEff = {"had": (0.0,     0.0,     0.005,   0.012,  0.019,  0.022,  0.018,  0.029),
-                           "muon":(0.0,     0.0,    0.0005,  0.0012, 0.0019, 0.0022, 0.0018, 0.0029)}
+              ##LM6-like (2011)
+              #signalXs = 0.3104, #pb
+              #signalEff = {"had": (0.0,     0.0,     0.005,   0.012,  0.019,  0.022,  0.018,  0.029),
+              #             "muon":(0.0,     0.0,    0.0005,  0.0012, 0.0019, 0.0022, 0.0018, 0.0029)}
               )
 
 #ul = f.upperLimit(makePlot = True); print "Upper limit:",ul
