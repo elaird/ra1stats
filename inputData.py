ef08c7338f720449bc30dfad131f5f194ee40253
elaird <elaird>
2011-05-31 18:43:52 +0000

    248/pb nHad and rename isExcl

--- inputData.py
+++ inputData.py
@@ -2,7 +2,7 @@ from data import data,scaled,excl
 
 class data2011(data) :
     def _fill(self) :
-        photIsExcl =                     (    1,     1,     0,     0,     0,     0,     0,     1)
+        isExcl =                         (    1,     1,     0,     0,     0,     0,     0,     1)
 
         self._htBinLowerEdges =          (275.0, 325.0, 375.0, 475.0, 575.0, 675.0, 775.0, 875.0)
         self._htMaxForPlot = 975.0
@@ -14,7 +14,8 @@ class data2011(data) :
         #self._constantMcRatioAfterHere = (    0,     0,     1,     0,     0)
         
         self._lumi = {
-            "had":     189.,
+            #"had":     189.,
+            "had":     248.,
             "hadBulk": 189.,
             
             "muon":    183., #mu  dataset (pt>25)
@@ -28,20 +29,21 @@ class data2011(data) :
             }
         self._htMeans = (     297.51,      347.25,      415.57,  516.2 , 617.17, 717.72, 818.33, 919.08)
         self._observations = {
-            "nHadBulk":  ( 9.047e+06,   3.793e+06,   2.623e+06,  823968, 289401, 112899,  48118,  65098),
-            "nHad":      (       130,          68,          47,      12,      5,      1,      0,      0),
-            #"nHad":      (       130,          68,        47.3,    12.7,      6,    2.2,    1.1,    1.6),#exp. LM6 added
-            "nPhot":excl((       142,          57,          43,      12,      2,      2,      2,      0), photIsExcl),
-            "nMuon":     (       177,          77,          13,      10,      4,      0,      0,      0), #mu  dataset (pt>25)
-            #"nMuon":    (        95,          73,          17,      12,      4,      0,      0,      0), #had dataset (pt>20)
+            "nHadBulk":scaled(( 9.047e+06,   3.793e+06,   2.623e+06,  823968, 289401, 112899,  48118,  65098), self.lumi()["had"]/self.lumi()["hadBulk"]),
+            #"nHad":           (       130,          68,          47,      12,      5,      1,      0,      0),#189/pb
+            #"nHad":           (       130,          68,        47.3,    12.7,      6,    2.2,    1.1,    1.6),#189/pb plus exp. LM6 added
+            "nHad":      excl((       164,          83,          77,      21,      8,      1,      0,      0), isExcl),#248/pb
+            "nPhot":     excl((       142,          57,          43,      12,      2,      2,      2,      0), isExcl),
+            "nMuon":          (       177,          77,          13,      10,      4,      0,      0,      0), #mu  dataset (pt>25)
+            #"nMuon":         (        95,          73,          17,      12,      4,      0,      0,      0), #had dataset (pt>20)
             }
         ep = 0.01        
         self._mcExpectations = {
             "mcMuon":         scaled((198.17,  100.06,   16.78,   4.82,   2.08,   1.13,   0.11, 0.02), self.lumi()["muon"]/self.lumi()["mcMuon"]), #mu  dataset (pt>25)
             #"mcMuon":        scaled((201.04,  101.8 ,   17.82,   4.86,   2.10,   1.13,   0.11, 0.03), self.lumi()["muon"]/self.lumi()["mcMuon"]), #had dataset (pt>20)
             "mcTtw":          scaled((  74.4,   27.82,   19.26,   7.17,   1.24,   1.05,   0.52, 0.55), self.lumi()["had" ]/self.lumi()["mcTtw" ]),
-            "mcPhot":    excl(scaled((   110,      37,      28,      9,      4,    0.8,    0.5,   ep), self.lumi()["phot"]/self.lumi()["mcPhot"]), photIsExcl),
-            "mcZinv":    excl(scaled((    39,      16,      18,      9,    1.7, ep+0.4,    0.4,   ep), self.lumi()["had"] /self.lumi()["mcZinv"]), photIsExcl),
+            "mcPhot":    excl(scaled((   110,      37,      28,      9,      4,    0.8,    0.5,   ep), self.lumi()["phot"]/self.lumi()["mcPhot"]), isExcl),
+            "mcZinv":    excl(scaled((    39,      16,      18,      9,    1.7, ep+0.4,    0.4,   ep), self.lumi()["had"] /self.lumi()["mcZinv"]), isExcl),
             }
         self._mcStatError = {
             "mcPhotErr":      scaled((    10,       3,       3,      1,      1,    0.5,    0.4,    1), self.lumi()["phot"]/self.lumi()["mcPhot"]),
