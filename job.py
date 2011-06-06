83ddb54f3ec926c55772c9db3b7dc92d3b7a739c
elaird <elaird>
2011-06-06 08:42:08 +0000

    organize and use muonEff

--- job.py
+++ job.py
@@ -15,6 +15,28 @@ def description(key, cl = None) :
     if key[:2]=="CL" : return key
     if key[-5:]=="Limit" : return "%g%% C.L. %s limit on XS factor"%(cl, key[:-5])
 
+def xs(switches, point) :
+    xsHisto = getattr(hp,"%sXsHisto"%("nlo" if switches["nlo"] else "lo"))
+    return xsHisto().GetBinContent(*point)
+    
+def signalEff(switches, data, binsInput, binsMerged, point) :
+    effHisto = getattr(hp,"%sEffHisto"%("nlo" if switches["nlo"] else "lo"))
+    out = {}
+    for item in ["had", "muon"] :
+        out[item] = [effHisto(box = item, scale = "1", htLower = htLower, htUpper = htUpper).GetBinContent(*point)\
+                     for htLower, htUpper in zip(binsInput, list(binsInput[1:])+[None])]
+        out[item] = data.mergeEfficiency(out[item])
+    #out["muon"] = tuple([0.0]*len(out["muon"])); print "HACK: muon sig eff set to 0"
+    return out
+
+def stuffVars(binsMerged, x, eff) :
+    out = {}
+    out["xs"] = (x, "#sigma (pb)")
+    for i,bin in enumerate(binsMerged) :
+        for sel in ["had", "muon"] :
+            out["eff%s%d"%(sel, bin)] = (eff[sel][i], "#epsilon of %s %d selection"%(sel, bin))
+    return out
+
 def go() :
     s = conf.switches()
     data = conf.data()
@@ -22,27 +44,12 @@ def go() :
     binsMerged = data.htBinLowerEdges()
     
     for point in points() :
-        xsHisto  = getattr(hp,"%sXsHisto"%("nlo" if s["nlo"] else "lo"))
-        effHisto = getattr(hp,"%sEffHisto"%("nlo" if s["nlo"] else "lo"))
-        
-        x = xsHisto().GetBinContent(*point)
-        signalEff = {}
-        signalEff["had" ] = [effHisto(box = "had", scale = "1", htLower = htLower, htUpper = htUpper).GetBinContent(*point)\
-                             for htLower, htUpper in zip(binsInput, list(binsInput[1:])+[None])]
-        signalEff["muon"] = [0.0]*len(binsInput)
-
-        for item in ["had","muon"] :
-            signalEff[item] = data.mergeEfficiency(signalEff[item])
-        #print x,signalEff
-
-        out = {}
-        out["xs"] = (x, "#sigma (pb)")
-        for i,bin in enumerate(binsMerged) :
-            for sel in ["had", "muon"] :
-                out["eff%s%d"%(sel, bin)] = (signalEff[sel][i], "#epsilon of %s %d selection"%(sel, bin))
+        x = xs(s, point)
+        eff = signalEff(s, data, binsInput, binsMerged, point)
+        out = stuffVars(binsMerged, x, eff)
 
         if "CLs" in s["method"] :
-            f = fresh.foo(inputData = data, REwk = s["REwk"], RQcd = s["RQcd"], signalXs = x, signalEff = signalEff)            
+            f = fresh.foo(inputData = data, REwk = s["REwk"], RQcd = s["RQcd"], signalXs = x, signalEff = eff)
             results = f.cls(method = s["method"], nToys = s["nToys"])
             for key,value in results.iteritems() : out[key] = (value, description(key))
             for cl in s["CL"] :
@@ -50,12 +57,11 @@ def go() :
                 out["excluded%g"%(100*cl)] = (2.0*(results["CLs"]<value) - 1.0, "is CLs<%g ?"%value)
         else :
             for cl in s["CL"] :
-                f = fresh.foo(inputData = data, REwk = s["REwk"], RQcd = s["RQcd"], signalXs = x, signalEff = signalEff)
+                f = fresh.foo(inputData = data, REwk = s["REwk"], RQcd = s["RQcd"], signalXs = x, signalEff = eff)
                 results = f.interval(cl = cl, method = s["method"])
                 cl2 = 100*cl
                 for key,value in results.iteritems() : out["%s%g"%(key, cl2)] = (value, description(key, cl2))
                 out["excluded%g"%cl2] = (2.0*(results["upperLimit"]<1.0) - 1.0, "is (%g%% upper limit on XS factor)<1?"%cl2)
-
         writeNumbers(conf.strings(*point)["pickledFileName"], out)
 
 if False :
