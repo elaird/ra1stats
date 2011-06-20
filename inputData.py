d6516c58a37344df81e8c6081ec7070d70e6a956
elaird <elaird>
2011-06-20 21:44:32 +0000

    update data

--- inputData.py
+++ inputData.py
@@ -14,12 +14,12 @@ class data2011_2(data) :
         #self._constantMcRatioAfterHere = (    0,     0,     1,     0,     0)
 
         self._lumi = {
-            "had":     441.,
-            "hadBulk": 441.,
+            "had":     602.,
+            "hadBulk": 602.,
             
-            "muon":    361.,
-            "mcMuon":  353.,
-            "mcTtw":   353.,
+            "muon":    448.,
+            "mcMuon":  441.,
+            "mcTtw":   441.,
 
             "phot":    468.8,
             "mcPhot":  468.8,
@@ -30,13 +30,13 @@ class data2011_2(data) :
             "nHadBulk":scaled(( 3.234e+07,   1.328e+07,   9.086e+06,  2.870e+06, 998416, 387024, 162452, 145889), self.lumi()["had"]/self.lumi()["hadBulk"]),
             "nHad":           (       458,         189,         109,         37,     18,      2,      2,      1),
             "nPhot":     excl((       402,         149,         139,         55,     21,      9,      4,      1), isExcl),
-            "nMuon":          (       114,          42,          35,         13,      6,      1,      0,      0),
+            "nMuon":          (       146,          53,          39,         17,      7,      1,      0,      0),
             }
 
         ep = 0.01        
         self._mcExpectations = {
-            "mcMuon":         scaled((121.78,   46.29,   31.85,  10.36,   3.63,   2.11,   0.15, 0.05), self.lumi()["muon"]/self.lumi()["mcMuon"]),
-            "mcTtw":          scaled((126.25,   47.82,   30.62,  11.29,   1.92,   1.81,   0.88, 0.25), self.lumi()["had" ]/self.lumi()["mcTtw"] ),
+            "mcMuon":         scaled((152.05,   57.80,   39.77,  12.93,   4.53,   2.63,   0.19, 0.06), self.lumi()["muon"]/self.lumi()["mcMuon"]),
+            "mcTtw":          scaled((157.63,   59.71,   38.23,  14.09,   2.40,   2.26,   1.10, 0.31), self.lumi()["had" ]/self.lumi()["mcTtw"] ),
             "mcPhot":    excl(scaled((   290,     112,     109,     37,     14,      3,    2.1,  0.9), self.lumi()["phot"]/self.lumi()["mcPhot"]), isExcl),
             "mcZinv":    excl(scaled((    90,      41,      51,     24,      4,   ep+1,      1,   ep), self.lumi()["had"] /self.lumi()["mcZinv"]), isExcl),
             }
@@ -52,6 +52,12 @@ class data2011_2(data) :
         # (3% ECAL, 2.5% vetoes, 2.5% JES and JER + the lumi uncert.) PDF uncertainties we used 10%.
         
 class data2011_2_no_cleaning_cuts(data) :
+    def addLists(self, l1, l2) :
+        out = []
+        for a,b in zip(l1,l2) :
+            out.append(a+b)
+        return out
+        
     def _fill(self) :
         isExcl =                         (    1,     1,     0,     0,     0,     0,     0,     1)
 
@@ -65,35 +71,34 @@ class data2011_2_no_cleaning_cuts(data) :
         #self._constantMcRatioAfterHere = (    0,     0,     1,     0,     0)
 
         self._lumi = {
-            "had":     441.,
-            "hadBulk": 441.,
+            "had":     602.,
+            "hadBulk": 602.,
             
-            "muon":    361.,
-            "mcMuon":  353.,
-            "mcTtw":   353.,
+            "muon":    448.,
+            "mcMuon":  441.,
+            "mcTtw":   441.,
 
             "phot":    468.8,
             "mcPhot":  468.8,
-            "mcZinv":  468.8,
             }
         self._htMeans =       (    297.51,      347.25,      415.57,     516.2 , 617.17, 717.72, 818.33, 919.08)
         self._observations = {
-            "nHadBulk":scaled(( 3.234e+07,   1.328e+07,   9.086e+06,  2.870e+06, 998416, 387024, 162452, 145889), self.lumi()["had"]/self.lumi()["hadBulk"]),
+            "nHadBulk":scaled(( 3.234e+07,   1.328e+07,   9.086e+06,  2.870e+06, 998415, 387023, 162452, 145889), self.lumi()["had"]/self.lumi()["hadBulk"]),
             "nHad":           (      2398,         633,         260,         56,     19,      3,      2,      1),
             "nPhot":     excl((       402,         149,         139,         55,     21,      9,      4,      1), isExcl),
-            "nMuon":          (       114,          42,          35,         13,      6,      1,      0,      0),
+            "nMuon":          (       146,          53,          39,         17,      7,      1,      0,      0),            
             }
 
         ep = 0.01        
         self._mcExpectations = {
             "mcMuon":         scaled((121.78,   46.29,   31.85,  10.36,   3.63,   2.11,   0.15, 0.05), self.lumi()["muon"]/self.lumi()["mcMuon"]),
-            "mcTtw":          scaled((126.25,   47.82,   30.62,  11.29,   1.92,   1.81,   0.88, 0.25), self.lumi()["had" ]/self.lumi()["mcTtw"] ),
+            "mcTtw": self.addLists([137.,  47.5,  32.8,  12.7,  0.91,  1.82,  0.91,  0.91], [79.1,  37.9,  27.4,  9.51,  3.08,  1.11,  0.37,  0.43]),
             "mcPhot":    excl(scaled((   290,     112,     109,     37,     14,      3,    2.1,  0.9), self.lumi()["phot"]/self.lumi()["mcPhot"]), isExcl),
-            "mcZinv":    excl(scaled((    90,      41,      51,     24,      4,   ep+1,      1,   ep), self.lumi()["had"] /self.lumi()["mcZinv"]), isExcl),
+            "mcZinv":                (  102.,    44.2,    32.5,   19.7,   3.49,      0,   1.16,    0),
             }
         self._mcStatError = {
             "mcPhotErr":      scaled((    20,       6,       6,      3,      2,      1,    0.8,  0.5), self.lumi()["phot"]/self.lumi()["mcPhot"]),
-            "mcZinvErr":      scaled((    10,       7,       8,      5,      2,      1,      1,    1), self.lumi()["had"] /self.lumi()["mcZinv"]),
+           #"mcZinvErr":      scaled((    10,       7,       8,      5,      2,      1,      1,    1), self.lumi()["had"] /self.lumi()["mcZinv"]),
             }
         self._fixedParameters = {
             "sigmaLumi":  0.04,
