2f0e8c3d1e784b4f1e2d92f97bdf4ed245b095ab
elaird <elaird>
2011-06-25 14:50:24 +0000

    fits bands etc.

--- ratios.py
+++ ratios.py
@@ -6,13 +6,16 @@ import ROOT as r
 
 def setupRoot() :
     r.gROOT.SetBatch(True)
+    r.gErrorIgnoreLevel = 2000
     r.gROOT.SetStyle("Plain")
+    r.gStyle.SetOptStat(0)
+    r.gStyle.SetOptFit(1011)
 
 def histo(inputData, name, title = "") :
     bins = array.array('d', list(inputData.htBinLowerEdges())+[inputData.htMaxForPlot()])
     out = r.TH1D(name, "%s;H_{T} (GeV);ratio"%title, len(bins)-1, bins)
     out.Sumw2()
-    out.SetStats(False)
+    #out.SetStats(False)
     return out
 
 def fill(h, contents, errors) :
@@ -21,7 +24,7 @@ def fill(h, contents, errors) :
         h.SetBinContent(1+i, c)
         h.SetBinError(1+i, e)
 
-def go(label = None, yLabel = "", title = "", minMax = (0.0, 2.0), mcPhot = None, mcPhotErr = None, mcZinv = None, mcZinvErr = None, scale = 1.0) :
+def go(label = None, yLabel = "", title = "", minMax = (0.0, 2.0), mcPhot = None, mcPhotErr = None, mcZinv = None, mcZinvErr = None, sysFactor = None, scale = 1.0) :
     ph = histo(data, "ph")
     fill(ph, mcPhot, mcPhotErr)
     
@@ -33,7 +36,26 @@ def go(label = None, yLabel = "", title = "", minMax = (0.0, 2.0), mcPhot = None
     z.GetYaxis().SetTitle(yLabel)
     z.GetYaxis().SetRangeUser(*minMax)
     z.Draw()
-    
+
+    z.Fit("pol0", "fq")
+    f = z.GetListOfFunctions().At(0)
+    value = f.GetParameter(0)    
+    f.SetLineColor(r.kBlue)
+    f.SetLineWidth(2)
+
+    b = r.TBox()
+    b.SetFillStyle(1)
+    b.SetLineColor(r.kRed)
+    box = b.DrawBox(z.GetXaxis().GetXmin(), value*(1.0-sysFactor), z.GetXaxis().GetXmax(), value*(1.0+sysFactor))
+
+    leg = r.TLegend(0.15, 0.85, 0.65, 0.65)
+    leg.SetBorderSize(0)
+    leg.SetFillStyle(0)
+    leg.AddEntry(z, "ratio from MC (error bars are from MC stats)", "pl")
+    leg.AddEntry(f, "fit to constant", "l")
+    leg.AddEntry(b, "systematic error band (theo. + exp.)", "l")
+
+    leg.Draw()
     fileName = "%s.eps"%label
     r.gPad.SetTickx()
     r.gPad.SetTicky()
@@ -51,6 +73,7 @@ go(label = "photons",
    mcPhotErr = data.mcStatError()["mcPhotErr"],
    mcZinv = data.mcExpectations()["mcZinv"],
    mcZinvErr = data.mcStatError()["mcZinvErr"],
+   sysFactor = data.fixedParameters()["sigmaPhotZ"],
    scale = data.lumi()["phot"]/data.lumi()["had"]
    )
 
@@ -62,5 +85,6 @@ go(label = "muons",
    mcPhotErr = data.mcStatError()["mcMuonErr"],
    mcZinv = data.mcExpectations()["mcTtw"],
    mcZinvErr = data.mcStatError()["mcTtwErr"],
+   sysFactor = data.fixedParameters()["sigmaMuonW"],   
    scale = data.lumi()["muon"]/data.lumi()["had"]
    )
