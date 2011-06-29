78b3a7dca779ad89fe3a706f235db43b7e52f924
elaird <elaird>
2011-06-29 13:36:54 +0000

    2jet numbers

--- inputData.py
+++ inputData.py
@@ -8,16 +8,15 @@ class data2011_3(data) :
 
         self._htBinLowerEdges =          (275.0, 325.0, 375.0, 475.0, 575.0, 675.0, 775.0, 875.0)
         self._htMaxForPlot = 975.0
-
+        
         self._mergeBins = None
         self._constantMcRatioAfterHere = (    1,     0,     0,     0,     0,     0,     0,     0)
-        self._hadControlMax            = (    0,     1,     0,     0,     0,     0,     0,     0)
         
-        #self._mergeBins =                (    0,     1,     2,     2,     2,     2,     2,     2)
-        #self._constantMcRatioAfterHere = (    0,     0,     1)
+        #self._mergeBins =                (    0,     1,     2,     3,     4,     4,     4,     4)
+        #self._constantMcRatioAfterHere = (    1,     0,     0,     0,     0)
 
         #self._mergeBins =                (    0,     1,     2,     3,     3,     4,     4,     4)
-        #self._constantMcRatioAfterHere = (    0,     0,     0,     0,     0)
+        #self._constantMcRatioAfterHere = (    1,     0,     0,     0,     0)
 
         self._lumi = {
             "had":     769.,
@@ -40,21 +39,27 @@ class data2011_3(data) :
             "nHad":           ( 5.720e+02, 2.370e+02, 1.400e+02, 4.500e+01, 1.800e+01, 3.000e+00, 2.000e+00, 1.000e+00),
             "nHad53":         ( 1.809e+03, 4.000e+02, 2.230e+02, 6.900e+01, 3.200e+01, 1.000e+01, 4.000e+00, 2.000e+00),
             "nPhot":     excl((       630,       227,       233,        81,        33,        15,         6,         3), isExcl),
+            "nPhot2Jet": excl((       255,        94,        99,        31,        11,         3,         1,         0), isExcl), #not used in fit
             "nMuon":          (       262,       100,        78,        31,        12,         4,         0,         0),
-            "nMumu":     excl((        22,         5,        11,         6,         3,         0,         0,         0), isExcl),            
+            "nMuon2Jet":      (        86,        23,        25,        10,         2,         0,         0,         0), #not used in fit
+            "nMumu":     excl((        22,         5,        11,         6,         3,         0,         0,         0), isExcl),
             }
         self._observations["nHadControl"] = tuple([n53-n55 for n53,n55 in zip(self._observations["nHad53"], self._observations["nHad"])])
         self._mcExpectations = {
-            "mcMuon":         scaled((252.07,  104.36,   67.61,  24.04,   9.39,   4.37,   0.32, 0.22), self.lumi()["muon"]/self.lumi()["mcMuon"]),
-            "mcTtw":          scaled((274.87,  104.11,   66.67,  24.58,   4.18,   3.94,   1.92, 0.54), self.lumi()["had" ]/self.lumi()["mcTtw"] ),
-            "mcGjets":   excl(scaled((   440,     190,     181,     62,     22,      5,      4,  1.5), self.lumi()["phot"]/self.lumi()["mcGjets"]), isExcl),
-            "mcZinv":    excl(scaled((    90,      41,      51,     24,      4,      1,      1,    0), self.lumi()["had"] /self.lumi()["mcZinv"]), isExcl),
-            "mcZmumu":   excl(scaled((    15,       9,      11,      7,      3,    0.9,      0,    0), self.lumi()["mumu"]/self.lumi()["mcZmumu"]), isExcl),
+            "mcMuon":          scaled((252.07,  104.36,   67.61,  24.04,   9.39,   4.37,   0.32, 0.22), self.lumi()["muon"]/self.lumi()["mcMuon"]),
+            "mcMuon2Jet":      scaled(( 86.03,   28.51,   25.63,   2.02,   4.78,   3.29,  0.107,    0), self.lumi()["muon"]/self.lumi()["mcMuon"]),
+            "mcTtw":           scaled((274.87,  104.11,   66.67,  24.58,   4.18,   3.94,   1.92, 0.54), self.lumi()["had" ]/self.lumi()["mcTtw"] ),
+            "mcGjets":    excl(scaled((   440,     190,     181,     62,     22,      5,      4,  1.5), self.lumi()["phot"]/self.lumi()["mcGjets"]), isExcl),
+            "mcPhot2Jet": excl(scaled((   210,      91,      71,     20,      8,    0.4,    0.4,  0.4), self.lumi()["phot"]/self.lumi()["mcGjets"]), isExcl),
+            "mcZinv":     excl(scaled((    90,      41,      51,     24,      4,      1,      1,    0), self.lumi()["had"] /self.lumi()["mcZinv"]), isExcl),
+            "mcZmumu":    excl(scaled((    15,       9,      11,      7,      3,    0.9,      0,    0), self.lumi()["mumu"]/self.lumi()["mcZmumu"]), isExcl),
             }
         self._mcStatError = {
             "mcMuonErr":             ( 14.79,   10.39,    8.91,   5.39,  3.244,  2.286,  0.186, 0.152),
+            "mcMuon2JetErr":         ( 11.59,    7.23,    6.92,   2.84,   2.83,   2.31,   0.11,   0.1),
             "mcTtwErr":              ( 17.58,   10.41,    8.47,   5.40,   2.32,   2.28,   1.60,  0.24),
             "mcGjetsErr":     scaled((    20,      10,      10,      6,      4,      2,      1,   0.9), self.lumi()["phot"]/self.lumi()["mcGjets"]),
+            "mcPhot2JetErr":  scaled((    20,      10,       6,      3,      2,    0.4,    0.4,   0.4), self.lumi()["phot"]/self.lumi()["mcGjets"]),
             "mcZinvErr":      scaled((    10,       7,       8,      5,      2,      1,      1,     1), self.lumi()["had"] /self.lumi()["mcZinv"]),
             "mcZmumuErr":     scaled((     4,       3,       3,      3,      2,      1,      1,     1), self.lumi()["mumu"]/self.lumi()["mcZmumu"]),
             }
