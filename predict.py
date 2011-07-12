538100350fe3e5bb03b445ad1183d24aebeffd9f
elaird <elaird>
2011-07-12 20:29:44 +0000

    include mc stat errors

--- predict.py
+++ predict.py
@@ -1,23 +1,48 @@
 #!/usr/bin/env python
 
 from inputData import data2011
-import math
+import math,array
+import ROOT as r
+
+def histo(inputData, name, title = "") :
+    bins = array.array('d', list(inputData.htBinLowerEdges())+[inputData.htMaxForPlot()])
+    out = r.TH1D(name, "%s;H_{T} (GeV);ratio"%title, len(bins)-1, bins)
+    out.Sumw2()
+    #out.SetStats(False)
+    return out
+
+def fill(h, contents, errors) :
+    h.Reset()
+    for i,c,e in zip(range(len(contents)), contents, errors) :
+        h.SetBinContent(1+i, c)
+        h.SetBinError(1+i, e)
+    return h
 
 data = data2011()
 
-htBins   = data.htBinLowerEdges()
-constR   = data.constantMcRatioAfterHere()
-nMuon    = data.observations()["nMuon2Jet"]
-mcMuon   = data.mcExpectations()["mcMuon2Jet"]
-nPhot    = data.observations()["nPhot2Jet"]
-mcPhot   = data.mcExpectations()["mcPhot2Jet"]
+htBins    = data.htBinLowerEdges()
+constR    = data.constantMcRatioAfterHere()
+nMuon     = data.observations()["nMuon2Jet"]
+mcMuon    = data.mcExpectations()["mcMuon2Jet"]
+mcMuonErr = data.mcStatError()["mcMuon2JetErr"]
+nPhot     = data.observations()["nPhot2Jet"]
+mcPhot    = data.mcExpectations()["mcPhot2Jet"]
+mcPhotErr = data.mcStatError()["mcPhot2JetErr"]
+syst      = data.fixedParameters()["sigmaPhotZ"]
 
-print     "HT   nPhoton    MC ratio       prediction                         nMuon"
-print     "-----------------------------------------------------------------------"
+muon = fill(histo(data, name = "mcMuon2Jet", title = "mcMuon2Jet"), mcMuon, mcMuonErr)
+phot = fill(histo(data, name = "mcPhot2Jet", title = "mcPhot2Jet"), mcPhot, mcPhotErr)
+muon.Divide(phot)
+
+print     "HT   nPhoton    MC ratio(err)       prediction                                            nMuon"
+print     "-----------------------------------------------------------------------------------------------"
 rFinal = None
-for i,ht,stopHere,nPh,mcPh,nMu,mcMu in zip(range(len(htBins)), htBins, constR, nPhot, mcPhot, nMuon, mcMuon) :
-    if stopHere :
-        rFinal = sum(mcMuon[i:])/sum(mcPhot[i:])
-        #rFinal = 0.38
-    ratio = rFinal if rFinal else mcMu/mcPh
-    print "%3d     %4d  *     %4.2g  = %5.1f +- %4.1f (stat) +- %5.1f (syst); %5d"%(ht, nPh, ratio, nPh*ratio, math.sqrt(nPh)*ratio, 0.4*nPh*ratio, nMu)
+for i,ht,nPh,mcPh,nMu,mcMu in zip(range(len(htBins)), htBins, nPhot, mcPhot, nMuon, mcMuon) :
+    mcRatio    = muon.GetBinContent(1+i)
+    mcRatioErr = muon.GetBinError(1+i)
+    out  = "%3d     %4d  *     %4.2f(+-%4.2f) = %5.1f "%(ht, nPh, mcRatio, mcRatioErr, nPh*mcRatio)
+    out += "+- %4.1f (stat) "%(math.sqrt(nPh)*mcRatio)
+    out += "+- %4.1f (mcStat) "%(nPh*mcRatioErr)
+    out += "+- %5.1f (syst)"%(syst*nPh*mcRatio)
+    out += "; %5d"%nMu
+    print out
