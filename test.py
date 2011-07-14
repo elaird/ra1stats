5ca06002aebab89fa9ee4f606fe6f1b36b0021a5
elaird <elaird>
2011-07-14 15:18:20 +0000

    add example

--- test.py
+++ test.py
@@ -5,10 +5,6 @@ from inputData import data2010,data2011
 
 def scaled(t, factor) : return tuple([factor*item for item in t])
 
-lm1_2011 = {"xs": 4.9,
-            "effHad":  (0.006,    0.01,    0.02,   0.029,  0.034,  0.023,  0.011,  0.005),
-            "effMuon": (0.0,     0.001,   0.002,   0.003,  0.003,  0.002,  0.001,  0.0, )}
-
 lm1_2010 = {"xs": 4.9,
             "effHad":  (0.0,    0.0,    0.02,   0.10),
             "effMuon": (0.0,    0.0,    0.002,  0.01)}
@@ -18,6 +14,11 @@ lm6_2011 = {"xs": 0.3104,
            #"effMuon":(0.0,     0.0,     0.005/6, 0.012/6,0.019/6,0.022/6,0.018/6,0.029/6),#approx. for pT>20 GeV
             "effMuon":scaled((0.045, 0.045, 0.1568, 0.245, 0.3254, 0.3481, 0.2836, 0.3618), 1.0e-2),#pT>10 GeV, |eta|<2.5
             }
+
+p_2011 = {"xs":0.0413,
+          "effMuon":[0.000426, 0.000596, 0.001506, 0.000771, 0.000939, 0.000613, 0.000846, 0.005793],
+          "effHad": [0.003254, 0.003499, 0.004932, 0.005420, 0.007190, 0.010089, 0.014170, 0.073909],
+          }
 
 filips_point1 = {'xs': 0.0909,
                 'effMuon': [0.0003, 0.0003, 0.0004, 0.0007, 0.0012, 0.0011, 0.0015, 0.0032],
@@ -36,8 +37,9 @@ f = fresh.foo(inputData = data2011(),
               RQcd = ["Zero", "FallingExp"][1],
               nFZinv = ["All", "One", "Two"][0],
               #qcdSearch = True,
-              #signal = [{}, lm6_2011, lm1_2011, lm1_2010, filips_point2, sue_anns_point][1],
+              #signal = [{}, lm6_2011, lm1_2010, filips_point2, sue_anns_point][1],
               signalExampleToStack = ("LM6 (LO)", lm6_2011),
+              #signalExampleToStack = ("m0=280 GeV, m12=540 GeV (NLO)", p_2011),
               #signalExampleToStack = ("m0=540 GeV, m12=440 GeV", filips_point1),
               #signalExampleToStack = ("m_{0} = 500 GeV;  m_{1/2} = 440 GeV", filips_point2),
               #signalExampleToStack = ("m0=100 GeV, m12=100 GeV", sue_anns_point),
