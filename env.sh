cda3ed9ec2d27ebc5bc654c74aabad17799a9d0a
elaird <elaird>
2011-07-17 21:17:24 +0000

    name change

--- env.sh
+++ env.sh
@@ -2,7 +2,12 @@
 #http://root.cern.ch/phpBB3/viewtopic.php?f=3&t=11374&p=49129&hilit=GLIBCXX_3.4.9#p49129
 
 TAG=x86_64-slc5-gcc43-opt
-BASEDIR=/vols/cms02/elaird1/18_root_from_afs/lcg/
+
+if [[ "$HOSTNAME" == *hep.ph.ic.ac.uk ]]; then
+    BASEDIR=/vols/cms02/elaird1/18_root_from_afs/lcg/
+else BASEDIR=/afs/cern.ch/sw/lcg/
+fi
+
 source ${BASEDIR}/contrib/gcc/4.3.2/${TAG}/setup.sh ${BASEDIR}/contrib
 source ${BASEDIR}/app/releases/ROOT/5.30.00/${TAG}/root/bin/thisroot.sh
 
