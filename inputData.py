4a9bd1328ea9731524535acf6197f6e09074d4ec
elaird <elaird>
2011-07-23 20:14:13 +0000

    comment

--- inputData.py
+++ inputData.py
@@ -105,7 +105,7 @@ class data2011_4(data) :
         
         self._fixedParameters = {
             "sigmaLumiLike": utils.quadSum({"lumi": 0.06, "deadEcal": 0.03, "lepVetoes": 0.025, "jesjer": 0.025, "pdf": 0.10}.values()),
-            #"sigmaLumiLike": 0.06,
+            #"sigmaLumiLike": 0.0001,
             "sigmaPhotZ": 0.40,
             "sigmaMuonW": 0.30,
             "sigmaMumuZ": 0.20,
