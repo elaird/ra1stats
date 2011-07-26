3a77588f7b935886e1e59368dce2064a211685ed
elaird <elaird>
2011-07-26 12:17:50 +0000

    multiple channels

--- cls.py
+++ cls.py
@@ -3,58 +3,65 @@
 import os,math
 import ROOT as r
 
-def like(n, mu) : return r.TMath.Poisson(n, mu)
-#def like(x, mu) : return r.TMath.Gaus(x, mu, 1.0)
+def like(nList, muList) :
+    out = 1.0
+    for n,mu in zip(nList, muList) :
+        out *= r.TMath.Poisson(n, mu)
+    return out
 
 def hist(values, name, title, nBins = 1000, min = 1, max = -1) :
     out = r.TH1D(name, title, nBins, min, max)
     map(out.Fill, values)
     return out
 
+def indexFraction(item, l) :
+    totalList = sorted(l+[item])
+    i1 = totalList.index(item)
+    totalList.reverse()
+    i2 = len(totalList)-totalList.index(item)-1
+    return (i1+i2)/2.0/len(l), (i2-i1)/2.0/len(l)
+
 def draw(items) :
     for i,item in enumerate(items) :
         item.Draw("" if not i else "same")
 
 class foo(object) :
-    def __init__(self, b = 3.0, s = 2.0, obs = 1, tsMin = None, tsMax = None, tsIndex = 0) :
+    def __init__(self, bList = [3.0, 3.0], sList = [2.0, 6.0], obsList = [3, 1], tsMin = None, tsMax = None, tsIndex = 0, nToys = 8000) :
         self.rand = r.TRandom3()
         self.rand.SetSeed(1)
 
-        self.nToys = 80000
         tsPair = [(self.tsN, "num events"),
                   (self.tsLLR, "log( L(n | s+b) / L(n | b) )"),
                   (self.tsLLRm, "log( L(n | b) / L(n | s+b) )"),
                   (self.tsLH, "log( L(n | b) )")][tsIndex]
-        self.ts = tsPair[0]
-        self.tsDesc = tsPair[1]
 
-        for item in ["b", "s", "obs", "tsMin", "tsMax"] :
+        self.ts,self.tsDesc = tsPair
+
+        for item in ["nToys", "bList", "sList", "obsList", "tsMin", "tsMax"] :
             setattr(self, item, eval(item))
+        self.sbList = [b+s for b,s in zip(self.bList, self.sList)]
 
-        self.stuff = []
+    def tsN(self, nList) :
+        return sum(nList)
 
-    def keep(self, x) :
-        self.stuff.append(x)
+    def tsLH(self, nList) :
+        return math.log(like(nList, self.bList))
         
-    def tsN(self, n) :
-        return n
-
-    def tsLH(self, n) :
-        return math.log(like(n, self.b))
+    def tsLLR(self, nList) :
+        return math.log(like(nList, self.sbList)/like(nList, self.bList))
         
-    def tsLLR(self, n) :
-        return math.log(like(n, self.b + self.s)/like(n, self.b))
+    def tsLLRm(self, nList) :
+        return -self.tsLLR(nList)
         
-    def tsLLRm(self, n) :
-        return -self.tsLLR(n)
-        
-    def tsValues(self, mu) :
-        return [self.ts(self.rand.Poisson(mu)) for i in range(self.nToys)]
-        #return [self.ts(self.rand.Gaus(mu, 1.0)) for i in range(self.nToys)]
+    def tsValues(self, muList) :
+        out = []
+        for i in range(self.nToys) :
+            out.append( self.ts([self.rand.Poisson(mu) for mu in muList]) )
+        return out
         
     def tsPlot(self, canvas, psFile) :
-        bValues = self.tsValues(self.b)
-        sbValues = self.tsValues(self.b + self.s)
+        bValues = self.tsValues(self.bList)
+        sbValues = self.tsValues(self.sbList)
 
         minimum = min(bValues + sbValues) if not self.tsMin else self.tsMin
         maximum = max(bValues + sbValues) if not self.tsMax else self.tsMax
@@ -62,24 +69,20 @@ class foo(object) :
         sbHist = hist(sbValues, name = "sb", title = ";%s;toys / bin"%self.tsDesc, min = minimum, max = maximum)
 
         yMax = max(bHist.GetMaximum(), sbHist.GetMaximum())
-        x = self.ts(self.obs)
+        x = self.ts(self.obsList)
         obsLine = r.TLine(x, 0.0, x, yMax)
         obsLine.SetLineWidth(1)
         obsLine.SetLineStyle(3)
 
-        center = bHist.Integral(0, bHist.FindBin(x))
-        plus   = bHist.Integral(0, bHist.FindBin(x)+1)
-        minus  = bHist.Integral(0, bHist.FindBin(x)-1)
-        total  = bHist.Integral(0, bHist.GetNbinsX()+1)
-        text = r.TText(bHist.GetXaxis().GetXmin(), bHist.GetMaximum(),
-                       "CLb = %6.4f +- %6.4f"%(center/total, (plus-minus)/total))
+        pValue,pValueError = indexFraction(x, bValues)
+        text = r.TText(bHist.GetXaxis().GetXmin(), bHist.GetMaximum(), "CLb = %6.4f +- %6.4f"%(pValue, pValueError))
             
         leg = r.TLegend(0.5, 0.7, 0.9, 0.9)
         leg.SetBorderSize(0)
         leg.SetFillStyle(0)
-        leg.AddEntry(bHist, "b hypothesis; b = %g"%self.b, "l")
-        leg.AddEntry(sbHist, "s + b hypothesis; s = %g"%self.s, "l")
-        leg.AddEntry(obsLine, "observed value", "l")
+        leg.AddEntry(bHist, "b hypothesis; b = %s"%str(self.bList), "l")
+        leg.AddEntry(sbHist, "s + b hypothesis; s = %s"%str(self.sList), "l")
+        leg.AddEntry(obsLine, "observed value; n = %s"%str(self.obsList), "l")
 
         bHist.SetLineColor(r.kBlue)
         sbHist.SetLineColor(r.kRed)
@@ -90,31 +93,41 @@ class foo(object) :
         draw([bHist, sbHist, obsLine, leg, text])
         canvas.Print(psFile)
 
-psFile = "out.ps"
-canvas = r.TCanvas()
-canvas.Print(psFile+"[")
+def go(name = "out", items = []) :
+    psFile = "%s.ps"%name
+    canvas = r.TCanvas()
+    canvas.Print(psFile+"[")
 
-#s1 = 29.0
-#s2 = 3.08
+    for item in items :
+        item.tsPlot(canvas, psFile)
 
-#items  = []
-#items += [foo(s = s1, tsIndex = 0, tsMin = 0.0, tsMax = 50.0)]
-#items += [foo(s = s2, tsIndex = 0, tsMin = 0.0, tsMax = 50.0)]
-#items += [foo(s = s1, tsIndex = 1)]
-#items += [foo(s = s2, tsIndex = 1)]
-#items += [foo(s = s1, tsIndex = 2, tsMin = -50.0, tsMax =  0.0)]
-#items += [foo(s = s2, tsIndex = 2, tsMin = -50.0, tsMax =  0.0)]
+    canvas.Print(psFile+"]")
+    os.system("ps2pdf %s"%psFile)
+    os.remove(psFile)
 
-s1 = 30.0
-s2 =  3.0
-
-items  = []
-items += [foo(s = s1, tsIndex = 2)]
-items += [foo(s = s2, tsIndex = 2)]
-
-for item in items :
-    item.tsPlot(canvas, psFile)
-
-canvas.Print(psFile+"]")
-os.system("ps2pdf %s"%psFile)
-os.remove(psFile)
+nToys = 8000
+go("oneBin_varying_sOverb", [foo(sList = [60.0], bList = [3.0], obsList = [1], tsIndex = 2, nToys = nToys), 
+                             foo(sList = [30.0], bList = [3.0], obsList = [1], tsIndex = 2, nToys = nToys),
+                             foo(sList = [12.0], bList = [3.0], obsList = [1], tsIndex = 2, nToys = nToys),
+                             foo(sList = [ 6.0], bList = [3.0], obsList = [1], tsIndex = 2, nToys = nToys),
+                             foo(sList = [ 3.0], bList = [3.0], obsList = [1], tsIndex = 2, nToys = nToys),
+                             foo(sList = [ 1.0], bList = [3.0], obsList = [1], tsIndex = 2, nToys = nToys),
+                             foo(sList = [0.25], bList = [3.0], obsList = [1], tsIndex = 2, nToys = nToys),
+                             ])
+   
+go("multiBin_varying_sOverb", [foo(sList = [60.0,  6.0], tsIndex = 2, nToys = nToys), 
+                               foo(sList = [30.0,  6.0], tsIndex = 2, nToys = nToys),
+                               foo(sList = [12.0,  6.0], tsIndex = 2, nToys = nToys),
+                               foo(sList = [ 6.0,  6.0], tsIndex = 2, nToys = nToys),
+                               foo(sList = [ 3.0,  6.0], tsIndex = 2, nToys = nToys),
+                               foo(sList = [ 0.0,  6.0], tsIndex = 2, nToys = nToys),
+                               ])
+   
+go("multiBin_constant_sOverb", [foo(sList = [60.0, 60.0], tsIndex = 2, nToys = nToys), 
+                                foo(sList = [30.0, 30.0], tsIndex = 2, nToys = nToys),
+                                foo(sList = [12.0, 12.0], tsIndex = 2, nToys = nToys),
+                                foo(sList = [ 6.0,  6.0], tsIndex = 2, nToys = nToys),
+                                foo(sList = [ 3.0,  3.0], tsIndex = 2, nToys = nToys),
+                                foo(sList = [ 1.0,  1.0], tsIndex = 2, nToys = nToys),
+                                foo(sList = [0.25, 0.25], tsIndex = 2, nToys = nToys),
+                                ])
