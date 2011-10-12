9da5359589db536f87d9c8ce968ddf04998f2ca0
Edward Laird <elaird@gmail.com>
2011-10-12 08:07:32 +0100

    slice options

--- likelihoodSpec.py
+++ likelihoodSpec.py
@@ -1,11 +1,13 @@
 def spec(simpleOneBin = False, qcdSearch = False, nHtBins = 8) :
     d = {}
 
-    d["alphaT"] = {#"52": {"htBinMask": [1]*nHtBins, "samples": [("had", True)]},
-                   #"53": {"htBinMask": [1]*nHtBins, "samples": [("had", True)]},
-                   #"55": {"htBinMask": [1]*nHtBins, "samples": [("had", True), ("muon", True), ("phot", False), ("mumu", False)][:-1]},
-                   "": {"htBinMask": [1]*nHtBins, "samples": [("had", True), ("muon", True), ("phot", False), ("mumu", False)][:-1]},
+    d["alphaT"] = {"53": {"htBinMask": [1]*nHtBins, "samples": [("had", True), ("muon", False), ("phot", False), ("mumu", False)][:-1]},
+                   "55": {"htBinMask": [1]*nHtBins, "samples": [("had", True), ("muon", False), ("phot", False), ("mumu", False)][:-1]},
+                   "70": {"htBinMask": [1]*nHtBins, "samples": [("had", True), ("muon", False), ("phot", False), ("mumu", False)][:-1]},
                    }
+
+    #d["alphaT"] = {"": {"htBinMask": [1]*nHtBins, "samples": [("had", True), ("muon", True),  ("phot", False), ("mumu", False)][:-1]} }
+    #d["alphaT"] = {"": {"htBinMask": [1]*nHtBins, "samples": [("had", True), ("muon", False), ("phot", False), ("mumu", False)][:-1]} }
 
     if simpleOneBin :
         d["simpleOneBin"] = {"b":3.0}
