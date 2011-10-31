3acc95dcf329879bcafe7998833b572ba3646677
Edward Laird <elaird@gmail.com>
2011-10-31 18:34:11 +0000

    cls t1 all points

--- configuration.py
+++ configuration.py
@@ -10,7 +10,7 @@ def method() :
     return {"CL": [0.95, 0.90][:1],
             "nToys": 500,
             "testStatistic": 3,
-            "method": ["", "profileLikelihood", "feldmanCousins", "CLs", "CLsCustom"][0],
+            "method": ["", "profileLikelihood", "feldmanCousins", "CLs", "CLsCustom"][2],
             "computeExpectedLimit": False,
             "expectedPlusMinus": {"OneSigma": 1.0},#, "TwoSigma": 2.0}
             }
@@ -33,12 +33,13 @@ def signal() :
             "drawBenchmarkPoints": True,
             "effRatioPlots": False,
 
-            "signalModel": ["tanBeta10", "tanBeta40", "T1", "T2"][0],
+            "signalModel": ["tanBeta10", "tanBeta40", "T1", "T2"][2],
             }
 
 def points() :
-    return {"listOfTestPoints": [[(29, 55, 1)], [(29, 25, 1)], [(181, 19, 1)], [(21, 1, 1)], [(39, 7, 1)], [(10, 3, 1), (10, 7, 1)], [(12, 3, 1), (12, 4, 1), (22, 5, 1)]][0],
-            #"listOfTestPoints": [],
+    return {#"listOfTestPoints": [[(29, 55, 1)], [(29, 25, 1)], [(181, 19, 1)], [(21, 1, 1)], [(39, 7, 1)], [(10, 3, 1), (10, 7, 1)], [(12, 3, 1), (12, 4, 1), (22, 5, 1)]][0],
+            "listOfTestPoints": [],
+            #"listOfTestPoints": [(32, 8, 1)],
             #"listOfTestPoints": [(21, 61, 1), (51, 51, 1), (101, 33, 1), (181, 21, 1)],
             #"xWhiteList": [ [29, 181], [16, 32]],
             }
