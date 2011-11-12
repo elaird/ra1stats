0127f6a15ae3904413c79bbaedfbf955abafa962
Edward Laird <elaird@gmail.com>
2011-11-12 18:36:07 +0000

    improve mask check

--- histogramProcessing.py
+++ histogramProcessing.py
@@ -67,7 +67,7 @@ def fillHoles(h, nZeroNeighborsAllowed = 0, cutFunc = None, mask = None) :
                 z = h.GetZaxis().GetBinLowEdge(iBinZ)
                 if h.GetBinContent(iBinX, iBinY, iBinZ) : continue
                 if cutFunc and not cutFunc(iBinX,x,iBinY,y,iBinZ,z) : continue
-                if mask!=None and (iBinX, iBinY, iBinZ) not in mask : continue
+                if mask and (iBinX, iBinY, iBinZ) not in mask : continue
                 
                 items = []
                 if iBinX!=1             : items.append(h.GetBinContent(iBinX-1, iBinY  , iBinZ))
