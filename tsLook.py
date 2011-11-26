47c3d4b0e5548a3010bdd1ebf1247ea52f685c87
Edward Laird <elaird@gmail.com>
2011-11-26 14:03:09 +0000

    styles; legend; CL values

--- tsLook.py
+++ tsLook.py
@@ -7,7 +7,7 @@ if len(sys.argv)<2 :
     print "Usage: %s root-file"%sys.argv[0]
     exit()
 
-def histos(keyList = ["bDist", "sbDist", "tsObs"]) :
+def histos(keyList = ["bDist", "sbDist", "tsObs", "CLs", "CLb", "CLsplusb", "testStatisticType"]) :
     out = []
     f = r.TFile(sys.argv[1])
     for i in range(1) :
@@ -23,34 +23,72 @@ def scaled(h) :
     h.Scale(1.0/h.Integral(0, 2+h.GetNbinsX(), "width"))
     return h
 
-def go() :
+def stylized(h, key = "", colors = {}, widths = {}) :
+    if key in colors : h.SetLineColor(colors[key])
+    if key in widths : h.SetLineWidth(widths[key])
+    return h
+
+def values(d) :
+    out = {}
+    for item in ["tsObs", "CLs", "CLb", "CLsplusb", "testStatisticType"] :
+        for error in ["", "Error"] :
+            key = item + error
+            if key not in d : continue
+            out[key] = d[key].GetBinContent(1)
+    return out
+
+def go(colors = {}, widths = {}, valuesInLegend = True, multiPage = False) :
     ps = sys.argv[1].replace(".root", "_tsLook.ps")
+    eps = ps.replace(".ps", ".eps")
 
     can = r.TCanvas()
     can.SetTickx()
     can.SetTicky()
-    can.Print(ps+"[")
 
-    for d in histos() :
-        b = scaled(d["bDist"])
-        sb = scaled(d["sbDist"])
+    histoList = histos()
+    if multiPage :
+        can.Print(ps+"[")
+    else :
+        assert len(histoList)==1,"histoList has length %d"%len(histoList)
 
-        b.SetStats(False)
-        b.SetLineColor(r.kBlue)
-        b.Draw("hist")
+    for d in histoList :
+        leg = r.TLegend(0.45, 0.7, 0.85, 0.85)
+        leg.SetBorderSize(0)
+        leg.SetFillStyle(0)
 
-        sb.SetLineColor(r.kRed)
-        sb.Draw("histsame")
-
+        val = values(d)
+            
+        b = stylized(scaled(d["bDist"]), "bDist", colors, widths)
+        sb = stylized(scaled(d["sbDist"]), "sbDist", colors, widths)
         yMax = 1.1*max(b.GetMaximum(), sb.GetMaximum())
-        b.SetMaximum(yMax)
 
-        xObs = d["tsObs"].GetBinContent(1)
-        obsLine = r.TLine()
-        obsLine.DrawLine(xObs, 0.0, xObs, 0.8*yMax)
-        can.Print(ps)
-    
-    can.Print(ps+"]")
-    utils.ps2pdf(ps)
+        assert abs(val["testStatisticType"]-3.0)<0.01,"modify x axis title for TS %g"%val["testStatisticType"]
+        null = r.TH2D("null", ";q_{1};p.d.f.", 1, 0.0, 20.0, 1, 0.0, yMax)
+        null.SetStats(False)
+        null.Draw()
 
-go()
+        sb.Draw("histsame")
+        b.Draw("histsame")
+
+        leg.AddEntry(sb,            "S+B%s"%("" if not valuesInLegend else "  (CL_{s+b} = %4.2f)"%val["CLsplusb"]), "l")
+        leg.AddEntry( b, "#color[0]{S+}B%s"%("" if not valuesInLegend else "  (CL_{b#color[0]{+s}} = %4.2f)"%val["CLb"]), "l")
+        
+        line = stylized(r.TLine(), "tsObs", colors, widths)
+        obsLine = line.DrawLine(val["tsObs"], 0.0, val["tsObs"], 0.5*yMax)
+        leg.AddEntry(obsLine, "observed value", "l")
+        leg.Draw()
+        if multiPage :
+            can.Print(ps)
+        else :
+            can.Print(eps)
+            can.Print(eps.replace(".eps", ".C"))
+
+    if multiPage :
+        can.Print(ps+"]")
+        utils.ps2pdf(ps)
+    else :
+        utils.epsToPdf(eps)
+        
+go(colors = {"bDist":r.kBlue, "sbDist":r.kRed, "tsObs":r.kBlack},
+   widths = {"bDist":2, "sbDist":2, "tsObs":2},
+   )
