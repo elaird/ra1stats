1bbaa44dd14c497ec5214a4797dcb78be941c316
Edward Laird <elaird@gmail.com>
2011-12-07 19:04:05 +0000

    5.32 env

--- env.sh
+++ env.sh
@@ -2,14 +2,12 @@
 #http://root.cern.ch/phpBB3/viewtopic.php?f=3&t=11374&p=49129&hilit=GLIBCXX_3.4.9#p49129
 
 TAG=x86_64-slc5-gcc43-opt
-
 if [[ "$HOSTNAME" == *hep.ph.ic.ac.uk ]]; then
     BASEDIR=/vols/cms02/elaird1/18_root_from_afs/lcg/
-    ROOTVER="5.30.00-patches"
-    #ROOTVER="5.30.04"
+    ROOTVER="5.32.00"
 else
     BASEDIR=/afs/cern.ch/sw/lcg/
-    ROOTVER="5.30.00/${TAG}"
+    ROOTVER="5.32.00/${TAG}"
 fi
 
 source ${BASEDIR}/contrib/gcc/4.3.2/${TAG}/setup.sh ${BASEDIR}/contrib
