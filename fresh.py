86a528824b1d19e7f5c5850e3e9c6e4902b132bc
Edward Laird <elaird@gmail.com>
2012-01-16 13:47:13 +0100

    if constraining qcd, include in file name

--- fresh.py
+++ fresh.py
@@ -1043,6 +1043,8 @@ def note(likelihoodSpec = {}) :
     
     if l["REwk"] : out += "REwk%s_"%l["REwk"]
     out += "RQcd%s"%l["RQcd"]
+    if l["constrainQcdSlope"] : out += "Ext"
+
     out += "_fZinv%s"%l["nFZinv"]
     if l["qcdSearch"] :  out += "_qcdSearch"
 
