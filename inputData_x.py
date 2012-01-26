e5d1246912b01ee3f168dcab2cbc36d745100df5
Sam Rogerson <samr@cern.ch>
2012-01-26 23:57:11 +0100

    abonding and rebranching

--- inputData_x.py
+++ inputData_x.py
@@ -2,13 +2,13 @@
 
 import histogramProcessing as hP
 
-def getMultiHists( d ) : 
+def getMultiHists( d ) :
     # d is a dictionary: structured:
     #   "filename_as_key" : { "phot" : [ "obs", "purity", "" ] }
 
     # h is local list of histograms to use with checkHistoBinning
     h = []
-    
+
     histo_dict = {}
     for file in d.keys() :
         for dir in d[file].keys() :
@@ -23,17 +23,19 @@ def getMultiHists( d ) :
 
     hP.checkHistoBinning( h )
     return histo_dict
-            
+
 
 
 # might use this....
 class inputData( object ) :
     """ Skeleton class for new approach to inputData """
-    def __init__( self, d ) :
+    def __init__( self, d , store_histos = False ) :
 
         histo_dict = getMultiHists( d )
         # hist_dict contains { "phot"       : [ histos ]
         #                      "phot_names" : [ "phot", "obs" ]
+        if store_histos :
+            self._histos = histo_dict
 
 
         h = 0
@@ -59,7 +61,7 @@ class inputData( object ) :
 
         # called from data.py mergeEfficiency
         self._mergeBins = None
-        self._constantMcRatioAfterHere =  [ ] 
+        self._constantMcRatioAfterHere =  [ ]
 
         self._htMeans = [ histo_dict["hadBulk"]["Htmeans"].GetXaxis().GetBinContent(bin) for bin in xbins ]
 
@@ -72,20 +74,20 @@ class inputData( object ) :
             if objName in objKeys :
                 self._mcExpectations[ "mc"+objName ]    =
                     tuple( [ [ histo_dict[objName][objName].GetBinContent(xbin,ybin)      for xbin in xbins ] for ybin in ybins ] )
-                self._mcStatError[ "mc"+objName+"Err" ] = 
+                self._mcStatError[ "mc"+objName+"Err" ] =
                     tuple( [ [ histo_dict[objName][objName].GetBinError(xbin,ybin)        for xbin in xbins ] for ybin in ybins ] )
 
             if "obs" in objKeys :
-                self._observations[ "n"+objName ]       = 
+                self._observations[ "n"+objName ]       =
                     tuple( [ [ histo_dict[objName]["obs"].GetBinContent(xbin,ybin)        for xbin in xbins ] for ybin in ybins ] )
             if "purity" in objKeys :
-                self._purities[ objName ]               = 
+                self._purities[ objName ]               =
                     tuple( [ [ histo_dict[objName]["purity"].GetBinError(xbin,ybin)       for xbin in xbins ] for ybin in ybins ] )
             if "atTriggerEff" in objKeys :
-                self._HtTriggerEff[dir] = 
+                self._atTriggerEff[dir] =
                     tuple( [ [ histo_dict[objName]["atTriggerEff"].GetBinError(xbin,ybin) for xbin in xbins ] for ybin in ybins ] )
             if "HtTriggerEff" in objKeys :
-                self._HtTriggerEff[dir] = 
+                self._HtTriggerEff[dir] =
                     tuple( [ histo_dict[objName]["HtTriggerEff"].GetBinError(xbin,ybin)   for xbin in xbins ] )
             if "lumiData" in objKeys :
                 self._lumi[dir] = histo_dict[dir]["lumiData"].GetBinContent(1)
@@ -103,17 +105,27 @@ class inputData( object ) :
         self._mcExtra["mcHad"]  = tuple([(ttw+zinv if ttw!=None and zinv!=None else None) for ttw,zinv in zip(self._mcExpectations["mcTtw"], self._mcExpectations["mcZinv"])])
         self._mcExtra["mcPhot"] = tuple([(gJet/purity if (gJet and purity) else None) for gJet,purity in zip(self._mcExpectations["mcGjets"], self._purities["phot"])])
         self._mcExtra["mcMumu"] = tuple([(zMumu/purity if (zMumu and purity) else None) for zMumu,purity in zip(self._mcExpectations["mcZmumu"], self._purities["mumu"])])
-                               
-        #self._mcExtra = {}
-        #self._mcExtra["mcHad"] 
-        #self._mcExtra["mcPhot"]
-        #self._mcExtra["mcMumu"]
-
-        self._fixedParameters =
-            "sigmaLumiLike": ut
-            #"sigmaLumiLike": 0
+        self._fixedParameters = {
+            "sigmaLumiLike": utils.quadSum({"lumi": 0.06, "deadEcal": 0.03, "lepVetoes": 0.025, "jesjer": 0.025, "pdf": 0.10}.values()),
             "sigmaPhotZ": 0.40,
             "sigmaMuonW": 0.30,
             "sigmaMumuZ": 0.20,
-            "k_qcd_nom"     : 3
-            "k_qcd_unc_inp" : 0
+            "k_qcd_nom"     : 3.3e-2,
+            "k_qcd_unc_inp" : 0.66e-2,
+            }
+
+    def HtProjection( self, aTmin = None, aTmax = None ) :
+        if not hasattr( self, "_histos" ) :
+            assert False, "Attempted to take projection of data_x class with no \
+            histograms stored.  class should be constructed with \
+            \"store_histos=True\" "
+
+# silly way of doing it as we don't store the histograms in the lists so we'd
+# have to maanuall project
+#        for attr in [ "_mcExtra", "_mcExpectations", "_lumi", "_HtTriggerEff", 
+#            "_atTriggerEff", "_purities", "_observations", "_mcStatError",
+#            "_mcExpectations" ] :
+#            d = getattr( self, attr, None )
+#            if d is not None :
+#                for histo in d.keys() :
+#                    # need to assign to some class here
