118d5ed30783ac9b8d3a99b845657da3825bbedd
Sam Rogerson <samr@cern.ch>
2012-01-27 18:20:55 +0100

    added small script ot make a test file containing phot random data

---
+++ make_test_file.py
@@ -0,0 +1,33 @@
+#! /usr/bin/env python
+
+import ROOT as r
+from array import array
+
+
+
+xbins = array('d', [ 275, 325, 375, 475, 575, 675, 775, 875 ] )
+ybins = array('d', [ 0.51, 0.52, 0.53, 0.54, 0.55, 0.56, 0.57 ] )
+
+nxbins = len(xbins)
+nybins = len(ybins)
+
+nbins = (nxbins+1) * (nybins+1)
+bins = range( 1, nbins+1 )
+
+f = r.TFile.Open( "data_factory_test.root", "UPDATE" )
+histo = r.TH2D("photMC", "DataFactory Test", nxbins-1, xbins, nybins-1, ybins)
+rn = r.TRandom()
+
+for bin in bins :
+    histo.SetBinContent(bin, rn.Rndm()*1000. )
+
+directory = "phot"
+if not f.cd( directory ) : 
+    print "--> Creating directory: %s" % ( directory )
+    f.mkdir( directory ).cd()
+    f.cd( directory )
+
+histo.Write("",r.TObject.kOverwrite)
+
+f.Close()
+
