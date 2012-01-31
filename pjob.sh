e6ef2a1cca3b42c679c39dab3d2205a5d3c25330
Sam Rogerson <samr@cern.ch>
2012-01-31 10:12:03 +0000

    added pbatch job shell script

---
+++ pjob.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+WDIR=$1
+ENV_SCRIPT=$2
+REDIR=$3
+
+POINT_FILE=$4
+
+POINT=$(awk -v L=$SGE_TASK_ID 'NR==L' $POINT_FILE) 
+
+cd $WDIR
+source $ENV_SCRIPT
+./job.py $WDIR $ENV_SCRIPT $REDIR $POINT >& $REDIR
