fa02ed51e4b8a18f05d90225242b4e8bfd5d84b5
Edward Laird <elaird@gmail.com>
2012-02-02 20:14:37 +0100

    include photons

--- inputData/afterAlphaT_b.py
+++ inputData/afterAlphaT_b.py
@@ -21,6 +21,10 @@ class data_55_v1(data) :
             "mcMuon":  4650.,
             "mcTtw":   4650.,
 
+            "phot":    4529.,
+            "mcGjets": 4529.,
+            "mcZinv":  4529.,
+
             "mumu":    4650.,
             "mcZmumu": 4650.,
             }
@@ -32,11 +36,13 @@ class data_55_v1(data) :
             "nHad":           (     396.0,     203.0,     156.0,      50.0,      17.0,       7.0,       3.0,       2.0),
             "nMuon":          (     304.0,     114.0,     116.0,      44.0,      14.0,       4.0,       2.0,       1.0),
             "nMumu":          (       9.0,       5.0,       5.0,       1.0,       1.0,       0.0,       0.0,       1.0),
+            "nPhot":     excl((      None,      None,       101,        28,        11,         5,         1,         1), isExcl),
             }
 
         self._triggerEfficiencies = {
             "hadBulk":       (     0.878,     0.906,     0.957,     1.000,     1.000,     1.000,     1.000,     1.000),
             "had":           (     0.727,     0.869,     0.943,     1.000,     1.000,     1.000,     1.000,     1.000),
+            "muon":          (     0.727,     0.869,     0.943,     1.000,     1.000,     1.000,     1.000,     1.000),
             }
 
         self._mcExpectations = {
@@ -44,6 +50,7 @@ class data_55_v1(data) :
                                     self._triggerEfficiencies["had"]),
             "mcTtw":      trig(     scaled(( 391.76, 163.16,  133.24,   52.21,  16.98,   8.90,   1.10,   1.49), self.lumi()["muon"]/self.lumi()["mcTtw"]),
                                     self._triggerEfficiencies["had"]),
+            "mcGjets":         excl(scaled((  None,    None,      91,      34,     15,      7,      4,    0.7), self.lumi()["phot"]/self.lumi()["mcGjets"]), isExcl),
             "mcZinv":     trig(            (  86.75,  35.80,   25.30,   10.69,    3.5,    1.4,    0.5,    0.2),
                                     self._triggerEfficiencies["had"]),
             "mcZmumu":    trig(     scaled((  13.55,   6.17,    2.13,    2.64,    0.1,    0.6,    0.1,    1.0), self.lumi()["mumu"]/self.lumi()["mcZmumu"]),
@@ -53,6 +60,7 @@ class data_55_v1(data) :
         self._mcStatError = {
             "mcMuonErr": (15.88, 11.03, 4.28, 2.57, 1.82, 0.94, 0.34, 0.23),
             "mcTtwErr":  (15.90, 11.24, 4.76, 3.00, 1.65, 1.25, 0.42, 0.49),
+            "mcGjetsErr":( None,  None, 8   ,    5,    3,    2,    2,  0.7),
             "mcZinvErr": (2.40,  1.51, 1.27, 0.82, 0.47, 0.30, 0.18, 0.13),
             "mcZmumuErr":(2.07,  1.54, 0.81, 0.93, 0.01, 0.49, 0.16, 0.53),
             }
