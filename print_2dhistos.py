c4bf43de1e0ad5d3d8a408dee17fe20b139ce9e0
Sam Rogerson <samr@cern.ch>
2012-02-07 16:24:42 +0100

    remove make_files random histo functionality; rename

--- print_2dhistos.py
+++ print_2dhistos.py
@@ -7,53 +7,27 @@ import DataFactory as DF
 
 import inspect
 
-def makeFile() :
-    xbins = array('d', [ 275, 325, 375, 475, 575, 675, 775, 875 ] )
-    ybins = array('d', [ 0.51, 0.52, 0.53, 0.54, 0.55, 0.56, 0.57 ] )
-    nxbins = len(xbins)
-    nybins = len(ybins)
-    nbins = (nxbins+1) * (nybins+1)
-    bins = range( 1, nbins+1 )
+baseline = { "~/Documents/RA1/RA1_Stats_baseline.root" :
+               { "had"  : [ "lumiData", "lumiMc", "WW", "WJets", "Zinv", "t", "ZZ",
+                         "DY", "tt", "obs", "WZ" ],
+                 "muon" : [ "lumiData", "lumiMc", "Zinv", "WW", "WJets", "t", "ZZ",
+                         "DY", "tt", "obs", "WZ" ],
+                 "mumu" : [ "lumiData", "lumiMc", "Zinv", "WW", "WJets", "t", "ZZ",
+                         "DY", "tt", "obs", "WZ" ],
+               },
+           }
 
-    f = r.TFile.Open( "data_factory_test.root", "UPDATE" )
-    histo = r.TH2D("photMC", "DataFactory Test", nxbins-1, xbins, nybins-1, ybins)
-    rn = r.TRandom()
-    for bin in bins :
-        histo.SetBinContent(bin, rn.Rndm()*1000. )
-    directory = "phot"
-    if not f.cd( directory ) : 
-        print "--> Creating directory: %s" % ( directory )
-        f.mkdir( directory ).cd()
-        f.cd( directory )
-    histo.Write("",r.TObject.kOverwrite)
-    f.Close()
+btagged = { "~/Documents/RA1/RA1_Stats_Btagged.root" :
+               { "had"  : [ "lumiData", "lumiMc", "WW", "WJets", "Zinv", "t", "ZZ",
+                         "DY", "tt", "obs", "WZ" ],
+                 "muon" : [ "lumiData", "lumiMc", "Zinv", "WW", "WJets", "t", "ZZ",
+                         "DY", "tt", "obs", "WZ" ],
+                 "mumu" : [ "lumiData", "lumiMc", "Zinv", "WW", "WJets", "t", "ZZ",
+                         "DY", "tt", "obs", "WZ" ],
+               },
+          } 
 
-r.gROOT.SetBatch(True)
-
-mF = False
-if mF: makeFile()
-
-#d = { "data_factory_test.root" : { "phot" : [ "photMC" ] } }
-d = { "~/Documents/RA1/RA1_Stats_baseline.root" :
-        { "had"  : [ "lumiData", "lumiMc", "WW", "WJets", "Zinv", "t", "ZZ",
-                     "DY", "tt", "obs", "WZ" ],
-          "muon" : [ "lumiData", "lumiMc", "Zinv", "WW", "WJets", "t", "ZZ",
-                     "DY", "tt", "obs", "WZ" ],
-          "mumu" : [ "lumiData", "lumiMc", "Zinv", "WW", "WJets", "t", "ZZ",
-                     "DY", "tt", "obs", "WZ" ],
-         },
-     }
-
-e = { "~/Documents/RA1/RA1_Stats_Btagged.root" :
-        { "had"  : [ "lumiData", "lumiMc", "WW", "WJets", "Zinv", "t", "ZZ",
-                     "DY", "tt", "obs", "WZ" ],
-          "muon" : [ "lumiData", "lumiMc", "Zinv", "WW", "WJets", "t", "ZZ",
-                     "DY", "tt", "obs", "WZ" ],
-          "mumu" : [ "lumiData", "lumiMc", "Zinv", "WW", "WJets", "t", "ZZ",
-                     "DY", "tt", "obs", "WZ" ],
-         },
-     }
-
+r.gROOT.SetBatch(1)
 
 dsf = DF.DataSliceFactory( d )
 ds_52_53 = dsf.makeSlice("x",55.5,55.6)
@@ -67,4 +41,3 @@ for slice in [ ds_52_53_b, ds_52_53 ] :
         if not "__" in attr:
             x = getattr( slice, attr )
             print "%s.%s = %s" % ( "self", attr, x )
-    print "\n\n\n"
