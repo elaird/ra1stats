91e9b91e79f5539e156a62fd34b19ccce787cd6e
Edward Laird <elaird@gmail.com>
2012-02-20 21:14:31 +0100

    accommodate name change

--- job.py
+++ job.py
@@ -50,7 +50,7 @@ def results(switches = None, likelihoodSpec = None, signal = None) :
     for cl in switches["CL"] :
         cl2 = 100*cl
         f = workspace.foo(signal = signal, likelihoodSpec = likelihoodSpec, extraSigEffUncSources = switches["extraSigEffUncSources"],
-                          rhoSignalMin = switches["rhoSignalMin"], fIni = switches["fIni"])
+                          rhoSignalMin = switches["rhoSignalMin"], fIniFactor = switches["fIniFactor"])
 
         if switches["method"]=="CLs" :
             results = f.cls(cl = cl, nToys = switches["nToys"], plusMinus = switches["expectedPlusMinus"],
