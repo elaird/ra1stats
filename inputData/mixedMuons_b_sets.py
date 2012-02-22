4712acdf33298354f8a44de8e137361c2ef8a53c
Edward Laird <elaird@gmail.com>
2012-02-22 14:11:54 +0100

    new photon numbers

--- inputData/mixedMuons_b_sets.py
+++ inputData/mixedMuons_b_sets.py
@@ -30,7 +30,6 @@ class data_55_0btag(data) :
         self._htMeans =       ( 2.960e+02, 3.464e+02, 4.128e+02, 5.144e+02, 6.161e+02, 7.171e+02, 8.179e+02, 9.188e+02) #old
         self._sigEffCorr =    (       1.0,       1.0,       1.0,       1.0,       1.0,       1.0,       1.0,       1.0)
 
-        #### 
         self._observations = {
             "nHadBulk":scaled(( 2.792e+08, 1.214e+08, 8.544e+07, 2.842e+07, 9.953e+06, 3.954e+06, 1.679e+06, 1.563e+06), self.lumi()["had"]/self.lumi()["hadBulk"]),
             "nHad"               :   ( 2919.0, 1166.0, 769.0, 255.0, 91.0, 31.0, 10.0, 4.0, ) ,
@@ -48,17 +47,15 @@ class data_55_0btag(data) :
             }
 
 	self._mcExpectations = {
-            "mcGjets": excl(scaled((  None,    None, 2.00e+3 - 2.3e2, 7.1e+2 - 82, 2.7e+2 - 35,  92-15,   34-6,  14-3), #>=0 b-tag minus >=1 b-tag
-                                   self.lumi()["phot"]/self.lumi()["mcGjets"]), isExcl),
+            "mcGjets": excl((  None,    None, 2.00e+3 - 2.3e2, 7.1e+2 - 82, 2.7e+2 - 35,  92-15,   34-6,  14-3), isExcl), #>=0 b-tag minus >=1 b-tag
             "mcTtw"              :   ( 1620.0, 601.5, 375.3, 128.7, 44.36, 17.35, 5.84, 4.109, ) ,
             "mcZinv"             :   ( 1515.0, 635.8, 475.6, 165.3, 63.21, 21.3, 9.142, 6.196, ) ,
             "mcMumu"             :   ( 110.6, 65.92, 255.8, 120.0, 53.79, 24.3, 13.31, 10.74, ) ,
             "mcMuon"             :   ( 1145.0, 532.1, 1886.0, 857.3, 371.9, 179.5, 85.31, 104.5, ) ,
 	    }
         
-        
         self._mcStatError = {
-            "mcGjetsErr": (None,  None,   10,    7,    5,    3,    2,    2),
+            "mcGjetsErr"         :   (  None,    None, 0.04e+3, 0.2e+2, 0.1e+2,  8,   5,   3), #>=0 b-tag
             "mcTtwErr"           :   ( 66.98, 40.54, 4.864, 2.85, 1.656, 1.151, 0.5746, 0.4422, ) ,
             "mcZinvErr"          :   ( 9.983, 6.39, 5.518, 3.25, 2.01, 1.167, 0.7642, 0.6292, ) ,
             "mcMuonErr"          :   ( 55.42, 38.09, 11.08, 7.601, 4.916, 3.504, 2.212, 2.589, ) ,
@@ -139,7 +136,7 @@ class data_55_1btag(data) :
             }
 
         self._mcExpectations = {
-            "mcGjets": excl(       (  None,    None,     2.3e2,    82,     35,     15,      6,    3  ), isExcl), #>=1 b-tag
+            "mcGjets":        excl(  (  None,    None,     2.3e2,    82,     35,     15,      6,    3  ), isExcl), #>=1 b-tag
             "mcTtw"              :   ( 428.6, 199.0, 143.2, 49.2, 16.34, 9.632, 2.014, 0.7877, ) ,
             "mcZinv"             :   ( 170.1, 72.91, 48.15, 20.86, 7.816, 2.654, 0.9874, 0.8448, ) ,
             "mcMumu"             :   ( 19.41, 6.645, 27.72, 13.81, 8.911, 3.04, 1.211, 2.145, ) ,
@@ -148,7 +145,7 @@ class data_55_1btag(data) :
         
         
         self._mcStatError = {
-            "mcGjetsErr": (None,  None,   10,    7,    5,    3,    2,    2),
+            "mcGjetsErr"         :   ( None,  None,   10,    7,    5,    3,    2,    2),
             "mcTtwErr"           :   ( 20.84, 16.65, 4.627, 2.682, 1.532, 1.247, 0.4522, 0.2632, ) ,
             "mcZinvErr"          :   ( 3.357, 2.164, 1.754, 1.154, 0.7066, 0.4118, 0.2512, 0.2323, ) ,
             "mcMuonErr"          :   ( 19.8, 12.09, 9.159, 6.487, 4.503, 2.997, 1.957, 1.934, ) ,
@@ -212,14 +209,12 @@ class data_55_2btag(data) :
         self._htMeans =       ( 2.960e+02, 3.464e+02, 4.128e+02, 5.144e+02, 6.161e+02, 7.171e+02, 8.179e+02, 9.188e+02) #old
         self._sigEffCorr =    (       1.0,       1.0,       1.0,       1.0,       1.0,       1.0,       1.0,       1.0)
 
-        #### 
         self._observations = {
             "nHadBulk":scaled(( 2.792e+08, 1.214e+08, 8.544e+07, 2.842e+07, 9.953e+06, 3.954e+06, 1.679e+06, 1.563e+06), self.lumi()["had"]/self.lumi()["hadBulk"]),
             "nHad"               :   ( 160.0, 68.0, 52.0, 19.0, 11.0, 7.0, 0.0, 2.0, ) ,
             "nMuon"              :   ( 116.0, 49.0, 264.0, 152.0, 63.0, 26.0, 10.0, 14.0, ) ,
             "nMumu"              :   ( 4.0, 3.0, 8.0, 7.0, 5.0, 2.0, 0.0, 0.0, ) ,
-            #"nPhot":     excl((      None,      None,       221,        84,        37,        16,         7,         2), isExcl),
-            "nPhot": (None,None,None,None,None,None,None,None),
+            "nPhot":            excl(( None,  None,  20,  10,  6,  4,  0,  0), isExcl),
             }
 
         self._triggerEfficiencies = {
@@ -231,17 +226,15 @@ class data_55_2btag(data) :
             }
 
 	self._mcExpectations = {
-            "mcGjets": excl(       (  None,    None,     2.3e2,    82,     35,     15,      6,    3  ), isExcl),
+            "mcGjets"  : excl(       (  None,  None,  25, 9, 3, 3, 0.9, 0.9 ), isExcl),
             "mcTtw"              :   ( 181.1, 69.64, 49.37, 26.42, 8.904, 3.795, 0.6144, 1.183, ) ,
             "mcZinv"             :   ( 19.91, 8.835, 8.185, 2.92, 1.218, 0.3464, 0.07037, 0.0, ) ,
             "mcMumu"             :   ( 3.331, 4.9, 10.42, 3.777, 0.8922, 0.9244, 0.03023, 0.06706, ) ,
             "mcMuon"             :   ( 149.8, 67.0, 281.1, 156.0, 74.87, 31.07, 14.01, 15.05, ) ,
 	    }
-        
-        
-        print "put in new stat errors"
+
         self._mcStatError = {
-            "mcGjetsErr": (None,  None,   10,    7,    5,    3,    2,    2),
+            "mcGjetsErr"         :   ( None,  None,   4, 2, 1, 1, 0.9, 0.9),
             "mcTtwErr"           :   ( 12.19, 6.101, 2.981, 2.186, 1.211, 0.8435, 0.3401, 0.4419, ) ,
             "mcZinvErr"          :   ( 1.137, 0.7513, 0.7231, 0.4319, 0.279, 0.1488, 0.06705, 0.0, ) ,
             "mcMuonErr"          :   ( 8.983, 6.111, 7.206, 5.346, 3.707, 2.384, 1.586, 1.637, ) ,
