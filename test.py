6cac240a6589c4260702eea63c33daef72393633
Edward Laird <elaird@gmail.com>
2012-03-06 18:03:35 +0000

    test cls

--- test.py
+++ test.py
@@ -151,8 +151,8 @@ def signal(i) :
     return out[i]
 
 f = workspace.foo(likelihoodSpec = likelihoodSpec.spec(),
-                  #signal = signal(0),
-                  signalExampleToStack = signal(2),
+                  signal = signal(2),
+                  #signalExampleToStack = signal(2),
                   #trace = True
                   #rhoSignalMin = 0.1,
                   #fIniFactor = 0.1,
@@ -161,11 +161,11 @@ f = workspace.foo(likelihoodSpec = likelihoodSpec.spec(),
 
 cl = 0.95 if f.likelihoodSpec.standardPoi() else 0.68
 #out = f.interval(cl = cl, method = ["profileLikelihood", "feldmanCousins"][0], makePlots = True); print out
-#out = f.cls(cl = cl, plusMinus = {"OneSigma": 1.0, "TwoSigma": 2.0},makePlots = True,
-#            calculatorType = ["frequentist", "asymptotic", "asymptoticNom"][1],
-#            testStatType = 3, nToys = 20, nWorkers = 1); print out
+out = f.cls(cl = cl, plusMinus = {"OneSigma": 1.0, "TwoSigma": 2.0},makePlots = True,
+            calculatorType = ["frequentist", "asymptotic", "asymptoticNom"][1],
+            testStatType = 3, nToys = 20, nWorkers = 1); print out
 #f.profile()
-f.bestFit(printValues = True)
+#f.bestFit(printValues = True)
 #f.bestFit(drawMc = False, printValues = False)
 #f.bestFit(drawMc = False, printValues = False, drawComponents = False)
 #f.bestFit(printPages = True)
