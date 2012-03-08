8806b859774cd5fb1dd602034347dd408554bdd3
Edward Laird <elaird@gmail.com>
2012-03-08 17:31:38 +0100

    2010 model and comments

--- test.py
+++ test.py
@@ -149,8 +149,8 @@ def signal(i) :
     return out[i]
 
 f = workspace.foo(likelihoodSpec = likelihoodSpec.spec(),
-                  #signal = signal(0),
-                  signalExampleToStack = signal(-4),
+                  signal = signal(-4),
+                  #signalExampleToStack = signal(-4),
                   #trace = True
                   #rhoSignalMin = 0.1,
                   #fIniFactor = 0.1,
@@ -160,8 +160,8 @@ f = workspace.foo(likelihoodSpec = likelihoodSpec.spec(),
 cl = 0.95 if f.likelihoodSpec.standardPoi() else 0.68
 #out = f.interval(cl = cl, method = ["profileLikelihood", "feldmanCousins"][0], makePlots = True); print out
 #out = f.cls(cl = cl, plusMinus = {"OneSigma": 1.0, "TwoSigma": 2.0},makePlots = True,
-#            calculatorType = ["frequentist", "asymptotic", "asymptoticNom"][1],
-#            testStatType = 3, nToys = 20, nWorkers = 1); print out
+#            calculatorType = ["frequentist", "asymptotic", "asymptoticNom"][0],
+#            testStatType = 3, nToys = 50, nWorkers = 1); print out
 #f.profile()
 f.bestFit(printValues = True)
 #f.bestFit(drawMc = False, printValues = False)
