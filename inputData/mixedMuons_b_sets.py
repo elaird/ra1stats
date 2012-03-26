bbca524e33f170f78eb381b7acb27cae5f685b76
Edward Laird <elaird@gmail.com>
2012-03-26 23:18:40 +0200

    another option for ge3b yields

--- inputData/mixedMuons_b_sets.py
+++ inputData/mixedMuons_b_sets.py
@@ -282,3 +282,80 @@ class data_55_gt2btag(data) :
         self._mcExtraBeforeTrigger["mcHad"]  = tuple([(ttw+zinv if ttw!=None and zinv!=None else None) for ttw,zinv in zip(self._mcExpectationsBeforeTrigger["mcTtw"], self._mcExpectationsBeforeTrigger["mcZinv"])])
         
         syst.load(self, mode = self.systMode)
+
+class data_55_gt2btag_v2(data) :
+    """muons and mumu have no alt cut for highest six bins;
+    MC expectations computed using measured b-eff. and mis-tag rate and ``matrix algebra''"""
+
+    def _fill(self) :
+        isExcl =                         (    1,     1,     0,     0,     0,     0,     0,     1)
+
+        self._htBinLowerEdges =          (275.0, 325.0, 375.0, 475.0, 575.0, 675.0, 775.0, 875.0)
+        self._htMaxForPlot = 975.0
+
+        self._mergeBins = None
+        self._constantMcRatioAfterHere = (    0,     0,     0,     0,     0,     0,     0,     1)
+
+        self._lumi = {
+            "had":     4650.,
+            "hadBulk": 4650.,
+
+            "muon":    4650.,
+            "mcMuon":  4650.,
+            "mcTtw":   4650.,
+
+            "phot":    4529.,
+            "mcGjets": 4529.,
+            "mcZinv":  4529.,
+
+            "mumu":    4650.,
+            "mcMumu":  4650.,
+            }
+        self._htMeans =       ( 2.960e+02, 3.464e+02, 4.128e+02, 5.144e+02, 6.161e+02, 7.171e+02, 8.179e+02, 9.188e+02) #old
+
+        self._observations = {
+            "nHadBulk":scaled(( 2.792e+08, 1.214e+08, 8.544e+07, 2.842e+07, 9.953e+06, 3.954e+06, 1.679e+06, 1.563e+06), self.lumi()["had"]/self.lumi()["hadBulk"]),
+            "nHad"               :   ( 10.0, 8.0, 8.0, 1.0, 0.0, 0.0, 0.0, 0.0, ) ,
+            "nMuon"              :   ( 9.0, 6.0, 22.0, 16.0, 13.0, 3.0, 1.0, 4.0, ) ,
+            "nMumu"              :   ( 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0, 0.0, ) ,
+            "nPhot"              :   (None, None,  1,   0,   0,   0,   0,   0, ),
+            }
+
+        self._triggerEfficiencies = {
+            "hadBulk":       (     0.878,     0.906,     0.957,     1.000,     1.000,     1.000,     1.000,     1.000),
+            "had":           (     0.727,     0.869,     0.943,     1.000,     1.000,     1.000,     1.000,     1.000),
+            "muon":          (     0.727,     0.869,     0.950,     0.950,     0.950,     0.950,     0.950,     0.950),
+            "phot":          (     1.000,     1.000,     1.000,     1.000,     1.000,     1.000,     1.000,     1.000),
+            "mumu":          (     0.727,     0.869,     0.950,     0.950,     0.950,     0.950,     0.950,     0.950),
+            }
+
+	self._mcExpectationsBeforeTrigger = {
+            "mcGjets"  : excl(       (  None,  None,  1.0, 0.8, 0.0, 0.0, 0.0, 0.0 ), isExcl),
+            #"mcTtw"              :   ( 14.98, 4.788, 4.243, 2.604, 0.5211, 0.3001, 0.01408, 0.06082, ) ,
+            #"mcZinv"             :   ( 1.079, 0.5188, 0.316, 0.1634, 0.0, 0.09114, 0.0, 0.0, ) ,
+            "mcTtw"               :   ( 7.313, 2.8670, 2.720, 1.5100,0.640397,0.469682,0.079102,0.064794,), #March 26 (ttw and zInv)
+            "mcZinv"             :   ( 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0), #March 26 ttw has both
+
+            "mcMumu"             :   ( 0.01635, 0.0, 0.6904, 0.0, 0.002354, 0.0, 0.0, 0.0, ) ,
+            #"mcMuon"            :   ( 13.1, 4.235, 26.51, 15.14, 8.438, 2.703, 1.364, 1.872, ) ,
+            "mcMuon"             :   ( 6.90, 2.94,  14.25,  9.20, 5.238, 1.988, 1.103, 1.169,), #March 26
+	    }
+
+        self._mcStatError = {
+            "mcGjetsErr"         :   ( None,  None,   0.8, 0.8, 0.0, 0.0, 0.0, 0.0),
+            "mcTtwErr"           :   ( 1.686, 0.9518, 0.8906, 0.6971, 0.2472, 0.2281, 0.05278, 0.05092, ) ,
+            "mcZinvErr"          :   ( 0.2626, 0.1821, 0.1421, 0.1022, 0.0, 0.07631, 0.0, 0.0, ) ,
+            "mcHadErr"           :   (0.266442,0.180258,0.155991,0.137346,0.105424,0.116832,0.039850,0.029252,), #March 26
+            #"mcMuonErr"          :   ( 1.593, 0.8971, 2.259, 1.693, 1.274, 0.7076, 0.5003, 0.5946, ) ,
+            "mcMuonErr"         :   ( 0.333, 0.1958, 0.379, 0.328, 0.325, 0.1546, 0.1190, 0.1576,), #March 26
+            "mcMumuErr"          :   ( 0.01389, 0.0, 0.3678, 0.0, 0.003354, 0.0, 0.0, 0.0, ) ,
+            }
+
+        self._purities = {
+            "phot":                  (  None,    None,    0.98,   0.99,   0.99,   0.99,   0.99, 0.99),
+            }
+
+        self._mcExtraBeforeTrigger = {}
+        self._mcExtraBeforeTrigger["mcHad"]  = tuple([(ttw+zinv if ttw!=None and zinv!=None else None) for ttw,zinv in zip(self._mcExpectationsBeforeTrigger["mcTtw"], self._mcExpectationsBeforeTrigger["mcZinv"])])
+
+        syst.load(self, mode = self.systMode)
