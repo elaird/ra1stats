372bdaf905bd2e5323f6515b2504856ac22bd578
Edward Laird <elaird@gmail.com>
2012-05-11 15:29:17 +0200

    make also eff plot

--- xsLimit.py
+++ xsLimit.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python
 
-import plottingGrid as pg
+import plottingGrid
 
-pg.makeTopologyXsLimitPlots(logZ = True,
-                            names = ["UpperLimit","upperLimit95"],
-                            #simpleExcl = True,
-                            #drawGraphs = False,
-                            #mDeltaFuncs = {"mDeltaMin":0.0, "mDeltaMax":400.0, "nSteps":4, "mGMax":1250.},
-                            #printXs = True,
-                            )
+plottingGrid.makeTopologyXsLimitPlots(logZ = True,
+                                      names = ["UpperLimit","upperLimit95"],
+                                      #simpleExcl = True,
+                                      #drawGraphs = False,
+                                      #mDeltaFuncs = {"mDeltaMin":0.0, "mDeltaMax":400.0, "nSteps":4, "mGMax":1250.},
+                                      #printXs = True,
+                                      )
+
+plottingGrid.makeEfficiencyPlot()
