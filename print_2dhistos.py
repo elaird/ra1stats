d1e322b391afa9d3179a1150cee32237b91d3f9c
Sam Rogerson <samr@cern.ch>
2012-06-01 20:21:45 +0100

    update to include phot files from Ted

--- print_2dhistos.py
+++ print_2dhistos.py
@@ -1,10 +1,8 @@
 #! /usr/bin/env python
 
 import ROOT as r
-
 from collections import Iterable
 from array import array
-
 import DataFactory as DF
 
 def print_unpack( item, level = 0, ending_comma = False ) :
@@ -41,9 +39,11 @@ std_selections = { "had"  : [ "lumiData", "lumiMc", "WW", "WJets", "Zinv", "t", 
                          "DY", "tt", "obs", "WZ" ],
                    "mumu" : [ "lumiData", "lumiMc", "Zinv", "WW", "WJets", "t", "ZZ",
                          "DY", "tt", "obs", "WZ" ],
+#                   "phot" : [ "lumiData", "lumiMc", "obs", "Phot" ],
                  }
+phot_selections = { "phot" : [ "lumiData", "lumiMc", "obs", "Phot" ] }
 
-base_dir = "~/public_html/03_RA1/07_numbers_from_darren/"
+base_dir = "~/public_html/03_RA1/07_ra1stats_numbers/"
 d_set = "03_01_06_2012"
 file_names = [  #"RA1_Stats_More_Than_One_btags.root",
                 "RA1_Stats_More_Than_Two_btag.root",
@@ -52,16 +52,24 @@ file_names = [  #"RA1_Stats_More_Than_One_btags.root",
                 "RA1_Stats_Two_btags.root",
                 "RA1_Stats_Zero_btags.root",
                 #"RA1_Stats_Zero_btags_AlphaT_Cut.root",
-               ]
+             ]
+
+phot_files = [ "g_barrel_caloJet_ge2__0b.root",
+               "g_barrel_caloJet_ge2__1b.root",
+               "g_barrel_caloJet_ge2__2b.root",
+               "g_barrel_caloJet_ge2__3b.root",
+             ]
 
 fullfiles = [ "{base}/{set}/{file}".format(base=base_dir, set=d_set, file=f) for f in file_names ]
-
+fullphots = [ "{base}/{set}/{file}".format(base=base_dir, set=d_set, file=f) for f in phot_files ]
 
 
 #names = [ "btag_gt1", "btag_gt2", "btag_gt0", "btag1", "btag2", "btag0", "btag0_aT" ]
-names = [ "btag_gt2", "btag1", "btag2", "btag0_aT" ]
+#names = [ "btag_gt2", "btag1", "btag2", "btag0_aT", "btag0_phot", "btag1_phot", "btag2_phot", "btag3_phot", ]
+names = [ "btag0_phot", "btag1_phot", "btag2_phot", "btag3_phot", ]
 
-selections = [ { rfile : std_selections } for rfile in fullfiles ] 
+#selections  = [ { rfile : std_selections } for rfile in fullfiles ] 
+selections = [ { rfile : phot_selections } for rfile in fullphots ]
 
 dsfs = [ DF.DataSliceFactory( selection ) for selection in selections ]
 dss  = [ dsf.makeSlice("x",55.5,55.6) for dsf in dsfs ]
