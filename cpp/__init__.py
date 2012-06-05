0827cc1c4c5a32ec26106fab1b5a0f4b70befc4b
Sam Rogerson <samr@cern.ch>
2012-06-05 22:46:29 +0100

    add compile for alt AsympCalc code

--- cpp/__init__.py
+++ cpp/__init__.py
@@ -1,5 +1,6 @@
 import ROOT as r
 
-def compile(files = ["cpp/StandardHypoTestInvDemo.cxx","cpp/Poisson.cxx","cpp/Gaussian.cxx"]) :
+def compile(files = ["cpp/StandardHypoTestInvDemo.cxx","cpp/Poisson.cxx",
+                     "cpp/Gaussian.cxx", "cpp/NckwWorkspace.cxx"]) :
     for f in files :
         r.gROOT.LoadMacro("%s+"%f)
