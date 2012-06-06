a62b5c85da5e89fa6e2adb44e9f8a9f788145c17
Sam Rogerson <samr@cern.ch>
2012-06-06 14:03:05 +0100

    example arg

--- test.py
+++ test.py
@@ -148,7 +148,7 @@ cl = 0.95 if f.likelihoodSpec.standardPoi() else 0.68
 out = f.cls(cl = cl, plusMinus = {"OneSigma": 1.0, "TwoSigma": 2.0},makePlots = True,
             calculatorType = ["frequentist", "asymptotic", "asymptoticNom"][1],
             testStatType = 3, nToys = 50, nWorkers = 1,
-            plSeed = True, plNIterationsMax = 10, calcToUse="NCKW"
+            plSeed = True, plNIterationsMax = 10, #calcToUse="NCKW"
             ); print out
 
 
