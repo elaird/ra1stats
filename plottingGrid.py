97f0a55229834bdb5b6df9c1561d55b03778eb6d
Edward Laird <elaird@gmail.com>
2012-06-23 16:06:15 +0100

    shift text

--- plottingGrid.py
+++ plottingGrid.py
@@ -194,7 +194,7 @@ def makeXsUpperLimitPlots(logZ = False, exclusionCurves = True, mDeltaFuncs = {}
             func.Draw("same")
 
     #stamp plot
-    s2 = stamp(text = "#alpha_{T}", x = 0.22, y = 0.55, factor = 1.3)
+    s2 = stamp(text = "#alpha_{T}", x = 0.22, y = 0.50, factor = 1.3)
     textMap = {"profileLikelihood":"PL", "CLs":"CL_{s}"}
     s3 = stamp(text = "%s,  5.0 fb^{-1},  #sqrt{s}=7 TeV"%textMap[s["method"]], x = 0.22, y = 0.55, factor = 0.7)
 
