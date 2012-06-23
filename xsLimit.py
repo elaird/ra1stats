bd486e7ad66bc113ecafb8f316cb94198c82c8f9
Edward Laird <elaird@gmail.com>
2012-06-23 15:25:18 +0100

    drive new xs upper limit plots

--- xsLimit.py
+++ xsLimit.py
@@ -2,12 +2,11 @@
 
 import plottingGrid
 
-plottingGrid.makeTopologyXsLimitPlots(logZ = True,
-                                      names = ["UpperLimit","upperLimit95"],
-                                      #simpleExcl = True,
-                                      #drawGraphs = False,
-                                      #mDeltaFuncs = {"mDeltaMin":0.0, "mDeltaMax":400.0, "nSteps":4, "mGMax":1250.},
-                                      #printXs = True,
-                                      )
+plottingGrid.makeXsUpperLimitPlots(logZ = True,
+                                   #simpleExcl = True,
+                                   #exclusionCurves = False,
+                                   #mDeltaFuncs = {"mDeltaMin":0.0, "mDeltaMax":400.0, "nSteps":4, "mGMax":1250.},
+                                   #printXs = True,
+                                   )
 
 plottingGrid.makeEfficiencyPlot()
