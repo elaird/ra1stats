9fe0e96d04ec60b96682264bad33c48100a9b18d
Sam Rogerson <samr@cern.ch>
2012-06-26 17:02:30 +0100

    50Gev false

--- configuration.py
+++ configuration.py
@@ -23,7 +23,7 @@ def method() :
             "calculatorType": ["frequentist", "asymptotic", "asymptoticNom"][1],
             "method": ["", "profileLikelihood", "feldmanCousins", "CLs", "CLsCustom"][3],
             "binaryExclusionRatherThanUpperLimit": False,
-            "fiftyGeVStepsOnly": True,
+            "fiftyGeVStepsOnly": False,
             }
 
 def signal() :
