a23e6823ec3f620a6ad57c545f10c6fc211ff533
Edward Laird <elaird@gmail.com>
2012-06-28 18:06:20 +0200

    2012 example

--- test.py
+++ test.py
@@ -9,7 +9,7 @@ def go(iLower = None, iUpper = None, year = 2011, ensemble = False) :
                                                            separateSystObs = not ensemble,
                                                            ),
                       #signalToTest = signals.t2tt2,
-                      #signalExampleToStack = signals.t2tt2,
+                      signalExampleToStack = signals.t1tttt_2012_2,
                       #signalToInject = signals.t1,
                       #trace = True
                       #rhoSignalMin = 0.1,
