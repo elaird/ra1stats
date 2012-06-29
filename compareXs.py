3da147202617032d885e79a99a7823d418f32fff
Sam Rogerson <samr@cern.ch>
2012-06-29 18:49:24 +0200

    remove rooter functionality and tidy up plotting

--- compareXs.py
+++ compareXs.py
@@ -1,140 +1,110 @@
 #!/usr/bin/env python
 
 import ROOT as r
-from collections import defaultdict
 from utils import threeToTwo
 
-def parsed(fileName = "") :
-    dct = {}
-    f = open(fileName)
-    for iLine,line in enumerate(f) :
-        if "GeV" not in line :
-            assert ("(pb)" in line) or line=="\n",line
-            continue
-        try:
-            mass,gev,xs,garbage,percentUnc = line.replace("|","").split()
-            relUnc = float(percentUnc.replace("%",""))/100.0
-            dct[int(mass)] = (float(xs), relUnc)
-        except:
-            print "Bad format on line %d of file %s:"%(1+iLine, fileName)
-            print line
-            exit()
-    f.close()
-    return dct
-
-def histos() :
-    histosOut = defaultdict(dict)
-    for fileName,histName in {#"sms_xs/gluglu_decoupled7TeV.txt":"gluino",
-                              "sms_xs/sqsq_decoupled7TeV.txt":"squark",
-                              #"sms_xs/stst_decoupled7TeV.txt":"stop_or_sbottom",
-                              }.iteritems() :
-        dct = parsed(fileName)
-        masses = sorted(dct.keys())
-        nMasses = len(masses)
-        assert nMasses>1,"%s: %d"%(histName,nMasses)
-
-        deltas = [masses[i+1]-masses[i] for i in range(nMasses-1)]
-        assert len(set(deltas))==1,set(deltas)
-
-        minMass = min(masses)
-        maxMass = max(masses)
-        binWidth = (maxMass - minMass + 0.0) / (nMasses-1)
-        histo = r.TH1D(histName, "%s; mass (GeV);#sigma (pb)"%histName, nMasses, minMass-binWidth/2.0, maxMass+binWidth/2.0)
-        for mass,(xs,xsErr) in dct.iteritems() :
-            histo.SetBinContent(histo.FindBin(mass), xs)
-        histosOut[histName]['hist'] = histo
-        histosOut[histName]['com'] = fileName[-8:-4]
-    return histosOut
+def getReferenceXsHisto(refHistoName, filename):
+    refFile = r.TFile(filename,'READ')
+    refHisto = refFile.Get(refHistoName).Clone()
+    refHisto.SetDirectory(0)
+    refFile.Close()
+    histoD = {
+        refHistoName: {
+            'hist': refHisto,
+            'LineColor': r.kBlue,
+            'label': '{0} pair production'.format(refHistoName)
+            }
+        }
+    return histoD
 
 
-def exclusionHisto(xsFile, yMinMax=(50,50)):
-    xsHistoDict = {
+def getExclusionHistos(limitFile, yMinMax=(50,50)):
+    limitHistoDict = {
         'UpperLimit': {
-            'com': 'Upper Limit',
-            'lineStyle': 0,
-            'lineWidth': 2,
-            'lineColor': r.kPink,
+            'label': 'Upper Limit',
+            'LineWidth': 2,
+            'LineColor': r.kPink,
+            'opts': '][',
             },
         'ExpectedUpperLimit': {
-            'com': 'Expected Upper Limit',
-            'lineStyle': 1,
-            'lineColor': 46,
+            'label': 'Expected Upper Limit',
+            'LineColor': 46,
+            'opts': 'F][',
             },
         'ExpectedUpperLimit_-1_Sigma': {
-            'com': 'Expected Upper Limit (-1 #sigma)',
-            'lineStyle': 2,
-            'lineColor': 48,
+            'label': 'Expected Upper Limit (-1 #sigma)',
+            'LineStyle': 2,
+            'LineColor': 48,
+            'opts': '][',
             },
         'ExpectedUpperLimit_+1_Sigma': {
-            'com': 'Expected Upper Limit (+1 #sigma)',
-            'lineStyle': 2,
-            'lineColor': 48,
+            'label': 'Expected Upper Limit (+1 #sigma)',
+            'LineStyle': 2,
+            'LineColor': 48,
+            'opts': '][',
             },
         }
 
-    rfile = r.TFile(xsFile,'READ')
-    for xsHistoName, opts in xsHistoDict.iteritems():
-        xsHisto = threeToTwo(rfile.Get(xsHistoName))
-        minYBin = xsHisto.GetYaxis().FindBin(yMinMax[0])
-        maxYBin = xsHisto.GetYaxis().FindBin(yMinMax[1])
+    rfile = r.TFile(limitFile,'READ')
+    for limitHistoName, opts in limitHistoDict.iteritems():
+        limitHisto = threeToTwo(rfile.Get(limitHistoName))
+        minYBin = limitHisto.GetYaxis().FindBin(yMinMax[0])
+        maxYBin = limitHisto.GetYaxis().FindBin(yMinMax[1])
 
-        opts['hist'] = xsHisto.ProjectionX('T2tt',minYBin,maxYBin).Clone()
+        opts['hist'] = limitHisto.ProjectionX('T2tt',minYBin,maxYBin).Clone()
         opts['hist'].SetDirectory(0)
 
     rfile.Close()
-    return xsHistoDict
+    return limitHistoDict
 
 
-def makeRootFile(fileName = "", xsFile=None) :
-    xsHistos = exclusionHisto(xsFile=xsFile)
+def compareXs(refProcess, refXsFile="sms_xs/sms_xs.root",
+              limitFile="xsLimit.root", pdfFile="sms_xs/compareXs.pdf",
+              plotOpts=None) :
+    if plotOpts is None:
+        plotOpts = {
+            'yMax': 2e+1,
+            'yMin': 2e-4,
+            'xMin': 300,
+            'xMax': 1200,
+            }
 
-    outFile = r.TFile(fileName, "RECREATE")
+    refHisto = getReferenceXsHisto(refProcess, refXsFile)
+    exclusionHistos = getExclusionHistos(limitFile)
 
     canvas = r.TCanvas()
-    pdfFile = "sms_xs/sms_xs.pdf"
-
-    hs = histos()
-    hs.update(xsHistos)
+    hs = dict(refHisto.items() + exclusionHistos.items())
 
     leg = r.TLegend(0.5, 0.7, 0.88, 0.88)
     leg.SetFillStyle(0)
     leg.SetBorderSize(0)
     for iHisto,(hname,props) in enumerate(sorted(hs.iteritems(), reverse=True)) :
         h = props['hist']
-        isExcl = ("Limit" in hname)
-        h.Write()
         h.SetStats(False)
         h.SetTitle("")
-        h.GetXaxis().SetRangeUser(300,1200)
-        h.SetMinimum(2e-4)
-        h.SetMaximum(2e+1)
-        baseOpts = "c" if not isExcl else "]["
-        h.Draw("%s%s"%(baseOpts, "same" if iHisto else ""))
-        lineColor = props.get('lineColor',4+iHisto)
-        markerColor = props.get('markerColor',1+iHisto)
-        lineStyle = props.get('lineStyle',1)
-        lineWidth = props.get('lineWidth',1)
-        h.SetLineColor(lineColor)
-        h.SetMarkerColor(markerColor)
-        h.SetLineStyle(lineStyle)
-        h.SetLineWidth(lineWidth)
-        entry = " ".join([props['com'].replace("TeV", " TeV"),
-                          h.GetName().replace("_"," "),
-                          "pair" if not isExcl else ""])
-        leg.AddEntry(h, entry, "lp")
+        h.GetXaxis().SetRangeUser(plotOpts['xMin'],plotOpts['xMax'])
+        h.SetMinimum(plotOpts['yMin'])
+        h.SetMaximum(plotOpts['yMax'])
+        h.Draw("%s%s"%(props.get('opts','c'), "same" if iHisto else ""))
+        for attr in ['LineColor', 'LineStyle', 'LineWidth']:
+            eval('h.Set{attr}(props.get("{attr}",1))'.format(attr=attr))
+        leg.AddEntry(h, props['label'], "l")
     leg.Draw()
     canvas.SetLogy()
     canvas.SetTickx()
     canvas.SetTicky()
     canvas.Print(pdfFile)
-    outFile.Close()
 
 def setup() :
     r.gROOT.SetBatch(True)
     r.gErrorIgnoreLevel = 2000
 
-setup()
-xsFile = ('~/Projects/ra1ToyResults/2011/1000_toys/T2tt/'
-          'CLs_frequentist_TS3_T2tt_lo_RQcdFallingExpExt_fZinvTwo_55_0b-1hx2p_'
-          '55_1b-1hx2p_55_2b-1hx2p_55_gt2b-1h.root')
-makeRootFile(fileName = "sms_xs/sms_xs.root", xsFile=xsFile)
+def main():
+    setup()
+    limitFile = ('~/Projects/ra1ToyResults/2011/1000_toys/T2tt/'
+                 'CLs_frequentist_TS3_T2tt_lo_RQcdFallingExpExt_fZinvTwo_55_'
+                 '0b-1hx2p_55_1b-1hx2p_55_2b-1hx2p_55_gt2b-1h.root')
+    compareXs(refProcess='squark', refXsFile="sms_xs/sms_xs.root", limitFile=limitFile)
+
+if __name__=="__main__":
+    main()
