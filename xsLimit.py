66ad42465625bf724705d27980b0cd52a9440fcb
Edward Laird <elaird@gmail.com>
2012-07-01 20:05:41 +0200

    adjust bin shift interface

--- xsLimit.py
+++ xsLimit.py
@@ -3,11 +3,12 @@
 import plottingGrid
 
 plottingGrid.makeXsUpperLimitPlots(logZ = True,
-                                   shiftBins = (True, True),
                                    #simpleExcl = True,
                                    #exclusionCurves = False,
                                    #mDeltaFuncs = {"mDeltaMin":0.0, "mDeltaMax":400.0, "nSteps":4, "mGMax":1250.},
                                    #printXs = True,
+                                   **{"shiftX":True, "shiftY":True, "interBin":"Center"}
+                                   #**{"shiftX":False, "shiftY":False, "interBin":"LowEdge"}
                                    )
 
 plottingGrid.makeEfficiencyPlot()
