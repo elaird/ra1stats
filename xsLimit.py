da3c7be1c446e50eda0b838114bbbd7d42e2a2b3
Sam Rogerson <samr@cern.ch>
2012-07-25 15:55:10 +0100

    remake limits

--- xsLimit.py
+++ xsLimit.py
@@ -5,10 +5,10 @@ import plottingGrid
 plottingGrid.makeXsUpperLimitPlots(logZ = True,
                                    debug = False,
                                    pruneYMin = True,
-                                   exclusionCurves = False,
+                                   exclusionCurves = True,
                                    #mDeltaFuncs = {"mDeltaMin":0.0, "mDeltaMax":400.0, "nSteps":4, "mGMax":1250.},
                                    #printXs = True,
-                                   **{"shiftX":True, "shiftY":True, "interBin":"Center"}
+                                   #**{"shiftX":True, "shiftY":True, "interBin":"Center"}
                                    #**{"shiftX":False, "shiftY":False, "interBin":"LowEdge"}
                                    )
 
