d6c5fde8eb7b1c7ad610f87e717c5cc02827cf81
Sam Rogerson <samr@cern.ch>
2012-09-11 14:19:11 +0100

    add new 2011 model and signalLineStyle option

--- test.py
+++ test.py
@@ -6,10 +6,12 @@ def go(iLower = None, iUpper = None, dataset = "2011", ensemble = False) :
     spec = likelihoodSpec.spec(iLower = iLower, iUpper = iUpper,
                                dataset = dataset, separateSystObs = not ensemble)
 
-    signalExampleToStack = {"2011": [signals.t2bb, signals.t1][1],
+    model_sel = 2
+    signalExampleToStack = {"2011": [signals.t2bb, signals.t1, signals.t2tt2][model_sel],
                             "2012ichep": signals.t1tttt_2012_3,
                             "2012dev": {},
                             }[dataset]
+    signalLineStyle = model_sel+1
 
     nToys = {"2011":3000,
              "2012ichep":1000,
@@ -18,7 +20,7 @@ def go(iLower = None, iUpper = None, dataset = "2011", ensemble = False) :
 
     f = workspace.foo(likelihoodSpec = spec,
                       #signalToTest = signals.t2tt2,
-                      signalExampleToStack = signalExampleToStack,
+                      signalExampleToStack = signalExampleToStack
                       #signalToInject = signals.t1,
                       #trace = True
                       #rhoSignalMin = 0.1,
@@ -43,7 +45,7 @@ def go(iLower = None, iUpper = None, dataset = "2011", ensemble = False) :
     #f.writeMlTable()
     #f.bestFit(drawMc = False, printValues = True, errorsFromToys = False, pullPlotMax = 4.0, pullThreshold = 5.0)
     f.bestFit(printPages = True, drawComponents = False, errorsFromToys = nToys,
-            signalLineStyle = 2)
+            signalLineStyle = signalLineStyle)
     #f.bestFit(drawMc = False, drawComponents = False, errorsFromToys = nToys)
     #f.qcdPlot()
     #print f.clsCustom(nToys = 500, testStatType = 1)
