31e5efcc731d4137d7c4680470514b603cf77957
Edward Laird <elaird@gmail.com>
2012-10-05 12:37:39 +0200

    functionize and loop on modes

--- aux/lognormalExample.py
+++ aux/lognormalExample.py
@@ -2,47 +2,66 @@
 
 import ROOT as r
 
-x     = r.RooRealVar("x", "x", 1.0, 0.0, 10.0) #desire to be fixed
-mu    = r.RooRealVar("mu","mean of gaussian", 1.0, 0.0, 3.0) #floating paramter in range [0-3]
-
-sigmaValues = [0.1, 0.2, 0.26, 0.4]
-canvas = r.TCanvas()
-canvas.Divide(len(sigmaValues), 1)
-
-keep = []
-text = r.TLatex()
-text.SetNDC()
-
-color = {"gauss":r.kRed,
-         "lognormal":r.kBlue,
-         }
-
-for i,sigmaValue in enumerate(sigmaValues) :
-    canvas.cd(1+i)
-    sigma = r.RooRealVar("sigma","width of gaussian", sigmaValue) #fixed
-    k     = r.RooRealVar("k","shape param. for log-normal", r.TMath.Exp(sigmaValue)) #fixed
-
-    gauss     = r.RooGaussian("gauss","gaussian PDF", x, mu, sigma)
-    lognormal = r.RooLognormal("lognormal","lognormal PDF", x, mu, k)
-
-    xframe = x.frame(0.0, 3.0) #specify range
-
-    for item in [gauss, lognormal] :
-        item.plotOn(xframe)
-        xframe.getAttLine().SetLineColor(color[item.GetName()])
-
-    xframe.Draw()
+def stamp(keep = [], mode = "", gauss = None, lognormal = None, sigmaValue = None) :
+    text = r.TLatex()
+    text.SetNDC()
 
     text.SetTextColor(r.kBlack)
-    keep.append( text.DrawLatex(0.5, 0.8, "#mu = 1.0,   #sigma = %4.2f"%sigmaValue) )
+    x0 = 0.47
+    y0 = 0.88
+    s = 0.03
+    if mode=="x" :
+        keep.append( text.DrawLatex(x0, y0-1*s, "#mu = %3.1f"%mu.getVal()) )
+        keep.append( text.DrawLatex(x0, y0-2*s, "#sigma = %4.2f"%sigmaValue) )
+    if mode=="mu" :
+        keep.append( text.DrawLatex(x0, y0-1*s, "x = %3.1f"%x.getVal()) )
+        keep.append( text.DrawLatex(x0, y0-2*s, "#sigma = %4.2f"%sigmaValue) )
 
     for i,item in enumerate([gauss, lognormal]) :
         name = item.GetName()
         text.SetTextColor(color[name])
-        keep.append( text.DrawLatex(0.5, 0.8-0.04*(1+i), name) )
+        keep.append( text.DrawLatex(x0, y0-s*(3+i), name+"(x|  #mu, #sigma)") )
 
-    r.gPad.SetTickx()
-    r.gPad.SetTicky()
+def plot() :
+    sigmaValues = [0.1, 0.2, 0.26, 0.4]
+    canvas = r.TCanvas()
+    canvas.Divide(len(sigmaValues), 1)
 
-canvas.cd(0)
-canvas.Print("lognormal.pdf")
+    keep = []
+    for i,sigmaValue in enumerate(sigmaValues) :
+        canvas.cd(1+i)
+        sigma = r.RooRealVar("sigma","width of gaussian", sigmaValue) #fixed
+        k     = r.RooRealVar("k","shape param. for log-normal", r.TMath.Exp(sigmaValue)) #fixed
+
+        gauss     = r.RooGaussian("gauss","gaussian PDF", x, mu, sigma)
+        lognormal = r.RooLognormal("lognormal","lognormal PDF", x, mu, k)
+
+        frame = (x if mode=="x" else mu).frame()
+
+        for item in [gauss, lognormal] :
+            item.plotOn(frame)
+            frame.getAttLine().SetLineColor(color[item.GetName()])
+
+        frame.SetTitle("")
+        frame.Draw()
+
+        stamp(keep, mode, gauss, lognormal, sigmaValue)
+
+        r.gPad.SetTickx()
+        r.gPad.SetTicky()
+
+    canvas.cd(0)
+    canvas.Print("lognormal_%s.pdf"%mode)
+
+color = {"gauss":r.kRed, "lognormal":r.kBlue}
+for mode in ["x", "mu"] :
+    if mode=="x" :
+        x     = r.RooRealVar("x", "observation (x)", 1.0, 0.0, 3.0) #observable
+        mu    = r.RooRealVar("mu","mean of gaussian (#mu)", 1.0) #fixed mean
+    if mode=="mu" :
+        x     = r.RooRealVar("x", "observation (x)", 1.0) #fixed observation
+        mu    = r.RooRealVar("mu","mean of gaussian (#mu)", 1.0, 0.0, 3.0) #floating paramter in range [0-3]
+    plot()
+
+
+
