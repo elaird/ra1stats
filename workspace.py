17ee6d72f74416948b314f8960571a274e5e4d83
Edward Laird <elaird@gmail.com>
2012-11-01 15:44:01 +0000

    eff unc rel from signal

--- workspace.py
+++ workspace.py
@@ -191,7 +191,6 @@ def hadTerms(w = None, inputData = None, label = "", systematicsLabel = "", kQcd
     #more
     iFirst = None
     iLast = len(htMeans)-1
-    systBin = inputData.systBins()["sigmaLumiLike"]
     for i,nHadValue in enumerate(obs["nHad"]) :
         if nHadValue==None : continue
         if iFirst==None : iFirst = i
@@ -228,7 +227,7 @@ def hadTerms(w = None, inputData = None, label = "", systematicsLabel = "", kQcd
             lumi = ni("hadLumi", label)
             eff = ni("signalEffHad", label, i)
             assert w.var(eff),eff
-            rho = ni("rhoSignal", systematicsLabel, systBin[i])
+            rho = ni("rhoSignal", systematicsLabel)
             wimport(w, r.RooProduct(hadS, hadS, r.RooArgSet(w.var("f"), w.var(rho), w.var("xs"), w.var(lumi), w.var(eff))))
             wimport(w, r.RooAddition(hadExp, hadExp, r.RooArgSet(w.function(hadB), w.function(hadS))))
             wimport(w, r.RooPoisson(hadPois, hadPois, w.var(nHad), w.function(hadExp)))
@@ -390,7 +389,6 @@ def muonTerms(w = None, inputData = None, label = "", systematicsLabel = "", kQc
             out["systObs"].append(one)
 
     systBin = inputData.systBins()["sigmaMuonW"]
-    signalSystBin = inputData.systBins()["sigmaLumiLike"]
     for i,nMuonValue,mcMuonValue,mcTtwValue,mcZinvValue in zip(range(len(inputData.observations()["nMuon"])),
                                                                inputData.observations()["nMuon"],
                                                                inputData.mcExpectations()["mcMuon"],
@@ -422,7 +420,7 @@ def muonTerms(w = None, inputData = None, label = "", systematicsLabel = "", kQc
             muonS = ni("muonS", label, i)
             muonExp = ni("muonExp", label, i)
             lumi = ni("muonLumi", label)
-            rhoSignal = ni("rhoSignal", systematicsLabel, signalSystBin[i])
+            rhoSignal = ni("rhoSignal", systematicsLabel)
             wimport(w, r.RooProduct(muonS, muonS, r.RooArgSet(w.var("f"), w.var(rhoSignal), w.var("xs"), w.var(lumi), w.var(eff))))
             wimport(w, r.RooAddition(muonExp, muonExp, r.RooArgSet(w.function(muonB), w.function(muonS))))
             wimport(w, r.RooPoisson(muonPois, muonPois, w.var(nMuon), w.function(muonExp)))
@@ -479,16 +477,15 @@ def signalTerms(w = None, inputData = None, label = "", systematicsLabel = "", k
 
     out = collections.defaultdict(list)
     if label==systematicsLabel :
-        for iPar in set(inputData.systBins()["sigmaLumiLike"]) :
-            #deltaSignalValue = utils.quadSum([inputData.fixedParameters()["sigmaLumiLike"]]+[signalToTest[item] for item in extraSigEffUncSources])
-            deltaSignalValue = inputData.fixedParameters()["sigmaLumiLike"][iPar]
+        for iPar in [None] :
             one = ni("oneRhoSignal", label, iPar)
             rho = ni("rhoSignal", label, iPar)
             delta = ni("deltaSignal", label, iPar)
             gaus = ni("signalGaus", label, iPar)
 
             wimport(w, r.RooRealVar(rho, rho, 1.0, rhoSignalMin, 2.0))
-            systTerm(w, name = gaus, obsName = one, obsValue = 1.0, muVar = w.var(rho), sigmaName = delta, sigmaValue = deltaSignalValue)
+            systTerm(w, name = gaus, obsName = one, obsValue = 1.0, muVar = w.var(rho),
+                     sigmaName = delta, sigmaValue = w.var("effUncRel").getVal())
 
             signalTermsName = ni("signalTerms", label, iPar)
             w.factory("PROD::%s(%s)"%(signalTermsName, gaus))
@@ -578,8 +575,9 @@ def setupLikelihood(w = None, selection = None, systematicsLabel = None, kQcdLab
     out["systObs" if separateSystObs else "obs"] += variables["systObs"]
     return out
 
-def startLikelihood(w = None, xs = None, fIniFactor = None, poi = {}) :
+def startLikelihood(w = None, xs = None, effUncRel = None, fIniFactor = None, poi = {}) :
     wimport(w, r.RooRealVar("xs", "xs", xs))
+    wimport(w, r.RooRealVar("effUncRel", "effUncRel", effUncRel))
     fIni,fMin,fMax = poi["f"]
     wimport(w, r.RooRealVar("f", "f", fIniFactor*fIni, fMin, fMax))
 
@@ -627,7 +625,8 @@ class foo(object) :
             args[item] = getattr(self, item)
 
         if not self.smOnly() :
-            startLikelihood(w = self.wspace, xs = self.signalToTest.xs, fIniFactor = fIniFactor, poi = self.likelihoodSpec.poi())
+            startLikelihood(w = self.wspace, xs = self.signalToTest.xs, effUncRel = signalToTest.effUncRel,
+                            fIniFactor = fIniFactor, poi = self.likelihoodSpec.poi())
 
         total = collections.defaultdict(list)
         for sel in self.likelihoodSpec.selections() :
