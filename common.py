5fafcf1f87a4efe406a1efa9407721b1a91e8a60
Edward Laird <elaird@gmail.com>
2012-11-12 13:42:12 +0100

    line props

--- common.py
+++ common.py
@@ -1,8 +1,8 @@
 import ROOT as r
 
 class signal(dict) :
-    def __init__(self, xs = None, label = "", effUncRel = None) :
-        for item in ["xs", "label", "effUncRel"] :
+    def __init__(self, xs = None, label = "", effUncRel = None, lineColor = r.kPink+7, lineStyle = 1) :
+        for item in ["xs", "label", "effUncRel", "lineColor", "lineStyle"] :
             assert eval(item)!=None,item
             setattr(self, item, eval(item))
     
