8b70ffef8764aaed8479bf4d08550cd39428c2ea
Edward Laird <elaird@gmail.com>
2012-11-12 11:42:06 +0100

    y axis range

--- likelihoodSpec.py
+++ likelihoodSpec.py
@@ -110,7 +110,7 @@ class spec(object) :
 
                 yAxisLogMinMax = {"0"  :(0.3, None),
                                   "1"  :(0.3, None),
-                                  "2"  :(0.3, None),
+                                  "2"  :(0.3, None) if j!="le3" else (0.05, None),
                                   "3"  :(0.3, None),
                                   "ge4":(0.1, None),
                                   }[b]
