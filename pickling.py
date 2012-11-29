47d9ab8369a9bd3bd2f89536496b3e55fe056f4a
Edward Laird <elaird@gmail.com>
2012-11-29 17:59:09 +0000

    continue and print error on negative weights

--- pickling.py
+++ pickling.py
@@ -70,7 +70,10 @@ def signalModel(point = None, eff = None, xs = None, xsLo = None, nEventsIn = No
             d[box+"Sum"] = sum(d[box])
             key = box.replace("eff","nEvents")
             d[key] = d[box+"Sum"]*out["nEventsIn"]
-            if d[key] : d[box+"SumUncRelMcStats"] = 1.0/math.sqrt(d[key])
+            if d[key]>=0.0 :
+                d[box+"SumUncRelMcStats"] = 1.0/math.sqrt(d[key])
+            else :
+                print "ERROR: negative value: ",point,d[key]
         out[selName] = d
     return out
 
