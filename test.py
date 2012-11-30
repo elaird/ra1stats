a84ebc283d017035e7f92422e644740c32972507
Edward Laird <elaird@gmail.com>
2012-11-30 18:32:32 +0100

    use new examples

--- test.py
+++ test.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python
 import ROOT as r
-import common,workspace,likelihoodSpec,signals,plotting
+import common,workspace,likelihoodSpec,signals,signals2,plotting
 
 def go(whiteList = [], dataset = "2011", ensemble = False, allCategories = [], ignoreHad = False) :
-    examples = {("0b_le3j",):signals.t2_1,
-                ("0b_ge4j",):signals.t1_1,
-                ("1b_le3j",):signals.t2bb,
-                ("1b_ge4j",):{},#signals.t2tt_1,
-                ("2b_le3j",):signals.t2bb,
-                ("2b_ge4j",):signals.t1bbbb_1,#signals.t2tt_1,
+    examples = {("0b_le3j",):signals2.t2,
+                ("0b_ge4j",):signals2.t1,
+                ("1b_le3j",):signals2.t2bb,
+                ("1b_ge4j",):signals2.t2tt,
+                ("2b_le3j",):signals2.t2bb,
+                ("2b_ge4j",):signals2.t2tt,
                 ("3b_le3j",):{},
-                ("3b_ge4j",):signals.t1bbbb_1,
-                ("ge4b_ge4j",):signals.t1bbbb_1,
+                ("3b_ge4j",):signals2.t1bbbb,
+                ("ge4b_ge4j",):signals2.t1tttt,
                 }
     signal = examples[tuple(whiteList)] if tuple(whiteList) in examples else {}
     f = workspace.foo(likelihoodSpec = likelihoodSpec.spec(whiteList = whiteList, dataset = dataset, ignoreHad = ignoreHad,
