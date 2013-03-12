3813aba957ed5cfdc656c122f8e211caf2f05520
Edward Laird <elaird@gmail.com>
2013-03-12 18:26:26 +0000

    pep8

--- configuration/__init__.py
+++ configuration/__init__.py
@@ -2,8 +2,14 @@ def method():
     return {"CL": [0.95, 0.90][:1],
             "nToys": 1000,
             "testStatistic": 3,
-            "calculatorType": ["frequentist", "asymptotic", "asymptoticNom"][1],
-            "method": ["", "profileLikelihood", "feldmanCousins", "CLs", "CLsCustom"][3],
+            "calculatorType": ["frequentist",
+                               "asymptotic",
+                               "asymptoticNom"][1],
+            "method": ["",
+                       "profileLikelihood",
+                       "feldmanCousins",
+                       "CLs",
+                       "CLsCustom"][3],
             "binaryExclusionRatherThanUpperLimit": False,
             "multiplesInGeV": None,
             }
@@ -33,34 +39,51 @@ def switches():
     return out
 
 
-def checkAndAdjust(d) :
+def checkAndAdjust(d):
     d["isSms"] = "tanBeta" not in d["signalModel"]
     binary = d["binaryExclusionRatherThanUpperLimit"]
     d["rhoSignalMin"] = 0.0 if binary else 0.1
     d["fIniFactor"] = 1.0 if binary else 0.05
-    d["extraSigEffUncSources"] = [] if binary else [] #["effHadSumUncRelMcStats"]
+    if binary:
+        d["extraSigEffUncSources"] = []
+    else:
+        d["extraSigEffUncSources"] = []  # ["effHadSumUncRelMcStats"]
 
     d["plSeedParams"] = {"usePlSeed": False} if binary else \
-                        {"usePlSeed": True, "plNIterationsMax": 10, "nPoints": 10, "minFactor": 0.0, "maxFactor":3.0}
-                        #{"usePlSeed": True, "plNIterationsMax": 10, "nPoints": 7, "minFactor": 0.5, "maxFactor":2.0}
-    return
+                        {"usePlSeed": True,
+                         "plNIterationsMax": 10,
+                         "nPoints": 10,
+                         "minFactor": 0.0,
+                         "maxFactor": 3.0,
+                         }
+                        #{"usePlSeed": True,
+                        # "plNIterationsMax": 10,
+                        # "nPoints": 7,
+                        # "minFactor": 0.5,
+                        # "maxFactor": 2.0}
 
 
-def mergedFileStem() :
+def mergedFileStem():
     s = switches()
     tags = [s["method"]]
-    if "CLs" in s["method"] : tags += [s["calculatorType"], "TS%d"%s["testStatistic"]]
-    if s["binaryExclusionRatherThanUpperLimit"] : tags.append("binaryExcl")
+    if "CLs" in s["method"]:
+        tags += [s["calculatorType"], "TS%d" % s["testStatistic"]]
+    if s["binaryExclusionRatherThanUpperLimit"]:
+        tags.append("binaryExcl")
     tags.append(s["signalModel"])
-    if not s["isSms"] : tags.append(s["xsVariation"])
+    if not s["isSms"]:
+        tags.append(s["xsVariation"])
     return "ra1r/scan/"+"_".join(tags)
 
-def directories() :
-    return {"job"   : "jobIO",
-            "plot"  : "plots",
-            "log"   : "log",
+
+def directories():
+    return {"job": "jobIO",
+            "plot": "plots",
+            "log": "log",
             "points": "points",
             }
 
-def pickledFileName(xBin, yBin, zBin) :
-    return "%s/m0_%d_m12_%d_mZ_%d.pickled"%(directories()["job"], xBin, yBin, zBin)
+
+def pickledFileName(xBin, yBin, zBin):
+    return "%s/m0_%d_m12_%d_mZ_%d.pickled" % (directories()["job"],
+                                              xBin, yBin, zBin)
