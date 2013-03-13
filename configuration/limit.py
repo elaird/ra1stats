d9bc6a879e407eeebb289fb9ee62ccbbf44d63c2
Edward Laird <elaird@gmail.com>
2013-03-13 15:52:09 +0000

    split

---
+++ configuration/limit.py
@@ -0,0 +1,59 @@
+def CL():
+    return [0.95, 0.90][:1]
+
+
+def nToys():
+    return 1000
+
+
+def testStatistic():
+    return 3
+
+
+def calculatorType():
+    return ["frequentist",
+            "asymptotic",
+            "asymptoticNom"][1]
+
+
+def method():
+    return ["",
+            "profileLikelihood",
+            "feldmanCousins",
+            "CLs",
+            "CLsCustom"][3]
+
+
+def binaryExclusion():
+    return False
+
+
+def multiplesInGeV():
+    return None
+
+
+def plSeedParams():
+    if binaryExclusion():
+        return {"usePlSeed": False}
+    else:
+        return {"usePlSeed": True,
+                "plNIterationsMax": 10,
+                "nPoints": 10,
+                "minFactor": 0.0,
+                "maxFactor": 3.0,
+                #"nPoints": 7,
+                #"minFactor": 0.5,
+                #"maxFactor": 2.0,
+                }
+
+
+def rhoSignalMin():
+    return 0.0 if binaryExclusion() else 0.1
+
+
+def fIniFactor():
+    return 1.0 if binaryExclusion() else 0.05
+
+
+def extraSigEffUncSources():
+    return []  # ["effHadSumUncRelMcStats"]
