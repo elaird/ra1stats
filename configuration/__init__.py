12efa8b108b9a6ed4dd0875360b2b6465521facd
Edward Laird <elaird@gmail.com>
2013-03-13 14:28:45 +0000

    consolidate

--- configuration/__init__.py
+++ configuration/__init__.py
@@ -1,32 +1,33 @@
-def method():
-    return {"CL": [0.95, 0.90][:1],
-            "nToys": 1000,
-            "testStatistic": 3,
-            "calculatorType": ["frequentist",
-                               "asymptotic",
-                               "asymptoticNom"][1],
-            "method": ["",
-                       "profileLikelihood",
-                       "feldmanCousins",
-                       "CLs",
-                       "CLsCustom"][3],
-            "binaryExclusionRatherThanUpperLimit": False,
-            "multiplesInGeV": None,
+def directories():
+    return {"job": "jobIO",
+            "plot": "plots",
+            "log": "log",
+            "points": "points",
+            "mergedFile": "ra1r/scan/",
             }
 
 
+def pickledFileName(xBin, yBin, zBin):
+    return "%s/m0_%d_m12_%d_mZ_%d.pickled" % (directories()["job"],
+                                              xBin, yBin, zBin)
+
+
 def switches():
-    out = {}
-    lst = [method()]
-    keys = sum([dct.keys() for dct in lst], [])
-    assert len(keys) == len(set(keys))
-    for dct in lst:
-        out.update(dct)
-    checkAndAdjust(out)
-    return out
+    d = {"CL": [0.95, 0.90][:1],
+         "nToys": 1000,
+         "testStatistic": 3,
+         "calculatorType": ["frequentist",
+                            "asymptotic",
+                            "asymptoticNom"][1],
+         "method": ["",
+                    "profileLikelihood",
+                    "feldmanCousins",
+                    "CLs",
+                    "CLsCustom"][3],
+         "binaryExclusionRatherThanUpperLimit": False,
+         "multiplesInGeV": None,
+         }
 
-
-def checkAndAdjust(d):
     binary = d["binaryExclusionRatherThanUpperLimit"]
     d["rhoSignalMin"] = 0.0 if binary else 0.1
     d["fIniFactor"] = 1.0 if binary else 0.05
@@ -41,23 +42,8 @@ def checkAndAdjust(d):
                          "nPoints": 10,
                          "minFactor": 0.0,
                          "maxFactor": 3.0,
+                         #"nPoints": 7,
+                         #"minFactor": 0.5,
+                         #"maxFactor": 2.0,
                          }
-                        #{"usePlSeed": True,
-                        # "plNIterationsMax": 10,
-                        # "nPoints": 7,
-                        # "minFactor": 0.5,
-                        # "maxFactor": 2.0}
-
-
-def directories():
-    return {"job": "jobIO",
-            "plot": "plots",
-            "log": "log",
-            "points": "points",
-            "mergedFile":"ra1r/scan/",
-            }
-
-
-def pickledFileName(xBin, yBin, zBin):
-    return "%s/m0_%d_m12_%d_mZ_%d.pickled" % (directories()["job"],
-                                              xBin, yBin, zBin)
+    return d
