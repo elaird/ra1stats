7af08948f6cd97a03fa415d45a38d11524fda467
Edward Laird <elaird@gmail.com>
2013-03-14 21:26:16 +0000

    pep8

--- test.py
+++ test.py
@@ -1,8 +1,16 @@
 #!/usr/bin/env python
-import ROOT as r
-import common,workspace,likelihoodSpec,signals2,plotting
 
-def go(whiteList=[], dataset="", ensemble=False, allCategories=[], ignoreHad=False):
+import common
+import likelihoodSpec
+import plotting
+import signals2
+import workspace
+import ROOT as r
+
+
+def go(whiteList=[], dataset="", ensemble=False,
+       allCategories=[], ignoreHad=False):
+
     examples_paper = {("0b_le3j",): signals2.t2,
                       ("0b_ge4j",): signals2.t1,
                       ("1b_le3j",): signals2.t2bb,
@@ -27,9 +35,14 @@ def go(whiteList=[], dataset="", ensemble=False, allCategories=[], ignoreHad=Fal
 
     examples = examples_t2cc
     signal = examples[tuple(whiteList)] if tuple(whiteList) in examples else {}
-    f = workspace.foo(likelihoodSpec=likelihoodSpec.spec(whiteList=whiteList, dataset=dataset, ignoreHad=ignoreHad,
-                                                         separateSystObs=not ensemble
-                                                         ),
+
+    ls = likelihoodSpec.spec(whiteList=whiteList,
+                             dataset=dataset,
+                             ignoreHad=ignoreHad,
+                             separateSystObs=not ensemble
+                             )
+
+    f = workspace.foo(likelihoodSpec=ls,
                       #signalToTest=signal,
                       signalExampleToStack=signal,
                       #signalToInject=signal,
@@ -40,51 +53,77 @@ def go(whiteList=[], dataset="", ensemble=False, allCategories=[], ignoreHad=Fal
                       )
 
     out = None
-    nToys = {"":0, "2010":300, "2011eps":300, "2011":3000,
-             "2012ichep":1000, "2012hcp":300, "2012hcp2":300, "2012dev":300}[dataset]
+    nToys = {"": 0, "2010": 300, "2011eps": 300, "2011": 3000,
+             "2012ichep": 1000, "2012hcp": 300,
+             "2012hcp2": 300, "2012dev": 300}[dataset]
 
     if ensemble:
         f.ensemble(nToys=nToys, stdout=True)
         return out
 
     #cl = 0.95 if f.likelihoodSpec.standardPoi() else 0.68
-    #out = f.interval(cl=cl, method=["profileLikelihood", "feldmanCousins"][0], makePlots=True); print out
-    #out = f.cls(cl=cl, plusMinus={"OneSigma": 1.0, "TwoSigma": 2.0}, makePlots=True,
-    #            calculatorType=["frequentist", "asymptotic", "asymptoticNom"][1],
-    #            testStatType=3, nToys=50, nWorkers=1,
-    #            #plSeedParams={"usePlSeed": True, "plNIterationsMax": 10, "nPoints": 7, "minFactor": 0.5, "maxFactor":2.0},
-    #            plSeedParams={"usePlSeed": True, "plNIterationsMax": 10, "nPoints": 10, "minFactor": 0.0, "maxFactor":3.0},
+    #out = f.interval(cl=cl,
+    #                 method=["profileLikelihood", "feldmanCousins"][0],
+    #                 makePlots=True,
+    #                 ); print out
+    #f.profile()
+    #out = f.cls(cl=cl,
+    #            makePlots=True,
+    #            testStatType=3,
+    #            nToys=50,
+    #            nWorkers=1,
+    #            plusMinus={"OneSigma": 1.0,
+    #                       "TwoSigma": 2.0},
+    #            calculatorType=["frequentist",
+    #                            "asymptotic",
+    #                            "asymptoticNom"][1],
+    #            #plSeedParams={"usePlSeed": True,
+    #            #              "plNIterationsMax": 10,
+    #            #              "nPoints": 7,
+    #            #              "minFactor": 0.5,
+    #            #              "maxFactor":2.0},
+    #            plSeedParams={"usePlSeed": True,
+    #                          "plNIterationsMax": 10,
+    #                          "nPoints": 10,
+    #                          "minFactor": 0.0,
+    #                          "maxFactor":3.0},
     #            ); print out
     #
-    #f.profile()
-    #f.writeMlTable(fileName="mlTables_%s.tex"%"_".join(whiteList), categories=allCategories)
+    #f.writeMlTable(fileName="mlTables_%s.tex" % "_".join(whiteList),
+    #               categories=allCategories)
     #f.bestFit(printPages=True, drawComponents=False, errorsFromToys=nToys)
-    out = f.bestFit(drawMc = False, drawComponents = False, errorsFromToys = nToys)
+    #out = f.bestFit(drawMc=False, drawComponents=False, errorsFromToys=nToys)
     #f.qcdPlot()
     #f.debug()
     #f.cppDrive(tool="")
     return out
 
-kargs = {"dataset" : ["", "2010", "2011eps", "2011", "2012ichep", "2012hcp", "2012hcp2", "2012dev"][-2],
-         "ensemble": False,
+kargs = {"ensemble": False,
+         "dataset": ["", "2010", "2011eps", "2011",
+                     "2012ichep", "2012hcp", "2012hcp2", "2012dev"][-2],
          }
-if kargs["dataset"]=="2011":
+
+if kargs["dataset"] == "2011":
     go(**kargs)
-else :
-    selections = likelihoodSpec.spec(dataset = kargs["dataset"]).selections()
+else:
+    selections = likelihoodSpec.spec(dataset=kargs["dataset"]).selections()
     hMap = {}
     bins = (len(selections), 0.0, len(selections))
     for key in ["chi2ProbSimple", "chi2Prob", "lMax"]:
-        hMap[key] = r.TH1D("pValueMap_%s"%key, ";category;p-value", *bins)
+        hMap[key] = r.TH1D("pValueMap_%s" % key, ";category;p-value", *bins)
 
     for iSel, sel in enumerate(selections):
-        args = {"whiteList":[sel.name], "allCategories":sorted([x.name for x in selections])}
+        args = {"whiteList": [sel.name],
+                "allCategories": sorted([x.name for x in selections]),
+                }
         args.update(kargs)
         dct = go(**args)
-        if not dct : continue
+        if not dct:
+            continue
         for key, pValue in dct.iteritems():
-            if key not in hMap : continue
+            if key not in hMap:
+                continue
             hMap[key].GetXaxis().SetBinLabel(1+iSel, sel.name)
             hMap[key].SetBinContent(1+iSel, pValue)
 
-    plotting.pValueCategoryPlots(hMap, )#logYMinMax = (1.0e-4, 1.0e2))
+    plotting.pValueCategoryPlots(hMap, )  # logYMinMax=(1.0e-4, 1.0e2))
