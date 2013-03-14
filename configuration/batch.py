4d4e90d8ed88e6c82dd1a3dfa5a2db58106a42d8
Edward Laird <elaird@gmail.com>
2013-03-14 08:34:27 +0000

    consolidate

--- configuration/batch.py
+++ configuration/batch.py
@@ -1,12 +1,12 @@
 batchHost = ["FNAL", "IC"][1]
 
+icQueues = {"short":  {"ncores": 336, "factor":  1.},
+            "medium": {"ncores": 116, "factor":  6.},
+            "long":   {"ncores":  52, "factor": 72.},
+            }
+
 
 def qData(selection=["short", "medium", "long"][0:1]):
-    icQueues = {"short":  {"ncores": 336, "factor":  1.},
-                "medium": {"ncores": 116, "factor":  6.},
-                "long":   {"ncores":  52, "factor": 72.},
-                }
-
     out = {}
     for name in selection:
         out[name] = icQueues[name]
@@ -26,7 +26,7 @@ def nJobsMax():
 
 
 def subCmd(icQueue="medium"):
-    assert icQueue in ["short", "medium", "long"], icQueue
+    assert icQueue in icQueues, icQueue
     return {"IC": "qsub -o /dev/null -e /dev/null -q hep%s.q" % icQueue,
             "FNAL": "condor_submit",
             }[batchHost]
