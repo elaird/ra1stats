71d024f4b8b9695ffbb99ea930ff5dc1b82ad26b
Edward Laird <elaird@gmail.com>
2013-05-15 15:24:03 +0200

    skeleton

---
+++ patches/T2_xs0p8.py
@@ -0,0 +1,34 @@
+def ExpectedUpperLimit_m1_Sigma():
+    return {"blackList": [],
+            "replace": {},
+            }
+
+
+def ExpectedUpperLimit_p1_Sigma():
+    return {"blackList": [],
+            "replace": {},
+            }
+
+
+def ExpectedUpperLimit():
+    return {"blackList": [],
+            "replace": {},
+            }
+
+
+def UpperLimit():
+    return {"blackList": [],
+            "replace": {},
+            }
+
+
+def UpperLimit_p1_Sigma():
+    return {"blackList": [],
+            "replace": {},
+            }
+
+
+def UpperLimit_m1_Sigma():
+    return {"blackList": [],
+            "replace": {},
+            }
