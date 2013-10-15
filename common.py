bb496a197022f580854666722d09c0534edfc766
yeshaq <yossofeshaq@gmail.com>
2013-10-15 13:32:13 +0200

    add nEventsIn

--- common.py
+++ common.py
@@ -1,8 +1,8 @@
 import ROOT as r
 
 class signal(dict) :
-    def __init__(self, xs = None, label = "", effUncRel = None, lineColor = r.kPink+7, lineStyle = 2) :
-        for item in ["xs", "label", "effUncRel", "lineColor", "lineStyle"] :
+    def __init__(self, xs = None, nEventsIn = "", label = "", effUncRel = None, lineColor = r.kPink+7, lineStyle = 2) :
+        for item in ["xs", "nEventsIn", "label", "effUncRel", "lineColor", "lineStyle"] :
             assert eval(item)!=None,item
             setattr(self, item, eval(item))
     
