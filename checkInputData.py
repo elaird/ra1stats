00431698384c68d696c1256bd487a5a617bc85e1
Edward Laird <elaird@gmail.com>
2013-12-14 06:26:24 +0100

    pep8

--- checkInputData.py
+++ checkInputData.py
@@ -1,29 +1,40 @@
 #!/usr/bin/env python
-import data,os,sys
 
-def check(topDir, fileName) :
-    if len(fileName)<3 or fileName[-3:]!=".py" : return
+import data
+import os
+import sys
+
+
+def check(topDir, fileName):
+    if len(fileName) < 3 or fileName[-3:] != ".py":
+        return
+
     module = fileName[:-3]
-    if module in ["__init__"] : return
+    if module in ["__init__"]:
+        return
 
-    exec("from %s import %s"%(topDir, module))
-    for item in dir(eval(module)) :
-        if item=="data" : continue
-        obj = eval("%s.%s"%(module,item))
-        if type(obj)!=type : continue
-        try :
+    exec("from %s import %s" % (topDir, module))
+    for item in dir(eval(module)):
+        if item == "data":
+            continue
+        obj = eval("%s.%s" % (module, item))
+        if type(obj) != type:
+            continue
+        try:
             a = obj()
         except:
-            print module,item
+            print module, item
             sys.excepthook(*sys.exc_info())
             print
 
-def walk(topDir = "") :
-    for dirName in os.listdir("%s/"%topDir) :
-        if dirName.startswith("__init__") or dirName.startswith("syst") : continue
-        print dirName,":"
-        
-        for fileName in os.listdir("%s/%s"%(topDir, dirName)) :
+
+def walk(topDir=""):
+    for dirName in os.listdir("%s/" % topDir):
+        if dirName.startswith("__init__") or dirName.startswith("syst"):
+            continue
+
+        print dirName, ":"
+        for fileName in os.listdir("%s/%s" % (topDir, dirName)):
             print fileName
             check(topDir, fileName)
 
