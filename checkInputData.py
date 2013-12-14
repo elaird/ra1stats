717892c0d86181b992d0dee142c64fd3c2d53b91
Edward Laird <elaird@gmail.com>
2013-12-14 06:49:52 +0100

    refine

--- checkInputData.py
+++ checkInputData.py
@@ -5,7 +5,7 @@ import os
 import sys
 
 
-def check(topDir, fileName):
+def check(topDir, subDir, fileName):
     if len(fileName) < 3 or fileName[-3:] != ".py":
         return
 
@@ -13,7 +13,14 @@ def check(topDir, fileName):
     if module in ["__init__"]:
         return
 
-    exec("from %s import %s" % (topDir, module))
+    try:
+        exec("from %s.%s import %s" % (topDir, subDir, module))
+    except:
+        print module
+        sys.excepthook(*sys.exc_info())
+        print
+        return False
+
     for item in dir(eval(module)):
         if item == "data":
             continue
@@ -22,21 +29,39 @@ def check(topDir, fileName):
             continue
         try:
             a = obj()
+            return True
         except:
             print module, item
             sys.excepthook(*sys.exc_info())
             print
+            return False
 
 
-def walk(topDir=""):
-    for dirName in os.listdir("%s/" % topDir):
-        if dirName.startswith("__init__") or dirName.startswith("syst"):
+def walk(top="", topSkip=[]):
+    passed = []
+    failed = []
+    for dirName in os.listdir("%s/" % top):
+        if dirName.startswith("__init__") or dirName.startswith("syst") or dirName in topSkip:
             continue
 
-        print dirName, ":"
-        for fileName in os.listdir("%s/%s" % (topDir, dirName)):
-            print fileName
-            check(topDir, fileName)
+        print "%s:" % dirName
+        for fileName in os.listdir("%s/%s" % (top, dirName)):
+            if fileName == "__init__.py" or fileName.endswith(".pyc"):
+                continue
+            t = (top, dirName, fileName)
+            (passed if check(*t) else failed).append(t)
+    return passed, failed
 
 
-walk("inputData")
+def report(passed=[], failed=[]):
+    def dump(l=[]):
+        for t in l:
+            print "/".join(t)
+
+    print "%d passed:" % len(passed)
+    dump(passed)
+    print "%d failed:" % len(failed)
+    dump(failed)
+
+
+report(*walk(top="inputData", topSkip=["data2011old"]))
