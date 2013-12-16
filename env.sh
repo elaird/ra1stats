c55f39c4bf6ec0300b8245d4d839ffd56975427d
Edward Laird <elaird@gmail.com>
2013-12-16 10:27:06 +0000

    generic dir

--- env.sh
+++ env.sh
@@ -6,8 +6,7 @@ GCCVER="4.3.2"
 PYVER="2.6.5"
 if [[ "$HOSTNAME" == *hep.ph.ic.ac.uk ]]; then
     . /vols/cms/grid/setup.sh
-    cd ~elaird1/CMSSW_6_1_2/src
-    cmsenv
+    cd /vols/sl5_exp_software/cms/slc5_amd64_gcc472/cms/cmssw/CMSSW_6_1_2/src  && eval `scramv1 runtime -sh` && cd - > /dev/null
 elif [[ "$HOSTNAME" == *.fnal.gov ]]; then
     #BASEDIR=/uscms_data/d1/samr/18_root_from_afs/lcg/
     ROOTVER="5.32.01"
