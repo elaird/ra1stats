e9d50d14381167f5d0f1cdc0e35b060b16b67234
Edward Laird <elaird@gmail.com>
2013-12-16 10:55:17 +0100

    also work outside cmssw

--- cpp/__init__.py
+++ cpp/__init__.py
@@ -5,12 +5,17 @@ import os
 def compile(files=["cpp/StandardHypoTestInvDemo.cxx",
                    #"cpp/NckwWorkspace.cxx", "cpp/RooLognormal2.cxx",
                    "cpp/Poisson.cxx", "cpp/Gaussian.cxx", "cpp/Lognormal.cxx"],
-            root=["`root-config --cflags --libs`"],
-            flags=" -W".join(["", "all", "extra"]),
-            libs=" -l".join(["", "RooFitCore", "RooFit", "RooStats"]),
-            libPaths=" -L".join(["", "${ROOFITSYS}/lib"]),
-            incPaths=" -I".join(["", "${ROOFITSYS}/include"]),
             ):
+    root = "`root-config --cflags --libs`"
+    flags = " -W".join(["", "all", "extra"])
+    incPaths = " -I".join(["", "${ROOFITSYS}/include"]) if os.environ.get("ROOFITSYS") else ""
+    libs = " -l".join(["", "RooFitCore", "RooFit", "RooStats"])
+
+    paths = []
+    for var in ["ROOFITSYS", "ROOTSYS"]:
+        if os.environ.get(var):
+            paths.append("${%s}/lib" % var)
+    libPaths = " -L".join([""] + paths) if paths else ""
 
     #r.gSystem.SetBuildDir("cpp")
     r.gSystem.SetIncludePath(incPaths)
@@ -18,6 +23,6 @@ def compile(files=["cpp/StandardHypoTestInvDemo.cxx",
     for f in files:
         r.gROOT.LoadMacro("%s+" % f)
 
-    cmd = " ".join(["g++", "-o cpp/drive", "cpp/drive.cxx"] + root)
-    cmd += " ".join([flags, libs, libPaths, incPaths])
+    cmd = "g++ -o cpp/drive cpp/drive.cxx "
+    cmd += " ".join([root, flags, libs, libPaths, incPaths])
     os.system(cmd)
