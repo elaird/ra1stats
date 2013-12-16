29e43f315fa7abdbe6a978274ef092ab02a795e7
Edward Laird <elaird@gmail.com>
2013-12-16 10:35:50 +0100

    make work with cmssw

--- cpp/__init__.py
+++ cpp/__init__.py
@@ -1,13 +1,23 @@
 import ROOT as r
+import os
 
-def compile(files = ["cpp/StandardHypoTestInvDemo.cxx",#"cpp/NckwWorkspace.cxx", "cpp/RooLognormal2.cxx",
-                     "cpp/Poisson.cxx","cpp/Gaussian.cxx","cpp/Lognormal.cxx"]) :
-    for f in files :
-        r.gROOT.LoadMacro("%s+"%f)
 
-    flags = ["-Wall", "-Wextra"]
-    root = ["`root-config --cflags --libs`", "-lRooFit", "-lRooStats"]
+def compile(files=["cpp/StandardHypoTestInvDemo.cxx",
+                   #"cpp/NckwWorkspace.cxx", "cpp/RooLognormal2.cxx",
+                   "cpp/Poisson.cxx", "cpp/Gaussian.cxx", "cpp/Lognormal.cxx"],
+            root=["`root-config --cflags --libs`"],
+            flags=" -W".join(["", "all", "extra"]),
+            libs=" -l".join(["", "RooFitCore", "RooFit", "RooStats"]),
+            libPaths=" -L".join(["", "${ROOFITSYS}/lib"]),
+            incPaths=" -I".join(["", "${ROOFITSYS}/include"]),
+            ):
 
-    cmd = " ".join(["g++", "-o cpp/drive", "cpp/drive.cxx"]+flags+root)
-    import os
+    #r.gSystem.SetBuildDir("cpp")
+    r.gSystem.SetIncludePath(incPaths)
+    r.gSystem.SetLinkedLibs(libPaths + libs)
+    for f in files:
+        r.gROOT.LoadMacro("%s+" % f)
+
+    cmd = " ".join(["g++", "-o cpp/drive", "cpp/drive.cxx"] + root)
+    cmd += " ".join([flags, libs, libPaths, incPaths])
     os.system(cmd)
