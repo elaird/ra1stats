3b4edc3947fdbbc7a7e1b07c466b991e281c1c32
Yossof Eshaq <yossofeshaq@gmail.com>
2014-07-17 08:20:28 +0100

    updating cut func

--- patches/__init__.py
+++ patches/__init__.py
@@ -26,7 +26,7 @@ def t2ccCut(iX, x, iY, y, iZ, z) :
 def cutFunc() :
     return {"T1":lambda iX,x,iY,y,iZ,z:(y<(x-150.1) and iZ==1 and x>287.4),
             "T2":lambda iX,x,iY,y,iZ,z:(y<(x-150.1) and iZ==1 and x>287.4 and x<1300.0),
-            "T2tt":lambda iX,x,iY,y,iZ,z:(y<(x-150.1) and iZ==1 and x>287.4 and x<900.0),
+            "T2tt":lambda iX,x,iY,y,iZ,z:(y<(x-75.1) and iZ==1 and x>100.0 and x<1000.0),
             "T2bb":lambda iX,x,iY,y,iZ,z:(y<(x-150.1) and iZ==1 and x>287.4 and x<1300.0),
             "T2cc":t2ccCut,
             "T2bw":lambda iX,x,iY,y,iZ,z:(y<(x-150.1) and iZ==1 and x>287.4),
