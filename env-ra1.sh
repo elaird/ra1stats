c7c98d9baeb475fb2d38ff7c336dc9e5aaf9ca87
Edward Laird <elaird@gmail.com>
2014-10-06 21:03:45 +0100

    copy-paste

--- env-ra1.sh
+++ env-ra1.sh
@@ -1,13 +1,13 @@
-ETC='/slc5_amd64_gcc472/cms/cmssw/CMSSW_6_1_2/src && eval `scramv1 runtime -sh` && cd - > /dev/null' ## using root 5.34
+RELEASE=slc5_amd64_gcc472/cms/cmssw/CMSSW_6_1_2/src
 
 if [[ "$HOSTNAME" == *hep.ph.ic.ac.uk ]]; then
     . /vols/cms/grid/setup.sh
-    cd /vols/sl5_exp_software/cms/${ETC}
+    cd /vols/sl5_exp_software/cms/${RELEASE} && eval `scramv1 runtime -sh` && cd - > /dev/null
 elif [[ "$HOSTNAME" == *.fnal.gov ]]; then
     source /uscmst1/prod/sw/cms/shrc prod
-    cd /uscmst1/prod/sw/cms/${ETC}
+    cd /uscmst1/prod/sw/cms/${RELEASE} && eval `scramv1 runtime -sh` && cd - > /dev/null
 elif [[ "$HOSTNAME" == *.cern.ch ]]; then
-    cd /afs/cern.ch/cms/${ETC}
+    cd /afs/cern.ch/cms/${RELEASE} && eval `scramv1 runtime -sh` && cd - > /dev/null
 fi
 
 
