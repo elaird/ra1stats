2e88cfe693ec77fb66c99a153fb20558e455673e
Edward Laird <elaird@gmail.com>
2014-10-06 21:21:46 +0200

    restructure; set up driver

--- env-534.sh
+++ env-534.sh
@@ -1,11 +1,15 @@
+ETC='/slc5_amd64_gcc472/cms/cmssw/CMSSW_6_1_2/src && eval `scramv1 runtime -sh` && cd - > /dev/null' ## using root 5.34
+
 if [[ "$HOSTNAME" == *hep.ph.ic.ac.uk ]]; then
     . /vols/cms/grid/setup.sh
-    LOC=/vols/sl5_exp_software/cms
+    cd /vols/sl5_exp_software/cms/${ETC}
 elif [[ "$HOSTNAME" == *.fnal.gov ]]; then
     source /uscmst1/prod/sw/cms/shrc prod
-    LOC=/uscmst1/prod/sw/cms/ #LL
-else
-    LOC=/afs/cern.ch/cms
+    cd /uscmst1/prod/sw/cms/${ETC}
+elif [[ "$HOSTNAME" == *.cern.ch ]]; then
+    cd /afs/cern.ch/cms/${ETC}
 fi
 
-cd ${LOC}/slc5_amd64_gcc472/cms/cmssw/CMSSW_6_1_2/src && eval `scramv1 runtime -sh` && cd - > /dev/null ## using root 5.34
+rm -rf driver
+rm driver.py
+ln -s drivers/ra1.py driver.py
