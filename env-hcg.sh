43d799f7ae8422c031e8e1cdfdd6dfc2289dd2e0
Edward Laird <elaird@gmail.com>
2014-10-06 21:30:45 +0200

    remove and set up links

--- env-hcg.sh
+++ env-hcg.sh
@@ -1,12 +1,15 @@
 if [[ "$HOSTNAME" == *hep.ph.ic.ac.uk ]]; then
     . /vols/cms/grid/setup.sh
-    LOC=/vols/sl5_exp_software/cms
-
+    cd /home/hep/ace09/alphat/stats/CMSSW_6_1_1/src && eval `scramv1 runtime -sh` && cd - > /dev/null
 elif [[ "$HOSTNAME" == *.fnal.gov ]]; then
-    echo "FIX ME"
+    echo "env-hcg.sh: FIX ME ($HOSTNAME)"
 else
-    LOC=/afs/cern.ch/cms
+    echo "env-hcg.sh: FIX ME ($HOSTNAME)"
 fi
 
-cd /home/hep/ace09/alphat/stats/CMSSW_6_1_1/src && eval `scramv1 runtime -sh` && cd - > /dev/null
+rm -rf driver
+rm -f driver.py
+ln -s drivers/hcg.py driver.py
 
+rm -f env.sh
+ln -s env-hcg.sh env.sh
