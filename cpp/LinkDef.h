568a388a91c14dd9fcbe954c19f3a12286bc0f60
Edward Laird <elaird@gmail.com>
2015-02-02 14:09:25 +0100

    replace classes with linkdef

---
+++ cpp/LinkDef.h
@@ -0,0 +1,10 @@
+#include "Poisson.h"
+#include "Gaussian.h"
+#include "Lognormal.h"
+
+#ifdef __CLING__
+#pragma link C++ class Poisson+;
+#pragma link C++ class Gaussian+;
+#pragma link C++ class Lognormal+;
+#endif
+
