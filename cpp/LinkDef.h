997dbf8f7b356c1c7240770da1e3809ede481d2d
Edward Laird <elaird@gmail.com>
2015-02-02 16:54:07 +0100

    add cint

--- cpp/LinkDef.h
+++ cpp/LinkDef.h
@@ -2,6 +2,12 @@
 #include "Gaussian.h"
 #include "Lognormal.h"
 
+#ifdef __CINT__
+#pragma link C++ class Poisson+;
+#pragma link C++ class Gaussian+;
+#pragma link C++ class Lognormal+;
+#endif
+
 #ifdef __CLING__
 #pragma link C++ class Poisson+;
 #pragma link C++ class Gaussian+;
