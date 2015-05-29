c385e0104a9e6c083b88c0211560c02c77bca5d1
Robert <robert.bainbridge@cern.ch>
2015-05-29 14:25:36 +0100

    asymptotic CLs

--- configuration/limit.py
+++ configuration/limit.py
@@ -16,7 +16,7 @@ def testStatistic():
 def calculatorType():
     return ["frequentist",
             "asymptotic",
-            "asymptoticNom"][0]
+            "asymptoticNom"][1]
 
 
 def method():
